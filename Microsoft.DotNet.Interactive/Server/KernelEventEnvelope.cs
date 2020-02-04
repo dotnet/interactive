f2d02349e98228d16e0e16e078db92a0ddb7d978
Dongbo Wang <dongbow@microsoft.com>
2020-02-03 17:19:29 -0800

    Fix test failure 2nd attempt

--- Microsoft.DotNet.Interactive/Server/KernelEventEnvelope.cs
+++ Microsoft.DotNet.Interactive/Server/KernelEventEnvelope.cs
@@ -37,6 +37,7 @@ namespace Microsoft.DotNet.Interactive.Server
                 [nameof(DisplayedValueUpdated)] = typeof(KernelEventEnvelope<DisplayedValueUpdated>),
                 [nameof(ErrorProduced)] = typeof(KernelEventEnvelope<ErrorProduced>),
                 [nameof(IncompleteCodeSubmissionReceived)] = typeof(KernelEventEnvelope<IncompleteCodeSubmissionReceived>),
+                [nameof(InputRequested)] = typeof(KernelEventEnvelope<InputRequested>),
                 [nameof(PackageAdded)] = typeof(KernelEventEnvelope<PackageAdded>),
                 [nameof(ReturnValueProduced)] = typeof(KernelEventEnvelope<ReturnValueProduced>),
                 [nameof(StandardErrorValueProduced)] = typeof(KernelEventEnvelope<StandardErrorValueProduced>),
