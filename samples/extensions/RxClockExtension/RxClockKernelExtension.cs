0659ee0abe8e293056a19759f04dc19405d9e7fb
Diego <dicolomb@microsoft.com>
2020-02-11 13:21:16 +0000

    add copyright header

--- samples/extensions/RxClockExtension/RxClockKernelExtension.cs
+++ samples/extensions/RxClockExtension/RxClockKernelExtension.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
