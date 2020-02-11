26ab349b29a5a1876d15f5a66641efabe3d6d972
Diego <dicolomb@microsoft.com>
2020-02-11 23:24:04 +0000

    proper parameter quoting

--- eng/build.sh
+++ eng/build.sh
@@ -36,9 +36,7 @@ function TestUsingNPM() {
 }
 
 # invoke regular build/test script
-. "$scriptroot/common/build.sh" /p:Projects=$scriptroot/../dotnet-interactive.sln $args
-
-. "$scriptroot/common/build.sh" /p:Projects=$scriptroot/../samples/extensions/SampleExtensions.sln $args
+. "$scriptroot/common/build.sh" "/p:Projects=$scriptroot/../dotnet-interactive.sln;$scriptroot/../samples/extensions/SampleExtensions.sln" $args
 
 # directly invoke npm tests
 # if [[ "$run_tests" == "true" ]]; then
