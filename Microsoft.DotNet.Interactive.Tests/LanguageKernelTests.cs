1d6d85a36bc8a70f6d7e36c3bb813db90d545aee
Tyler Leonhardt <tylerl0706@gmail.com>
2020-02-24 12:56:53 -0800

    Add a couple other test cases

--- Microsoft.DotNet.Interactive.Tests/LanguageKernelTests.cs
+++ Microsoft.DotNet.Interactive.Tests/LanguageKernelTests.cs
@@ -898,6 +898,8 @@ for ($j = 1; $j -le 4; $j++ ) {
             var accelerator = typeof(PSObject).Assembly.GetType("System.Management.Automation.TypeAccelerators");
             dynamic typeAccelerators = accelerator.GetProperty("Get").GetValue(null);
             Assert.Equal(typeAccelerators["Graph.Scatter"].FullName, $"{typeof(Graph).FullName}+Scatter");
+            Assert.Equal(typeAccelerators["Layout"].FullName, $"{typeof(Layout).FullName}+Layout");
+            Assert.Equal(typeAccelerators["Chart"].FullName, typeof(Chart).FullName);
         }
     }
 }
