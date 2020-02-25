b92284308dbff96e148b9ca10a109a99874dac7d
Diego <dicolomb@microsoft.com>
2020-02-25 16:10:56 +0000

    wip

--- dotnet-interactive/Startup.cs
+++ dotnet-interactive/Startup.cs
@@ -38,6 +38,8 @@ namespace Microsoft.DotNet.Interactive.App
             IApplicationBuilder app,
             IHostApplicationLifetime lifetime)
         {
+            
+           
         }
     }
 }
