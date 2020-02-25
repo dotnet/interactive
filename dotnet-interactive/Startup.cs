cbfe353f113004bc040807623a130ace1e8ad1cf
Diego <dicolomb@microsoft.com>
2020-02-25 23:38:00 +0000

    call it a day

--- dotnet-interactive/Startup.cs
+++ dotnet-interactive/Startup.cs
@@ -1,7 +1,13 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Threading.Tasks;
+using FSharp.Compiler;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.DotNet.Interactive.App.CommandLine;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,14 +38,49 @@ namespace Microsoft.DotNet.Interactive.App
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         public void Configure(
             IApplicationBuilder app,
             IHostApplicationLifetime lifetime)
         {
-            
-           
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseRouter(r => { r.Routes.Add(new SanityCheckRouter()); });
+            app.UseEndpoints(builder =>
+            {
+                builder.MapHub<KernelHub>("/kernel");
+            });
         }
+    }
+
+    public class SanityCheckRouter : IRouter
+    {
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null;
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            context.Handler = async httpContext => { await httpContext.Response.WriteAsync("hello dude"); };
+            return Task.CompletedTask;
+        }
+    }
+
+    public class KernelHub : Hub
+    {
+        private readonly Func<IKernel> _kernelFactory;
+
+        public KernelHub()
+        {
+            //_kernelFactory = kernelFactory ?? throw new ArgumentNullException(nameof(kernelFactory));
+            
+        }
+
+        
     }
 }
