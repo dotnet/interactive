8035dd9dc45920acacba572d5cec2f3f3e4f1aeb
Diego <dicolomb@microsoft.com>
2020-03-03 12:01:11 +0000

    create kernel objects to simplify api calls

---
+++ dotnet-interactive/HttpRouting/KernelsRouter.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+
+namespace Microsoft.DotNet.Interactive.App.HttpRouting
+{
+    public class KernelsRouter : IRouter
+    {
+        private readonly IKernel _kernel;
+
+        public KernelsRouter(IKernel kernel)
+        {
+            _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null;
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            if (context.HttpContext.Request.Method == HttpMethods.Get)
+            {
+                var segments =
+                    context.HttpContext
+                        .Request
+                        .Path
+                        .Value
+                        .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (segments[0] == "kernels")
+                {
+                    var targetKernel = _kernel;
+                    var names = new List<string>
+                    {
+                        targetKernel.Name
+                    };
+
+                    if (targetKernel is CompositeKernel compositeKernel)
+                    {
+                        names.AddRange(compositeKernel.ChildKernels.Select(k => k.Name));
+                    }
+
+                    context.Handler = async httpContext =>
+                    {
+                        httpContext.Response.ContentType = "application/json";
+                        await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(names));
+                    };
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
