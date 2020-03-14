64633a56c548b5030817fcc7ce6d555b5724449c
Jon Sequeira <jonsequeira@gmail.com>
2020-03-14 12:39:26 -0700

    R: fix warning

--- Microsoft.DotNet.Interactive.Tests/Language.cs
+++ Microsoft.DotNet.Interactive.Tests/Language.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#pragma warning disable 8509
 namespace Microsoft.DotNet.Interactive.Tests
 {
     public enum Language
