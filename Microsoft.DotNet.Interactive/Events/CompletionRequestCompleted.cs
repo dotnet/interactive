73c25597fd2deddf4e31e9e467952421504d47d8
Tyler Leonhardt <tylerl0706@gmail.com>
2020-03-16 13:35:14 -0700

    Better completions for PowerShell

--- Microsoft.DotNet.Interactive/Events/CompletionRequestCompleted.cs
+++ Microsoft.DotNet.Interactive/Events/CompletionRequestCompleted.cs
@@ -9,11 +9,43 @@ namespace Microsoft.DotNet.Interactive.Events
 {
     public class CompletionRequestCompleted : KernelEventBase
     {
+        /// <summary>
+        /// The start index (inclusive) of where to replace in a completion request.
+        /// </summary>
+        public int? ReplacementStartIndex { get; }
+
+        /// <summary>
+        /// The end index (exclusive) of where to replace in a completion request.
+        /// </summary>
+        public int? ReplacementEndIndex { get; }
+
+        /// <summary>
+        /// The list of completion options.
+        /// </summary>
         public IEnumerable<CompletionItem> CompletionList { get; }
 
-        public CompletionRequestCompleted(IEnumerable<CompletionItem> completionList, IKernelCommand command) : base(command)
+        public CompletionRequestCompleted(
+            IEnumerable<CompletionItem> completionList,
+            IKernelCommand command,
+            int? replacementStartIndex = null,
+            int? replacementEndIndex = null) : base(command)
         {
             CompletionList = completionList ?? throw new ArgumentNullException(nameof(completionList));
+
+            // replacementStartIndex and replacementEndIndex must either both be null
+            // or both be defined.
+            if (replacementStartIndex != null && replacementEndIndex == null)
+            {
+                throw new ArgumentNullException(nameof(replacementEndIndex));
+            }
+
+            if (replacementStartIndex == null && replacementEndIndex != null)
+            {
+                throw new ArgumentNullException(nameof(replacementStartIndex));
+            }
+
+            ReplacementStartIndex = replacementStartIndex;
+            ReplacementEndIndex = replacementEndIndex;
         }
     }
 }
