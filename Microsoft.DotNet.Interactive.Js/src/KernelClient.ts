89841b3b15130a850e7004450d212b818b5176ed
Diego <dicolomb@microsoft.com>
2020-03-19 16:17:55 +0000

    initial steps

--- Microsoft.DotNet.Interactive.Js/src/KernelClient.ts
+++ Microsoft.DotNet.Interactive.Js/src/KernelClient.ts
@@ -1,11 +1,19 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 export interface KernelClient {    
-    GetVariable(variableName: string): any ;    
+    GetVariable(variableName: string): any ;   
+    GetStuff() : number;
 }
 
 export function createClient(): KernelClient{
     return {
-        GetVariable(variableName: string): any{
-            return 1;
+        
+        GetVariable : (variableName: string): any => {
+            return 1;            
+        },
+        GetStuff: (): number => {
+            return 1;            
         }
-    }
+    };
 }
