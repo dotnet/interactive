f0d45029102799e90cfb73ba909001c863824701
Diego <dicolomb@microsoft.com>
2020-03-19 16:17:55 +0000

    initial steps

--- Microsoft.DotNet.Interactive.Js/tests/client.spec.ts
+++ Microsoft.DotNet.Interactive.Js/tests/client.spec.ts
@@ -1,4 +1,6 @@
-//import  * as interactive from "../src/dotnet-interactive";
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 import { expect } from "chai";
 import * as interactive from "../src/dotnet-interactive"
 
@@ -6,7 +8,13 @@ describe("client", () => {
     describe("not sure", () => {
         it("runs", () => {
             let client = interactive.createClient();
-            let value = client.GetVariable("code");
+            let value = client.GetVariable("code");            
+            expect(value).to.eq(1);
+        });
+
+        it("runs again", () => {
+            let client = interactive.createClient();
+            let value = client.GetStuff();            
             expect(value).to.eq(1);
         });
     });
--- Microsoft.DotNet.Interactive.Js/wallaby.js
+++ Microsoft.DotNet.Interactive.Js/wallaby.js
@@ -1,19 +1,19 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 module.exports = function (wallaby) {
     return {
+        env: {
+            type: "node"
+        },
         files: [
             "src/**/*.ts",
             { pattern: "tests/**/*.ts", instrument: false },            
             "!tests/**/*.spec.ts*",
-            {pattern: 'tests/test-main.js', instrument: false}
         ],
         tests: ["tests/**/*.spec.ts"],
         compilers: {
-            '**/*.ts?(x)': wallaby.compilers.typeScript({ 
-                useStandardDefaults: true })
+            '**/*.ts?(x)': wallaby.compilers.typeScript({ })
         },
         testFramework: 'mocha',
         debug: true,
