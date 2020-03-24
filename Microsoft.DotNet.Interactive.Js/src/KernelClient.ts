effbc3f6c4f87a1f70f21df4c7d0bbf803bfe991
Diego <dicolomb@microsoft.com>
2020-03-24 15:11:06 +0000

    add api code

--- Microsoft.DotNet.Interactive.Js/src/KernelClient.ts
+++ Microsoft.DotNet.Interactive.Js/src/KernelClient.ts
@@ -1,19 +1,44 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export interface KernelClient {    
-    GetVariable(variableName: string): any ;   
-    GetStuff() : number;
+export interface VariableRequest {
+    [kernelName: string]: Array<any>;
 }
 
-export function createClient(): KernelClient{
-    return {
-        
-        GetVariable : (variableName: string): any => {
-            return 1;            
-        },
-        GetStuff: (): number => {
-            return 1;            
+export interface VariableResponse {
+    [kernelName: string]: {
+        [variableName: string]: any
+    }
+}
+
+export interface KernelClient {
+    GetVariable(variableName: string): any;
+}
+
+export interface DotnetInteractiveClient {
+    GetVariable(kernelName: string, variableName: string): any
+    GetVariables(variableRequest: VariableRequest): VariableResponse;
+}
+
+export class DotnetInteractiveScopeContainer {
+    [key:string] : DotnetInteractiveScope
+}
+
+export class DotnetInteractiveScope{
+    [key:string]: any
+}
+
+export function init(global: any) {
+    global.getDotnetInteractiveScope = (key: string) =>{
+        if (typeof(global.interactiveScopes) === undefined){
+            global.interactiveScopes = new DotnetInteractiveScopeContainer();
         }
-    };
+
+        if(typeof(global.interactiveScopes[key]) === undefined){
+            global.interactiveScopes[key] = new DotnetInteractiveScope();
+        }
+
+        return global.interactiveScopes[key];
+    }
+    
 }
