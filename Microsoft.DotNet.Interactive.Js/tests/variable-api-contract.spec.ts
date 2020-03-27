208b3da89d995ce2ff8b507b9744e772d0f46df0
Diego <dicolomb@microsoft.com>
2020-03-27 18:52:40 +0000

    function matcher

--- Microsoft.DotNet.Interactive.Js/tests/variable-api-contract.spec.ts
+++ Microsoft.DotNet.Interactive.Js/tests/variable-api-contract.spec.ts
@@ -35,12 +35,11 @@ describe("variable api contract", () => {
             fsharp: ["number", "boolean"]
         };
 
-        fetchMock.mock(
-            {
-                url: `${rootUrl}/variables`,
-                method: "POST",
-                body: varaibleRequest
-
+        fetchMock.mock((url, opt) => {
+            let matchesUrl = url === `${rootUrl}/variables`;
+            let matchesMEthod = opt.method === "POST";
+            let matchedBody = opt.body === JSON.stringify(varaibleRequest);
+            return matchesUrl && matchesMEthod && matchedBody;
             },
             require("./Responses/variableBundleResponse01.json"));
 
