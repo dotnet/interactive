d4d56b6a79a6ad49cb568f5d41183dbc4c48dce4
Diego <dicolomb@microsoft.com>
2020-03-27 11:38:04 +0000

    add variable api tests

--- Microsoft.DotNet.Interactive.Js/tests/client.spec.ts
+++ Microsoft.DotNet.Interactive.Js/tests/client.spec.ts
@@ -5,41 +5,42 @@ import { expect } from "chai";
 import * as interactive from "../src/dotnet-interactive"
 import { createDotnetInteractiveClient } from "../src/dotnet-interactive/KernelClientImpl";
 import * as fetchMock from "fetch-mock";
+import { asKernelClientContainer } from "./testSupprot";
 
 describe("dotnet-interactive", () => {
-    
+
     afterEach(fetchMock.restore);
     describe("initialisation", () => {
         it("injects function to create scope for dotnet interactive", () => {
-            let global : any = {};
+            let global: any = {};
             interactive.init(global);
 
-            expect(typeof(global.getDotnetInteractiveScope))
-            .to
-            .equal('function');
+            expect(typeof (global.getDotnetInteractiveScope))
+                .to
+                .equal('function');
         });
 
         it("injects function to create dotnet interactive client", () => {
-            let global : any = {};
+            let global: any = {};
             interactive.init(global);
 
-            expect(typeof(global.createDotnetInteractiveClient))
-            .to
-            .equal('function');
-        });       
+            expect(typeof (global.createDotnetInteractiveClient))
+                .to
+                .equal('function');
+        });
     });
 
-    describe("kernele discovery", () =>{
-        it("creates kernel clients for all discovered kernesl", async () =>{
+    describe("kernel discovery", () => {
+        it("creates kernel clients for all discovered kernesl", async () => {
             const rootUrl = "https://dotnet.interactive.com:999";
-            var expectedKernels = [".NET","csharp","fsharp","powershell","javascript","html"];
-            fetchMock.get(`${rootUrl}/kernels`,  expectedKernels );
-            let client : any = await createDotnetInteractiveClient(rootUrl);
-            
-            for( let kernelName of expectedKernels){
+            var expectedKernels = [".NET", "csharp", "fsharp", "powershell", "javascript", "html"];
+            fetchMock.get(`${rootUrl}/kernels`, expectedKernels);
+            let client = asKernelClientContainer(await createDotnetInteractiveClient(rootUrl));
+
+            for (let kernelName of expectedKernels) {
                 expect(client[kernelName]).not.to.be.undefined;
             }
 
         })
-    } );
+    });
 });
---
+++ Microsoft.DotNet.Interactive.Js/tests/variableApi.spec.ts
@@ -0,0 +1,62 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { expect } from "chai";
+import { createDotnetInteractiveClient } from "../src/dotnet-interactive/KernelClientImpl";
+import * as fetchMock from "fetch-mock";
+import { VariableResponse, VariableRequest, KernelClientContainer } from "../src/dotnet-interactive/dotnet-interactive-interfaces";
+import { asKernelClientContainer } from "./testSupprot";
+
+describe("dotnet-interactive", () => {
+    const rootUrl = "https://dotnet.interactive.com:999";
+    beforeEach(() => {
+        var expectedKernels = [".NET", "csharp", "fsharp", "powershell", "javascript", "html"];
+        fetchMock.get(`${rootUrl}/kernels`, expectedKernels);
+    })
+    afterEach(fetchMock.restore);
+    describe("Variable api", () => {
+        it("car load multiple variables in a single request", async () => {
+            let expectedVariables: VariableResponse = {
+                csharp:
+                {
+                    a: 1,
+                    b: "2"
+                },
+                fsharp: {
+                    c: 3,
+                    d: false
+                }
+            }
+
+            let varaibleRequest: VariableRequest = {
+                csharp: ["a", "b"],
+                fsharp: ["c", "d"]
+            };
+
+            fetchMock.post(`${rootUrl}/variables`, expectedVariables);
+
+            let client = await createDotnetInteractiveClient(rootUrl);
+            let variables = await client.getVariables(varaibleRequest);
+
+            expect(variables).to.deep.eq(expectedVariables);
+        });
+
+        it("car load variable using kernel client", async () => {
+            let expectedVariable = 123;
+            let variableName = "data";
+            let kernelName = "csharp";
+
+            fetchMock.get(`${rootUrl}/variables/${kernelName}/${variableName}`,
+                {
+                    body: expectedVariable,
+                    status: 200
+                });
+
+            let client = asKernelClientContainer( await createDotnetInteractiveClient(rootUrl) );
+            let csharpClient = client[kernelName];
+            let variable = await csharpClient.getVariable(variableName);
+
+            expect(variable).to.deep.eq(expectedVariable);
+        });
+    });
+});
