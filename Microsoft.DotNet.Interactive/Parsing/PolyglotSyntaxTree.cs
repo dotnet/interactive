e1114754e453a7ada1fc47c0780ae0f92779bb62
Jon Sequeira <jonsequeira@gmail.com>
2020-03-31 18:30:24 -0700

    R: code cleanup

--- Microsoft.DotNet.Interactive/Parsing/PolyglotSyntaxTree.cs
+++ Microsoft.DotNet.Interactive/Parsing/PolyglotSyntaxTree.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
 #nullable enable
@@ -29,53 +25,6 @@ namespace Microsoft.DotNet.Interactive.Parsing
         public SyntaxNode GetRoot()
         {
             return _root;
-        }
-
-        public bool TryGetText(out SourceText text)
-        {
-            text = default;
-            return false;
-        }
-
-        public SourceText GetText(CancellationToken cancellationToken = new CancellationToken())
-        {
-            return default;
-        }
-
-        protected Task<SyntaxNode> GetRootAsyncCore(CancellationToken cancellationToken)
-        {
-            return default;
-        }
-
-
-        public IEnumerable<Diagnostic> GetDiagnostics(CancellationToken cancellationToken = new CancellationToken())
-        {
-            return default;
-        }
-
-        public IEnumerable<Diagnostic> GetDiagnostics(SyntaxNode node)
-        {
-            return default;
-        }
-
-        public IEnumerable<Diagnostic> GetDiagnostics(SyntaxToken token)
-        {
-            return default;
-        }
-
-        public IEnumerable<Diagnostic> GetDiagnostics(SyntaxTrivia trivia)
-        {
-            return default;
-        }
-
-        public IEnumerable<Diagnostic> GetDiagnostics(CodeAnalysis.SyntaxNodeOrToken nodeOrToken)
-        {
-            return default;
-        }
-
-        public FileLinePositionSpan GetLineSpan(TextSpan span, CancellationToken cancellationToken = new CancellationToken())
-        {
-            return default;
         }
 
         public override string ToString()
