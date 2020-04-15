55612cc8b42d1d0b7484f7a73124d4a4cc6b0955
Tyler Leonhardt <tylerl0706@gmail.com>
2020-04-15 12:12:02 -0700

    Add PROFILE tests

--- Microsoft.DotNet.Interactive.PowerShell.Tests/PowerShellKernelTests.cs
+++ Microsoft.DotNet.Interactive.PowerShell.Tests/PowerShellKernelTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO;
+using System.Management.Automation;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
@@ -11,6 +12,8 @@ namespace Microsoft.DotNet.Interactive.PowerShell.Tests
 {
     public class PowerShellKernelTests
     {
+        private readonly string _allUsersCurrentHostProfilePath = Path.Combine(Path.GetDirectoryName(typeof(PSObject).Assembly.Location), "Microsoft.dotnet-interactive_profile.ps1");
+
         [Theory]
         [InlineData(@"$x = New-Object -TypeName System.IO.FileInfo -ArgumentList c:\temp\some.txt", typeof(FileInfo))]
         [InlineData("$x = \"hello!\"", typeof(string))]
@@ -23,6 +26,63 @@ namespace Microsoft.DotNet.Interactive.PowerShell.Tests
             kernel.TryGetVariable("x", out var fi).Should().BeTrue();
 
             fi.Should().BeOfType(expectedType);
+        }
+
+        [Fact]
+        public async Task GetCorrectProfilePaths()
+        {
+            using var kernel = new PowerShellKernel();
+
+            // Set variables we will retrieve later.
+            await kernel.SubmitCodeAsync("$currentUserCurrentHost = $PROFILE.CurrentUserCurrentHost");
+            await kernel.SubmitCodeAsync("$allUsersCurrentHost = $PROFILE.AllUsersCurrentHost");
+
+            kernel.TryGetVariable("currentUserCurrentHost", out var profileObj).Should().BeTrue();
+            profileObj.Should().BeOfType<string>();
+            string currentUserCurrentHost = profileObj.As<string>();
+
+            // Get $PROFILE default.
+            kernel.TryGetVariable("PROFILE", out profileObj).Should().BeTrue();
+            profileObj.Should().BeOfType<string>();
+            string profileDefault = profileObj.As<string>();
+
+            // Check that $PROFILE is not null or empty and it is the same as
+            // $PROFILE.CurrentUserCurrentHost
+            profileDefault.Should().NotBeNullOrEmpty();
+            profileDefault.Should().Be(currentUserCurrentHost);
+
+            kernel.TryGetVariable("allUsersCurrentHost", out profileObj).Should().BeTrue();
+            profileObj.Should().BeOfType<string>();
+            string allUsersCurrentHost = profileObj.As<string>();
+
+            // Check that $PROFILE.AllUsersCurrentHost is what we expect it is:
+            // $PSHOME + Microsoft.dotnet-interactive_profile.ps1
+            allUsersCurrentHost.Should().Be(_allUsersCurrentHostProfilePath);
+        }
+
+        [Fact]
+        public async Task VerifyAllUsersProfileRuns()
+        {
+            var randomVariableName = Path.GetRandomFileName().Split('.')[0];
+            File.WriteAllText(_allUsersCurrentHostProfilePath, $"$global:{randomVariableName} = $true");
+
+            try
+            {
+                using var kernel = new PowerShellKernel();
+
+                // trigger first time setup.
+                await kernel.SubmitCodeAsync("Get-Date");
+
+                kernel.TryGetVariable(randomVariableName, out var profileObj).Should().BeTrue();
+
+                profileObj.Should().BeOfType<bool>();
+                profileObj.As<bool>().Should().BeTrue();
+            }
+            finally
+            {
+                
+                File.Delete(_allUsersCurrentHostProfilePath);
+            }
         }
     }
 }
