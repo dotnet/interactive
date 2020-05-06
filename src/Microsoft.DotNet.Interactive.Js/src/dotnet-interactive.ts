84c7b42f9d5dbbd9bdadceb131b6bb6bbb331698
Diego <dicolomb@microsoft.com>
2020-05-07 00:31:50 +0100

    export createClient without init

--- src/Microsoft.DotNet.Interactive.Js/src/dotnet-interactive.ts
+++ src/Microsoft.DotNet.Interactive.Js/src/dotnet-interactive.ts
@@ -1,3 +1,4 @@
 export * from "./dotnet-interactive/library-init";
 export * from "./dotnet-interactive/dotnet-interactive-interfaces";
 export * from "./dotnet-interactive/contracts";
+export { createDotnetInteractiveClient } from "./dotnet-interactive/kernel-client-impl";
