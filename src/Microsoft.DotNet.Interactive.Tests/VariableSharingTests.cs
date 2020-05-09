5d6452b8db1673103767737d2ad1672c7f9a6d13
Jon Sequeira <jonsequeira@gmail.com>
2020-05-09 07:51:39 -0700

    improve variable sharing tests

--- src/Microsoft.DotNet.Interactive.Tests/VariableSharingTests.cs
+++ src/Microsoft.DotNet.Interactive.Tests/VariableSharingTests.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.DotNet.Interactive.CSharp;
@@ -16,39 +19,31 @@ namespace Microsoft.DotNet.Interactive.Tests
     public class VariableSharingTests
     {
         [Theory]
+        // To C#
         [InlineData(
             "#!fsharp",
             "let x = 123",
-            "#!csharp",
             "(GetKernel(\"fsharp\") as Microsoft.DotNet.Interactive.DotNetLanguageKernel).TryGetVariable(\"x\", out int x);\nx")]
         [InlineData(
             "#!fsharp",
             "let x = 123",
-            "#!csharp",
-            "#!share --from fsharp x\nfsharp.TryGetVariable(\"x\", out int x);\nx", Skip = "WIP")]
-        public async Task Variables_can_be_read_from_other_kernels(
-            string fromLanguage,
+            "#!share --from fsharp x\nx")]
+        [InlineData(
+           "#!pwsh",
+           "$x = 123",
+           "#!share --from pwsh x\nx")]
+        public async Task csharp_kernel_can_read_variables_from_other_kernels(
+            string from,
             string codeToWrite,
-            string toLanguage,
             string codeToRead)
         {
-            using var kernel = new CompositeKernel
-            {
-                new CSharpKernel()
-                    .UseKernelHelpers()
-                    .UseDotNetVariableSharing(),
-                new FSharpKernel()
-                    .UseKernelHelpers()
-                    .UseDotNetVariableSharing(),
-                new PowerShellKernel()
-                    .UseDotNetVariableSharing()
-            }.LogEventsToPocketLogger();
+            using var kernel = CreateKernel();
 
             using var events = kernel.KernelEvents.ToSubscribedList();
 
-            await kernel.SubmitCodeAsync($"{fromLanguage}\n{codeToWrite}");
+            await kernel.SubmitCodeAsync($"{from}\n{codeToWrite}");
 
-            await kernel.SubmitCodeAsync($"{toLanguage}\n{codeToRead}");
+            await kernel.SubmitCodeAsync($"#!csharp\n{codeToRead}");
 
             events.Should()
                   .ContainSingle<ReturnValueProduced>()
@@ -56,6 +51,111 @@ namespace Microsoft.DotNet.Interactive.Tests
                   .Value
                   .Should()
                   .Be(123);
+        }
+
+        [Theory(Skip = "Requires FSharpKernel.SetVariableAsync")]
+        [InlineData(
+            "#!csharp",
+            "var x = 123;",
+            "#!share --from csharp x\nx")]
+        [InlineData(
+            "#!pwsh",
+            "$x = 123",
+            "#!share --from pwsh x\nx")]
+        public async Task fsharp_kernel_can_read_variables_from_other_kernels(
+            string from,
+            string codeToWrite,
+            string codeToRead)
+        {
+            using var kernel = CreateKernel();
+
+            using var events = kernel.KernelEvents.ToSubscribedList();
+
+            await kernel.SubmitCodeAsync($"{from}\n{codeToWrite}");
+
+            await kernel.SubmitCodeAsync($"#!fsharp\n{codeToRead}");
+
+            events.Should()
+                  .ContainSingle<ReturnValueProduced>()
+                  .Which
+                  .Value
+                  .Should()
+                  .Be(123);
+        }
+
+        [Theory]
+        [InlineData(
+            "#!csharp",
+            "var x = 123;",
+            "#!share --from csharp x\n\"$($x):$($x.GetType().ToString())\"")]
+        [InlineData(
+            "#!fsharp",
+            "let x = 123",
+            "#!share --from fsharp x\n\"$($x):$($x.GetType().ToString())\"")]
+        public async Task pwsh_kernel_can_read_variables_from_other_kernels(
+            string from,
+            string codeToWrite,
+            string codeToRead)
+        {
+            using var kernel = CreateKernel();
+
+            using var events = kernel.KernelEvents.ToSubscribedList();
+
+            await kernel.SubmitCodeAsync($"{from}\n{codeToWrite}");
+
+            await kernel.SubmitCodeAsync($"#!pwsh\n{codeToRead}");
+
+            events.Should()
+                  .ContainSingle<StandardOutputValueProduced>()
+                  .Which
+                  .Value
+                  .As<string>()
+                  .Trim()
+                  .Should()
+                  .Be("123:System.Int32");
+        }
+
+        [Fact]
+        public async Task Directives_can_access_local_kernel_variables()
+        {
+            using var kernel = CreateKernel();
+            kernel.DefaultKernelName = "csharp";
+            var csharpKernel = (CSharpKernel) kernel.FindKernel("csharp");
+
+            using var events = kernel.KernelEvents.ToSubscribedList();
+            var receivedValue = 0;
+
+            var directive = new Command("#!grab")
+            {
+                new Argument<int>("x")
+            };
+            directive.Handler = CommandHandler.Create<KernelInvocationContext, int>((context, x) =>
+            {
+                return receivedValue = x;
+            });
+
+            csharpKernel.AddDirective(directive);
+
+            await kernel.SubmitCodeAsync("var x = 123;");
+            await kernel.SubmitCodeAsync("#!grab $x");
+
+            receivedValue.Should().Be(123);
+        }
+
+        private static CompositeKernel CreateKernel()
+        {
+            return new CompositeKernel
+            {
+                new CSharpKernel()
+                    .UseKernelHelpers()
+                    .UseDotNetVariableSharing(),
+                new FSharpKernel()
+                    .UseKernelHelpers()
+                    .UseDefaultNamespaces() 
+                    .UseDotNetVariableSharing(),
+                new PowerShellKernel()
+                    .UseDotNetVariableSharing()
+            }.LogEventsToPocketLogger();
         }
 
         [Fact(Skip = "WIP")]
