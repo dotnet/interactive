698663966adcbad32e83de5888200a5e3e7c5d42
Diego <dicolomb@microsoft.com>
2020-05-11 22:10:42 +0100

    split files

--- src/dotnet-interactive/CommandLine/JupyterFrontedEnvironment.cs
+++ src/dotnet-interactive/CommandLine/JupyterFrontedEnvironment.cs
@@ -1,10 +1,12 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.DotNet.Interactive.App.CommandLine
 {
-    public class BrowserFrontendEnvironment : FrontendEnvironment
+    public class JupyterFrontedEnvironment : BrowserFrontendEnvironment
     {
-        
+        public Uri DiscoveredUri { get; set; }
     }
 }
