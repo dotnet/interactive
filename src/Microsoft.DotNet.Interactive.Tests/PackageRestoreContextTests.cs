0a19a16808be2aa5326ac22deccf9ad8eff283ad
Diego <dicolomb@microsoft.com>
2020-05-12 00:53:18 +0100

    update tests and ensure registration of fronted environment as base type

--- src/Microsoft.DotNet.Interactive.Tests/PackageRestoreContextTests.cs
+++ src/Microsoft.DotNet.Interactive.Tests/PackageRestoreContextTests.cs
@@ -8,9 +8,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
-using System.Collections.Generic;
-using Microsoft.DotNet.Interactive.Utility;
-using Microsoft.DotNet.DependencyManager;
 
 namespace Microsoft.DotNet.Interactive.Tests
 {
