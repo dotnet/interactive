278ab7223210f6b4c03f68bd7dc70e4c0ad32239
Diego <dicolomb@microsoft.com>
2020-05-19 09:22:04 +0100

    include process in api seed

--- src/dotnet-interactive/HttpApiBootstrapper.cs
+++ src/dotnet-interactive/HttpApiBootstrapper.cs
@@ -12,6 +12,7 @@ namespace Microsoft.DotNet.Interactive.App
         public static string GetHtmlInjection(Uri[] probingUris, string seed)
         {
             var apiCacheBuster = $"{Process.GetCurrentProcess().Id}.{seed}";
+            var apiSeed = apiCacheBuster;
             var template = @"
 <div>
     <div id='dotnet-interactive-this-cell-$CACHE_BUSTER$' style='display: none'>
