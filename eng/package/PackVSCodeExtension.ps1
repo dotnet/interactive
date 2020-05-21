55cd8cdbaed5c27ed28f1d0ca306403a327043bb
Brett V. Forsgren <brettfo@microsoft.com>
2020-05-21 12:41:03 -0700

    append git sha to vscode package (#448)

---
+++ eng/package/PackVSCodeExtension.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding(PositionalBinding=$false)]
+param (
+    [string]$stableToolVersionNumber,
+    [string]$gitSha,
+    [string]$outDir
+)
+
+Set-StrictMode -version 2.0
+$ErrorActionPreference = "Stop"
+
+try {
+    # set version
+    Write-Host "Setting package version to $stableToolVersionNumber"
+    npm version $stableToolVersionNumber
+
+    # append git sha to package description
+    $packageJsonPath = Join-Path (Get-Location) "package.json"
+    Write-Host "Appending git sha to description in $packageJsonPath"
+    $packageJsonContents = (Get-Content $packageJsonPath | Out-String | ConvertFrom-Json)
+    $packageJsonContents.description += "  Git SHA $gitSha"
+    $packageJsonContents | ConvertTo-Json -depth 100 | Out-File $packageJsonPath
+
+    # pack
+    Write-Host "Packing extension"
+    npx vsce package --out "$outDir\dotnet-interactive-vscode-$stableToolVersionNumber.vsix"
+}
+catch {
+    Write-Host $_
+    Write-Host $_.Exception
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
