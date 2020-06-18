dcd8f480d988901aa4b2d0068d7759d7e23c1f65
Diego <dicolomb@microsoft.com>
2020-06-18 09:24:10 +0100

    test is more explicit about intent

--- src/Microsoft.DotNet.Interactive.Tests/LanguageServices/CompletionTests.cs
+++ src/Microsoft.DotNet.Interactive.Tests/LanguageServices/CompletionTests.cs
@@ -192,42 +192,42 @@ namespace Microsoft.DotNet.Interactive.Tests.LanguageServices
         }
 
         [Theory]
-        [InlineData("[|#!t|]", "#!two,#!twilight", null)]
-        [InlineData("[|#!tw|]\n", "#!two,#!twilight", null)]
-        [InlineData("[|#!t|]", "#!two,#!twilight", Language.CSharp)]
-        [InlineData("[|#!tw|]\n", "#!two,#!twilight", Language.CSharp)]
-        [InlineData("[|#!t|]", "#!two,#!twilight", Language.FSharp)]
-        [InlineData("[|#!tw|]\n", "#!two,#!twilight", Language.FSharp)]
-        [InlineData("[|#!t|]", "#!two,#!twilight", Language.PowerShell)]
-        [InlineData("[|#!tw|]\n", "#!two,#!twilight", Language.PowerShell)]
-        public async Task Completions_are_available_for_magic_commands_added_at_runtime(
+        [InlineData("[|#!d|]", "#!directiveOnParent", null)]
+        [InlineData("[|#!dir|]\n", "#!directiveOnParent", null)]
+        [InlineData("[|#!d|]", "#!directiveOnChild,#!directiveOnParent", Language.CSharp)]
+        [InlineData("[|#!dir|]\n", "#!directiveOnChild,#!directiveOnParent", Language.CSharp)]
+        [InlineData("[|#!d|]", "#!directiveOnChild,#!directiveOnParent", Language.FSharp)]
+        [InlineData("[|#!dir|]\n", "#!directiveOnChild,#!directiveOnParent", Language.FSharp)]
+        [InlineData("[|#!d|]", "#!directiveOnChild,#!directiveOnParent", Language.PowerShell)]
+        [InlineData("[|#!dir|]\n", "#!directiveOnChild,#!directiveOnParent", Language.PowerShell)]
+        public async Task Completions_are_available_for_magic_commands_added_at_runtime_to_child_and_parent_kernels(
             string markupCode,
             string expected,
-            Language? language)
+            Language? childKernel)
         {
             MarkupTestFile.GetSpan(markupCode, out var code, out var span);
 
             var sourceText = SourceText.From(code);
 
-            var kernel = CreateKernel(language ?? Language.CSharp);
+            var kernel = CreateKernel(childKernel ?? Language.CSharp);
 
             using var _ = new AssertionScope();
 
             _output.WriteLine($"Checking positions from {span.Start} to {span.End}");
 
-            KernelBase kernelToExtend = kernel;
-            if (language != null)
+           
+            if (childKernel != null)
             {
-                kernelToExtend = kernel.FindKernel(language.Value.LanguageName()) as KernelBase;
+                var kernelToExtend = kernel.FindKernel(childKernel.Value.LanguageName()) as KernelBase;
+                
+                kernelToExtend?.AddDirective(new Command("#!directiveOnChild")
+                {
+                    Handler = CommandHandler.Create(() => { })
+                });
             }
 
-            kernelToExtend.AddDirective(new Command("#!two")
-            {
-                Handler = CommandHandler.Create(() => { })
-            });
 
-
-            kernel.AddDirective(new Command("#!twilight")
+            kernel.AddDirective(new Command("#!directiveOnParent")
             {
                 Handler = CommandHandler.Create(() => { })
             });
