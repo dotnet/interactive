f753243493e585eebb1ebc28053a1eef1ad39119
Justin Chen <t-chenju@microsoft.com>
2020-06-19 11:23:25 -0700

    reverted changes, moved back to OutDisplayCommand

--- src/Microsoft.DotNet.Interactive.PowerShell/PowerShellKernel.cs
+++ src/Microsoft.DotNet.Interactive.PowerShell/PowerShellKernel.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Management.Automation.Language;
@@ -11,7 +10,6 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Events;
-using Microsoft.DotNet.Interactive.Formatting;
 using Microsoft.DotNet.Interactive.LanguageService;
 using Microsoft.DotNet.Interactive.PowerShell.Host;
 using Microsoft.PowerShell;
@@ -72,32 +70,6 @@ namespace Microsoft.DotNet.Interactive.PowerShell
             // Add accelerators that exist in other namespaces.
             addAccelerator.Invoke(null, new object[] { "Layout", typeof(Layout.Layout) });
             addAccelerator.Invoke(null, new object[] { "Chart", typeof(Chart) });
-
-            //TODO: Is this declared anywhere else?
-            string[] mimeTypes = new string[] { HtmlFormatter.MimeType, JsonFormatter.MimeType, PlainTextFormatter.MimeType };
-
-            foreach (string mt in mimeTypes)
-            {
-                Formatter<PSObject>.Register((psObj, writer) =>
-                {
-                    object obj;
-                    if (psObj.BaseObject is PSCustomObject)
-                    {
-                        Dictionary<string, object> table = new Dictionary<string, object>();
-                        var props = psObj.Properties;
-                        foreach (var p in props)
-                        {
-                            table.Add(p.Name, p.Value);
-                        }
-                        obj = table;
-                    }
-                    else
-                    {
-                        obj = psObj.BaseObject;
-                    }
-                    obj.FormatTo(writer, mt);
-                }, mimeType: mt);
-            }
         }
 
         public PowerShellKernel() : base(DefaultKernelName)
