555de05cb0c2fb86a454d4483a7768461b06391f
Justin Chen <t-chenju@microsoft.com>
2020-06-24 17:02:03 -0700

    removed unnecessary using

--- src/Microsoft.DotNet.Interactive.PowerShell/Commands/OutDisplayCommand.cs
+++ src/Microsoft.DotNet.Interactive.PowerShell/Commands/OutDisplayCommand.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Microsoft.DotNet.Interactive.Events;
 using System.Management.Automation;
-using System.Collections.Generic;
 using static Microsoft.DotNet.Interactive.Kernel;
 
 namespace Microsoft.DotNet.Interactive.PowerShell.Commands
