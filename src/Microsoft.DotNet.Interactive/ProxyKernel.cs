f22efde6a8d5d067329e7f1cb68c0da5c2cf6710
Diego <dicolomb@microsoft.com>
2020-06-26 12:58:22 +0100

    restructure proxy kernel

--- src/Microsoft.DotNet.Interactive/ProxyKernel.cs
+++ src/Microsoft.DotNet.Interactive/ProxyKernel.cs
@@ -1,12 +1,41 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Server;
+
 namespace Microsoft.DotNet.Interactive
 {
-    internal abstract class ProxyKernel : Kernel
+    public sealed class ProxyKernel : Kernel, IKernelCommandHandler<SubmitCode>, IKernelCommandHandler<RequestCompletions>
     {
-        public ProxyKernel(string name) : base(name)
+        private readonly KernelClient _client;
+
+        public ProxyKernel(string name, KernelClient client) : base(name)
         {
+            _client = client;
+            RegisterForDisposal(client);
+            RegisterForDisposal(() =>
+            {
+                client.KernelEvents.Subscribe(OnKernelEvents);
+            });
+        }
+
+        private void OnKernelEvents(KernelEvent kernelEvent)
+        {
+            PublishEvent(kernelEvent);
+        }
+
+        public Task HandleAsync(SubmitCode command, KernelInvocationContext context)
+        {
+            return _client.SendAsync(command);
+        }
+
+        public Task HandleAsync(RequestCompletions command, KernelInvocationContext context)
+        {
+            return _client.SendAsync(command);
         }
     }
 }
