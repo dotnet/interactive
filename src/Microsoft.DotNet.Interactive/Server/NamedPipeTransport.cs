d57f4b14820e5081b4868c7c03ef518583e95d74
Diego <dicolomb@microsoft.com>
2020-06-26 12:41:31 +0100

    remove namedPipe kernel server and consolidate stream abstractions

---
+++ src/Microsoft.DotNet.Interactive/Server/NamedPipeTransport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace Microsoft.DotNet.Interactive.Server
+{
+    public static class NamedPipeTransport
+    {
+        public static KernelServer CreateServer(Kernel kernel, NamedPipeServerStream server)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException(nameof(kernel));
+            }
+            var input = new PipeStreamInputStream(server);
+            var output = new PipeStreamOutputStream(server);
+            var kernelServer = new KernelServer(kernel, input, output);
+            kernel.RegisterForDisposal(kernelServer);
+            return kernelServer;
+        }
+
+        public static KernelClient CreateClient(Kernel kernel, NamedPipeClientStream remote)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException(nameof(kernel));
+            }
+
+            if (remote == null)
+            {
+                throw new ArgumentNullException(nameof(remote));
+            }
+
+            var input = new PipeStreamInputStream(remote);
+            var output = new PipeStreamOutputStream(remote);
+            var kernelClient = new KernelClient(input, output);
+            kernel.RegisterForDisposal(kernelClient);
+            return kernelClient;
+        }
+    }
+}
