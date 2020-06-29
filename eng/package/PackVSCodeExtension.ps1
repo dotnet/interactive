83bfd8f79a1f8aae5d0b50ae90127c2990a8acc6
Brett V. Forsgren <brettfo@microsoft.com>
2020-06-29 13:14:02 -0700

    set tool version on package build (#594)

--- eng/package/PackVSCodeExtension.ps1
+++ eng/package/PackVSCodeExtension.ps1
@@ -9,15 +9,25 @@ Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
 try {
-    # set version
+    # get JSON model for package.json
+    Write-Host "Getting JSON package model"
+    $packageJsonPath = Join-Path (Get-Location) "package.json"
+    $packageJsonContents = (Get-Content $packageJsonPath | Out-String | ConvertFrom-Json)
+
+    # set package version
     Write-Host "Setting package version to $stableToolVersionNumber"
-    npm version $stableToolVersionNumber
+    $packageJsonContents.version = $stableToolVersionNumber
+
+    # set tool version
+    Write-Host "Setting tool version to $stableToolVersionNumber"
+    $packageJsonContents.contributes.configuration.properties."dotnet-interactive.minimumInteractiveToolVersion"."default" = $stableToolVersionNumber
 
     # append git sha to package description
-    $packageJsonPath = Join-Path (Get-Location) "package.json"
     Write-Host "Appending git sha to description in $packageJsonPath"
-    $packageJsonContents = (Get-Content $packageJsonPath | Out-String | ConvertFrom-Json)
     $packageJsonContents.description += "  Git SHA $gitSha"
+
+    # write out updated JSON
+    Write-Host "Writing updated package.json"
     $packageJsonContents | ConvertTo-Json -depth 100 | Out-File $packageJsonPath
 
     # create destination
