f387d96130f08b026f531bed2083cf98fc85b1cb
Jon Sequeira <jonsequeira@gmail.com>
2020-07-01 17:44:27 -0700

    fix filenames

---
+++ src/Microsoft.DotNet.Interactive/Connection/NamedPipeConnectionOptions.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+namespace Microsoft.DotNet.Interactive.Connection
+{
+    public class NamedPipeConnectionOptions : KernelConnectionOptions
+    {
+        public string? PipeName { get; set; }
+    }
+}
