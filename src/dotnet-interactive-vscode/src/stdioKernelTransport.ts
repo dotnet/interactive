31685fbbb0f0d3717e917cbf47b58c9e680942cb
Diego <colombod@me.com>
2020-09-07 10:04:41 +0100

    add port to diagnostic message

--- src/dotnet-interactive-vscode/src/stdioKernelTransport.ts
+++ src/dotnet-interactive-vscode/src/stdioKernelTransport.ts
@@ -75,7 +75,7 @@ export class StdioKernelTransport {
 
     public async setExternalUri(externalUri: Uri) : Promise<void>
     {
-        this.diagnosticChannel.appendLine(`Tunnel uri for Kernel process ${this.childProcess?.pid} is ${externalUri.toString()}`);
+        this.diagnosticChannel.appendLine(`Tunnel uri for Kernel process ${this.childProcess?.pid} is ${externalUri.toString()} mappint to Kernel Port ${this.httpPort}`);
         this.externalUri = externalUri;
         let apitunnelUri = `${externalUri.toString()}apitunnel`;
         let response = await fetch(apitunnelUri, {
