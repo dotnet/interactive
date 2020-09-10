51670a82896c37025bc97a50a952a9328b3e9753
Cory Rivera <corivera@microsoft.com>
2020-09-10 13:27:14 -0700

    Update a test.

--- src/Microsoft.DotNet.Interactive.Sql.Tests/MsSqlKernelTest.cs
+++ src/Microsoft.DotNet.Interactive.Sql.Tests/MsSqlKernelTest.cs
@@ -30,17 +30,9 @@ namespace Microsoft.DotNet.Interactive.Sql.Tests
             using var kernel = new MsSqlKernel("sql", "");
             using var events = kernel.KernelEvents.ToSubscribedList();
 
-            // just reading expectedType to avoid error
-            if (expectedType == null)
-            {
-            }
-
-            // Set connection context
-
-
             await kernel.SubmitCodeAsync(code);
 
-            events.Should().ContainSingle<DisplayedValueProduced>().Which.Value.Should().BeOfType<string>().Which.Should().Be("HELLO WORLD");
+            events.Should().ContainSingle<DisplayedValueProduced>().Which.Value.Should().BeOfType(expectedType).And.Subject.Should().Be("HELLO WORLD");
         }
         
         [Fact]
@@ -51,12 +43,14 @@ namespace Microsoft.DotNet.Interactive.Sql.Tests
             using var events = kernel.KernelEvents.ToSubscribedList();
 
             var testUri = "connection://test";
-
             var connectResult = await kernel.ConnectAsync(testUri);
-            var completionItemsResult = await kernel.ProvideCompletionItemsAsync();
+            Assert.True(connectResult, "Connect should succeed.");
 
+            var completionItemsResult = await kernel.ProvideCompletionItemsAsync();
             Assert.True(completionItemsResult != null, "Completion list should not be null");
 
+            var disconnectResult = await kernel.DisconnectAsync(testUri);
+            Assert.True(disconnectResult, "Disconnect should succeed.");
         }
         [Fact]
         public async Task ConnectionTestAsync()
@@ -81,6 +75,9 @@ namespace Microsoft.DotNet.Interactive.Sql.Tests
             var subsetResults = await kernel.ExecuteQueryExecuteSubsetAsync(testUri);
             Assert.True(subsetResults.ResultSubset.RowCount == 1, "Row count should not be 0");
             Assert.True(subsetResults.ResultSubset.Rows[0][0].DisplayValue == "HELLO WORLD", "Display value does not match");
+
+            var disconnectResult = await kernel.DisconnectAsync(testUri);
+            Assert.True(disconnectResult, "Disconnect should succeed.");
         }
     }
 }
