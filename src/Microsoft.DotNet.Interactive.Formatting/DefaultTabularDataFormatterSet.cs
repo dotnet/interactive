e9c673e055dd682a19c4e1889da5f8670f583d90
Diego <colombod@me.com>
2020-09-17 12:27:27 +0100

    fix formatter for dictionaries

--- src/Microsoft.DotNet.Interactive.Formatting/DefaultTabularDataFormatterSet.cs
+++ src/Microsoft.DotNet.Interactive.Formatting/DefaultTabularDataFormatterSet.cs
@@ -2,16 +2,14 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections;
-using System.Collections.Generic;
 
 namespace Microsoft.DotNet.Interactive.Formatting
 {
     internal static class DefaultTabularDataFormatterSet
     {
         internal static readonly ITypeFormatter[] DefaultFormatters =
-            new ITypeFormatter[]
             {
-                new TabularDataFormatter<IEnumerable<object>>((context, source, writer) =>
+                new TabularDataFormatter<IEnumerable>((context, source, writer) =>
                 {
                     var tabularData = source.ToTabularJsonString();
                         writer.Write(tabularData.ToString());
