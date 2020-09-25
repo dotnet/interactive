313364c68e1e044f58e4a0c1b9daa109b74533c2
Diego Colombo <dicolomb@microsoft.com>
2020-09-25 15:35:06 +0100

    tests

---
+++ src/Microsoft.DotNet.Interactive.Http/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Microsoft.DotNet.Interactive.Http
+{
+    public class Server : IDisposable
+    {
+        private readonly Kernel _kernel;
+
+        public Server(Kernel kernel)
+        {
+            this.kernel = kernel;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
