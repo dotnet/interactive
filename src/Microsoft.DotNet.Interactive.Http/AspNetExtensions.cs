4c4a06ebc23b9ed62e208ac9cd42f9584837d5d7
Diego <colombod@me.com>
2020-09-29 13:55:31 +0100

    formatting

--- src/Microsoft.DotNet.Interactive.Http/AspNetExtensions.cs
+++ src/Microsoft.DotNet.Interactive.Http/AspNetExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reflection;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.DotNet.Interactive.Commands;
