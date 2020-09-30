7d302edf3f517ff729a6006386a7021cddcc9eb0
Diego <colombod@me.com>
2020-09-30 10:14:06 +0100

    attribute to skip tests if the mssql is not present

---
+++ src/Microsoft.DotNet.Interactive.ExtensionLab.Tests/MSSQLFact.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Data.SqlClient;
+
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.ExtensionLab.Tests
+{
+    public sealed class MSSQLFact : FactAttribute
+    {
+        public MSSQLFact(string connectionString)
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Dispose();
+            }
+            catch
+            {
+                Skip = "Required connection cannot be found or established";
+            }
+        }
+    }
+}
