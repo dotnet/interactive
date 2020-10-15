936e2ffa26bd323e0382d3918cae1010b19dbcb6
Diego <dicolomb@microsoft.com>
2020-10-15 14:09:30 +0100

    simplified loading csv from string

--- src/Microsoft.DotNet.Interactive.ExtensionLab.Tests/DataFrameKernelExtensionTests.cs
+++ src/Microsoft.DotNet.Interactive.ExtensionLab.Tests/DataFrameKernelExtensionTests.cs
@@ -37,13 +37,10 @@ namespace Microsoft.DotNet.Interactive.ExtensionLab.Tests
 
             await kernelExtension.OnLoadAsync(kernel);
 
-            using var stream = new MemoryStream();
-            using var writer = new StreamWriter(stream);
-            writer.AutoFlush = true;
-            writer.Write(@"id,name,color,deliciousness
+            using var stream = @"id,name,color,deliciousness
 1,apple,green,10
 2,banana,yellow,11
-3,cherry,red,9000");
+3,cherry,red,9000".ToStream();
             stream.Position = 0;
 
             var dataframe = DataFrame.LoadCsv(stream);
