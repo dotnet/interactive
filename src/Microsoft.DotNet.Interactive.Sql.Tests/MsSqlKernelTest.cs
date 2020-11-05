dc879505f6bc6013e0504197be434471dda9dbdc
Cory Rivera <corivera@microsoft.com>
2020-11-05 00:23:24 -0800

    Remove test warning suppressions.

--- src/Microsoft.DotNet.Interactive.Sql.Tests/MsSqlKernelTest.cs
+++ src/Microsoft.DotNet.Interactive.Sql.Tests/MsSqlKernelTest.cs
@@ -1,19 +1,12 @@
 using System;
-using System.IO;
-using System.Management.Automation;
 using System.Threading.Tasks;
 using FluentAssertions;
-using System.Linq;
-using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.ExtensionLab;
 using Microsoft.DotNet.Interactive.Tests;
 using Microsoft.DotNet.Interactive.Tests.Utility;
-using XPlot.Plotly;
 using Xunit;
 using Xunit.Abstractions;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Microsoft.DotNet.Interactive.ExtensionLab;
 
 namespace Microsoft.DotNet.Interactive.Sql.Tests
 {
@@ -25,7 +18,7 @@ namespace Microsoft.DotNet.Interactive.Sql.Tests
 
         [Theory]
         [InlineData(@"SELECT 'HELLO WORLD'", typeof(string))]
-        public async Task HellowWorldEventTest(string code, Type expectedType)
+        public async Task HellowWorldEventTestAsync(string code, Type expectedType)
         {
             using var kernel = new MsSqlKernel("sql", "");
             using var events = kernel.KernelEvents.ToSubscribedList();
