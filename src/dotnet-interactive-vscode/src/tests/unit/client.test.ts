b3c931a437bf26ce372f94c061a137230549e3ec
Diego <dicolomb@microsoft.com>
2020-11-10 22:07:01 +0000

    debounce diagnostics request on execute

--- src/dotnet-interactive-vscode/src/tests/unit/client.test.ts
+++ src/dotnet-interactive-vscode/src/tests/unit/client.test.ts
@@ -7,6 +7,7 @@ import { ClientMapper } from '../../clientMapper';
 import { TestKernelTransport } from './testKernelTransport';
 import { CellOutput, CellOutputKind } from '../../interfaces/vscode';
 import { CodeSubmissionReceivedType, CompleteCodeSubmissionReceivedType, CommandSucceededType, DisplayedValueProducedType, ReturnValueProducedType, DisplayedValueUpdatedType, CommandFailedType } from '../../contracts';
+import { debounce, wait } from '../../utilities';
 
 describe('InteractiveClient tests', () => {
     it('command execution returns deferred events', async () => {
@@ -66,7 +67,7 @@ describe('InteractiveClient tests', () => {
         }));
         let client = await clientMapper.getOrAddClient({ fsPath: 'test/path' });
         let result: Array<CellOutput> = [];
-        await client.execute(code, 'csharp', outputs => result = outputs, _ => { }, token);
+        await client.execute("id0", code, 'csharp', outputs => result = outputs, _ => { }, token);
         expect(result).to.deep.equal([
             {
                 outputKind: CellOutputKind.Rich,
@@ -140,7 +141,7 @@ describe('InteractiveClient tests', () => {
         }));
         let client = await clientMapper.getOrAddClient({ fsPath: 'test/path' });
         let result: Array<CellOutput> = [];
-        await client.execute(code, 'csharp', outputs => result = outputs, _ => { }, token);
+        await client.execute("id0", code, 'csharp', outputs => result = outputs, _ => { }, token);
         expect(result).to.deep.equal([
             {
                 outputKind: CellOutputKind.Rich,
@@ -229,7 +230,7 @@ describe('InteractiveClient tests', () => {
         }));
         let client = await clientMapper.getOrAddClient({ fsPath: 'test/path' });
         let result: Array<CellOutput> = [];
-        await client.execute(code, 'csharp', outputs => result = outputs, _ => { }, token);
+        await client.execute("id0", code, 'csharp', outputs => result = outputs, _ => { }, token);
         expect(result).to.deep.equal([
             {
                 outputKind: CellOutputKind.Rich,
@@ -302,7 +303,7 @@ describe('InteractiveClient tests', () => {
 
         // execute first command
         let result1: Array<CellOutput> = [];
-        await client.execute(code, 'csharp', outputs => result1 = outputs, _ => { }, 'token 1');
+        await client.execute("id0", code, 'csharp', outputs => result1 = outputs, _ => { }, 'token 1');
         expect(result1).to.deep.equal([
             {
                 outputKind: CellOutputKind.Rich,
@@ -314,7 +315,7 @@ describe('InteractiveClient tests', () => {
 
         // execute second command
         let result2: Array<CellOutput> = [];
-        await client.execute(code, 'csharp', outputs => result2 = outputs, _ => { }, 'token 2');
+        await client.execute("id0", code, 'csharp', outputs => result2 = outputs, _ => { }, 'token 2');
         expect(result2).to.deep.equal([]);
 
         // ensure first result array was updated
@@ -340,7 +341,7 @@ describe('InteractiveClient tests', () => {
             ]
         }));
         clientMapper.getOrAddClient({ fsPath: 'test/path' }).then(client => {
-            client.execute('bad-code-that-will-fail', 'csharp', _ => { }, _ => { }, token).then(result => {
+            client.execute("id0", 'bad-code-that-will-fail', 'csharp', _ => { }, _ => { }, token).then(result => {
                 done(`expected execution to fail promise, but passed with: ${result}`);
             }).catch(_err => {
                 done();
@@ -364,5 +365,30 @@ describe('InteractiveClient tests', () => {
                 done();
             });
         });
+    });
+
+    it('execution prevents diagnostics request forwarding', async () => {
+        let token = 'test-token';
+
+        let clientMapper = new ClientMapper(async (notebookPath) => new TestKernelTransport({
+            'SubmitCode': [
+
+                {
+                    eventType: CommandSucceededType,
+                    event: {},
+                    token
+                }
+            ]
+        }));
+
+        let diagnosticsCallbackFired = false;
+        debounce("id0", 500, () => {
+            diagnosticsCallbackFired = true;
+        });
+
+        const client = await clientMapper.getOrAddClient({ fsPath: 'test-path.dib' });
+        await client.execute("id0", "1+1", "csharp", (_outputs) => { }, (_diagnostics) => { }, token);
+        await wait(1000);
+        expect(diagnosticsCallbackFired).to.be.false;
     });
 });
