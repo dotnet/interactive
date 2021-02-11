79eb08ffded686a6209ab6550659e0b50ddd53be
Brett V. Forsgren <brettfo@microsoft.com>
2021-02-11 13:00:59 -0800

    parse arrays and objects in bare JSON

--- src/Microsoft.DotNet.Interactive/Notebook/DataDictionaryConverter.cs
+++ src/Microsoft.DotNet.Interactive/Notebook/DataDictionaryConverter.cs
@@ -12,7 +12,7 @@ namespace Microsoft.DotNet.Interactive.Notebook
     {
         public override Dictionary<string, object> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            EnsureStartObject(reader,typeToConvert);
+            EnsureStartObject(reader, typeToConvert);
 
             var value = new Dictionary<string, object>();
 
@@ -27,33 +27,61 @@ namespace Microsoft.DotNet.Interactive.Notebook
                 object itemValue;
 
                 reader.Read();
-
-                switch (reader.TokenType)
-                {
-                    case JsonTokenType.String:
-                        itemValue = reader.GetString();
-                        break;
-                    case JsonTokenType.Number:
-                        itemValue = reader.GetDouble();
-                        break;
-                    case JsonTokenType.True:
-                        itemValue = true;
-                        break;
-                    case JsonTokenType.False:
-                        itemValue = false;
-                        break;
-                    case JsonTokenType.Null:
-                        itemValue = null;
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
+                itemValue = GetValue(ref reader, options);
 
                 value.Add(keyString, itemValue);
             }
 
             throw new JsonException($"Cannot deserialize {typeToConvert.Name}");
+        }
+
+        private static object GetValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            object itemValue;
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    itemValue = reader.GetString();
+                    break;
+                case JsonTokenType.Number:
+                    itemValue = reader.GetDouble();
+                    break;
+                case JsonTokenType.True:
+                    itemValue = true;
+                    break;
+                case JsonTokenType.False:
+                    itemValue = false;
+                    break;
+                case JsonTokenType.Null:
+                    itemValue = null;
+                    break;
+                case JsonTokenType.StartArray:
+                    itemValue = ParseArray(ref reader, options);
+                    break;
+                case JsonTokenType.StartObject:
+                    itemValue = JsonSerializer.Deserialize(ref reader, typeof(Dictionary<string, object>), options);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return itemValue;
+        }
+
+        private static object ParseArray(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            var values = new List<object>();
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return values.ToArray();
+                }
+
+                values.Add(GetValue(ref reader, options));
+            }
+
+            throw new JsonException("Missing end of array.");
         }
     }
 }
