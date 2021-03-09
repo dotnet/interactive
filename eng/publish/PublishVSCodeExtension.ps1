3b5579e82432d65a5bc77216aff32b8c69a69748
Brett V. Forsgren <brettfo@microsoft.com>
2021-03-09 09:50:52 -0800

    allow publishing multiple extensions with one invocation (#1151)

--- eng/publish/PublishVSCodeExtension.ps1
+++ eng/publish/PublishVSCodeExtension.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding(PositionalBinding = $false)]
 param (
     [string]$artifactsPath,
-    [string]$vscodeTarget,
+    [string[]]$vscodeTargets,
     [string]$publishToken
 )
 
@@ -9,18 +9,26 @@ Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
 try {
-    # find extension vsix
-    $extension = Get-ChildItem "$artifactsPath\$vscodeTarget\dotnet-interactive-vscode-*.vsix" | Select-Object -First 1
-
-    # verify
-    . "$PSScriptRoot\VerifyVSCodeExtension.ps1" -extensionPath $extension
+    npm install -g vsce
     if ($LASTEXITCODE -ne 0) {
         exit $LASTEXITCODE
     }
 
-    # publish
-    npm install -g vsce
-    vsce publish --packagePath $extension --pat $publishToken --noVerify
+    $vscodeTargets | ForEach-Object {
+        $vscodeTarget = $_
+
+        # find extension vsix
+        $extension = Get-ChildItem "$artifactsPath\$vscodeTarget\dotnet-interactive-vscode-*.vsix" | Select-Object -First 1
+
+        # verify
+        . "$PSScriptRoot\VerifyVSCodeExtension.ps1" -extensionPath $extension
+        if ($LASTEXITCODE -ne 0) {
+            exit $LASTEXITCODE
+        }
+
+        # publish
+        vsce publish --packagePath $extension --pat $publishToken --noVerify
+    }
 }
 catch {
     Write-Host $_
