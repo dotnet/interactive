de7d0f1ea11ea76e3d0af828910ef05b19ed154d
Diego Colombo <dicolomb@microsoft.com>
2021-03-11 13:00:01 +0000

    define controller for widget

--- src/Microsoft.DotNet.Interactive.SandDance.js/src/dataTypes.ts
+++ src/Microsoft.DotNet.Interactive.SandDance.js/src/dataTypes.ts
@@ -2,18 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 export interface Data {
-    schema: Schema;
+    schema: TableSchema;
     data: Datapoint[];
 }
-export interface Schema {
-    fields: Field[];
+export interface TableSchema {
+    fields: TableSchemaFieldDescriptor[];
     pandas_version?: string;
     primaryKey?: string[];
 }
 
-export interface Field {
+export interface TableSchemaFieldDescriptor {
     name: string;
     type: string;
+    description?: string;
+    format?: string;
 }
 
 export interface Datapoint {
@@ -22,6 +24,7 @@ export interface Datapoint {
 
 // this type follow the schema published at https://specs.frictionlessdata.io//tabular-data-resource/
 export interface TabularDataResource {
+    profile: string;
     schema: {
         fields: Array<{ name: string, type: string }>,
         primaryKey: string
