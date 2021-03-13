179f77754e18d02466dbd0839c289fb01054fffe
Brett V. Forsgren <brettfo@microsoft.com>
2021-03-12 17:45:30 -0800

    create npm package during build (#1160)
    Creates an NPM package from our VS Code extension sources, excluding the vscode-specific directory.  Most of this
    was simply setting up a package.json and ensuring it gets built during CI.  Eventually we'll publish this package
    to npmjs.com when appropriate, but for now an artifact named rtifacts/npm/dotnet-interactive-1.0.123456.tgz will
    be created.

--- eng/package/PackVSCodeExtension.ps1
+++ eng/package/PackVSCodeExtension.ps1
@@ -8,35 +8,22 @@ param (
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
-function Build-Extension([string] $packageDirectory, [string] $packageVersionNumber, [string] $kernelVersionNumber) {
+function Build-VsCodeExtension([string] $packageDirectory, [string] $packageVersionNumber, [string] $kernelVersionNumber) {
     Push-Location $packageDirectory
 
-    # get JSON model for package.json
-    Write-Host "Getting JSON package model"
     $packageJsonPath = Join-Path (Get-Location) "package.json"
-    $packageJsonContents = (Get-Content $packageJsonPath | Out-String | ConvertFrom-Json)
-
-    # set package version
-    Write-Host "Setting package version to $packageVersionNumber"
-    $packageJsonContents.version = $packageVersionNumber
+    $packageJsonContents = ReadJson -packageJsonPath $packageJsonPath
+    SetNpmVersionNumber -packageJsonContents $packageJsonContents -packageVersionNumber $packageVersionNumber
+    AddGitShaToDescription -packageJsonContents $packageJsonContents -gitSha $gitSha
 
     # set tool version
     Write-Host "Setting tool version to $kernelVersionNumber"
     $packageJsonContents.contributes.configuration.properties."dotnet-interactive.minimumInteractiveToolVersion"."default" = $kernelVersionNumber
 
-    # append git sha to package description
-    Write-Host "Appending git sha to description in $packageJsonPath"
-    $packageJsonContents.description += "  Git SHA $gitSha"
-
-    # write out updated JSON
-    Write-Host "Writing updated package.json"
-    $packageJsonContents | ConvertTo-Json -depth 100 | Out-File $packageJsonPath
+    SaveJson -packageJsonPath $packagejsonPath -packageJsonContents $packageJsonContents
 
     # create destination
-    New-Item -Path "$outDir\$packageDirectory" -ItemType Directory
-
-    # copy publish scripts
-    Copy-Item -Path $PSScriptRoot\..\publish\* -Destination $outDir -Recurse
+    EnsureCleanDirectory -location "$outDir\$packageDirectory"
 
     # pack
     Write-Host "Packing extension"
@@ -46,10 +33,16 @@ function Build-Extension([string] $packageDirectory, [string] $packageVersionNum
 }
 
 try {
+    . "$PSScriptRoot\PackUtilities.ps1"
+
+    # copy publish scripts
+    EnsureCleanDirectory -location $outDir
+    Copy-Item -Path $PSScriptRoot\..\publish\* -Destination $outDir -Recurse
+
     $stablePackageVersion = "${stableToolVersionNumber}0"
     $insidersPackageVersion = "${stableToolVersionNumber}1"
-    Build-Extension -packageDirectory "stable" -packageVersionNumber $stablePackageVersion -kernelVersionNumber $stableToolVersionNumber
-    Build-Extension -packageDirectory "insiders" -packageVersionNumber $insidersPackageVersion -kernelVersionNumber $stableToolVersionNumber
+    Build-VsCodeExtension -packageDirectory "stable" -packageVersionNumber $stablePackageVersion -kernelVersionNumber $stableToolVersionNumber
+    Build-VsCodeExtension -packageDirectory "insiders" -packageVersionNumber $insidersPackageVersion -kernelVersionNumber $stableToolVersionNumber
 }
 catch {
     Write-Host $_
