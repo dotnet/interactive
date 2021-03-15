3f62eb9664a795bc16e9ac7da03d1c48b3e4f2c2
Brett V. Forsgren <brettfo@microsoft.com>
2021-03-15 11:44:24 -0700

    pass reference assemblies to interactive workspace to enable bcl doc comments

--- src/Microsoft.DotNet.Interactive.CSharp/InteractiveWorkspace.cs
+++ src/Microsoft.DotNet.Interactive.CSharp/InteractiveWorkspace.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reactive.Disposables;
 
 using Microsoft.CodeAnalysis;
@@ -21,6 +24,8 @@ namespace Microsoft.DotNet.Interactive.CSharp
         private readonly CSharpParseOptions _parseOptions;
         private Compilation _currentCompilation;
         private DocumentId _workingDocumentId;
+        private readonly IReadOnlyCollection<MetadataReference> _referenceAssemblies;
+        private readonly List<MetadataReference> _packageManagerReferences = new List<MetadataReference>();
 
         public InteractiveWorkspace() : base(MefHostServices.DefaultHost, WorkspaceKind.Interactive)
         {
@@ -29,10 +34,65 @@ namespace Microsoft.DotNet.Interactive.CSharp
                 DocumentationMode.Parse,
                 SourceCodeKind.Script);
 
+            _referenceAssemblies = ResolveRefAssemblies();
+
             _disposables.Add(Disposable.Create(() =>
             {
                 _currentCompilation = null;
             }));
+        }
+
+        private static string ResolveRefAssemblyPath()
+        {
+            var runtimeDir = Path.GetDirectoryName(typeof(object).Assembly.Location);
+            var refAssemblyDir = runtimeDir; // if any of the below path probing fails, fall back to the runtime so we can still run
+
+            // e.g., will transform
+            //   `C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.3`
+            // to
+            //   `C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\5.0.0\ref\net5.0`
+            if (Version.TryParse(Path.GetFileName(runtimeDir), out var runtimeVersion))
+            {
+                var appRefDir = Path.Combine(runtimeDir, "..", "..", "..", "packs", "Microsoft.NETCore.App.Ref");
+                if (Directory.Exists(appRefDir))
+                {
+                    var latestRuntimeDirAndVersion =
+                        Directory.GetDirectories(appRefDir)
+                        .Select(dir => Path.GetFileName(dir))
+                        .Select(dir => new { Directory = dir, Version = Version.TryParse(dir, out var version) ? version : new Version() })
+                        .OrderBy(dirPair => dirPair.Version)
+                        .Where(dirPair => dirPair.Version <= runtimeVersion)
+                        .LastOrDefault();
+                    if (latestRuntimeDirAndVersion is { })
+                    {
+                        var refVersion = latestRuntimeDirAndVersion.Directory; // e.g., `5.0.0`
+                        var tfmName = $"net{latestRuntimeDirAndVersion.Version.Major}.{latestRuntimeDirAndVersion.Version.Minor}"; // e.g., `net5.0`
+                        refAssemblyDir = Path.Combine(appRefDir, refVersion, "ref", tfmName);
+                    }
+                }
+            }
+
+            return refAssemblyDir;
+        }
+
+        private static IReadOnlyCollection<MetadataReference> ResolveRefAssemblies()
+        {
+            var assemblyRefs = new List<MetadataReference>();
+            foreach (var assemblyRef in Directory.EnumerateFiles(ResolveRefAssemblyPath(), "*.dll"))
+            {
+                try
+                {
+                    var resolved = CachingMetadataResolver.ResolveReferenceWithXmlDocumentationProvider(assemblyRef, MetadataReferenceProperties.Assembly);
+                    assemblyRefs.Add(resolved);
+                }
+                catch (Exception ex) when (ex is ArgumentNullException || ex is ArgumentException || ex is IOException)
+                {
+                    // the only exceptions that can be thrown by `ResolveReferenceWithXmlDocumentationProvider` which
+                    // internally calls `XmlDocumentationProvider.CreateFromFile`
+                }
+            }
+
+            return assemblyRefs;
         }
 
         public void UpdateWorkspace(ScriptState scriptState)
@@ -59,6 +119,16 @@ namespace Microsoft.DotNet.Interactive.CSharp
             return solution;
         }
 
+        private IEnumerable<MetadataReference> GetReferenceSet(Compilation compilation)
+        {
+            var references =
+                _referenceAssemblies
+                .Concat(_packageManagerReferences)
+                .Concat(compilation.DirectiveReferences)
+                .ToArray();
+            return references;
+        }
+
         private (ProjectId projectId, DocumentId workingDocumentId) AddNewWorkingProjectToSolution(Compilation previousCompilation, ProjectId projectReferenceProjectId)
         {
             var solution = CurrentSolution;
@@ -67,12 +137,15 @@ namespace Microsoft.DotNet.Interactive.CSharp
 
             var projectId = ProjectId.CreateNewId(debugName: $"workingProject{assemblyName}");
 
+            var references = GetReferenceSet(previousCompilation);
+
             solution = CreateProjectAndAddToSolution(
-                projectId, 
-                assemblyName, 
-                compilationOptions, 
+                projectId,
+                assemblyName,
+                compilationOptions,
                 solution,
-                projectReferenceProjectId);
+                projectReferenceProjectId,
+                references);
 
             var workingDocumentId = DocumentId.CreateNewId(
                 projectId);
@@ -140,13 +213,15 @@ namespace Microsoft.DotNet.Interactive.CSharp
                 projectId = ProjectId.CreateNewId(debugName: debugName);
             }
 
+            var references = GetReferenceSet(compilation);
+
             solution = CreateProjectAndAddToSolution(
-                projectId, 
-                assemblyName, 
-                compilationOptions, 
+                projectId,
+                assemblyName,
+                compilationOptions,
                 solution,
-                projectReferenceProjectId, 
-                compilation.References);
+                projectReferenceProjectId,
+                references);
 
             var currentSubmissionDocumentId = DocumentId.CreateNewId(
                 projectId,
@@ -186,6 +261,11 @@ namespace Microsoft.DotNet.Interactive.CSharp
             SetCurrentSolution(solution);
             return workingDocument;
 
+        }
+
+        public void AddPackageManagerReference(MetadataReference reference)
+        {
+            _packageManagerReferences.Add(reference);
         }
 
         protected override void Dispose(bool finalize)
