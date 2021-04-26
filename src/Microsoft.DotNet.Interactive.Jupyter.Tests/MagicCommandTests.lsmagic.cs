59f98f0d840566bb25a228fb891d454429f12ede
WhiteBlackGoose <wbg@angouri.org>
2021-04-26 20:00:23 +0300

    Test on #1288 added

--- src/Microsoft.DotNet.Interactive.Jupyter.Tests/MagicCommandTests.lsmagic.cs
+++ src/Microsoft.DotNet.Interactive.Jupyter.Tests/MagicCommandTests.lsmagic.cs
@@ -1,8 +1,11 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.CommandLine;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.DotNet.Interactive.Commands;
@@ -114,6 +117,24 @@ namespace Microsoft.DotNet.Interactive.Jupyter.Tests
                   .ToDisplayString("text/html")
                   .Should()
                   .NotContain("#!hidden");
+        }
+
+        [Fact]
+        public async Task r_executes_even_with_comment()
+        {
+            // Make sure the considered package gets successfully installed without comments afterwards
+            using var kernel1 = new FSharpKernel().UseNugetDirective();
+            var code1 = new SubmitCode("#r \"nuget:Plotly.NET.Interactive, 2.0.0-beta8\"");
+            var res1 = await kernel1.SendAsync(code1);
+            Expression<Func<KernelEvent, bool>> f1 = ev => ev is DisplayEvent && ((DisplayEvent)ev).ToDisplayString("text/plain").Contains("Installing");
+            res1.KernelEvents.ToSubscribedList().Should().Contain(f1);
+
+            // Now, let us add a comment after the command
+            using var kernel2 = new FSharpKernel().UseNugetDirective();
+            var code2 = new SubmitCode("#r \"nuget:Plotly.NET.Interactive, 2.0.0-beta8\" // ducks best!");
+            var res2 = await kernel2.SendAsync(code2);
+            Expression<Func<KernelEvent, bool>> f2 = ev => ev is DisplayEvent && ((DisplayEvent)ev).ToDisplayString("text/plain").Contains("Installing");
+            res2.KernelEvents.ToSubscribedList().Should().Contain(f2);
         }
     }
 }
