926e2692f85946aaaf201d3956739923f2df0ac1
Jon Sequeira <jonsequeira@gmail.com>
2021-05-17 17:04:58 -0700

    dispose writer when FormatContext owns it

--- src/Microsoft.DotNet.Interactive.Formatting/FormatContext.cs
+++ src/Microsoft.DotNet.Interactive.Formatting/FormatContext.cs
@@ -13,14 +13,21 @@ namespace Microsoft.DotNet.Interactive.Formatting
     public class FormatContext : IDisposable
     {
         private Dictionary<string, IHtmlContent> _requiredContent;
+        private readonly bool _disposeWriter;
 
-        public FormatContext() : this(new StringWriter())
+        public FormatContext() : this(new StringWriter(), true)
         {
         }
 
         public FormatContext(TextWriter writer)
         {
             Writer = writer;
+        }
+        
+        private FormatContext(TextWriter writer, bool disposeWriter)
+        {
+            Writer = writer;
+            _disposeWriter = disposeWriter;
         }
 
         internal int Depth { get; private set; }
@@ -64,6 +71,11 @@ namespace Microsoft.DotNet.Interactive.Formatting
                 }
 
                 _requiredContent = null;
+            }
+
+            if (_disposeWriter)
+            {
+                Writer.Dispose();
             }
         }
     }
