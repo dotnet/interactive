7207f25827a5b227498e636d17a178eab5444dcc
Brett V. Forsgren <brettfo@microsoft.com>
2021-06-03 11:59:29 -0600

    use notebook uri for client mapper

--- src/dotnet-interactive-vscode/common/interactiveNotebook.ts
+++ src/dotnet-interactive-vscode/common/interactiveNotebook.ts
@@ -81,7 +81,7 @@ export function backupNotebook(rawData: Uint8Array, location: string): Promise<N
 
 export function notebookCellChanged(clientMapper: ClientMapper, cellDocument: Document, language: string, diagnosticDelay: number, callback: (diagnostics: Array<Diagnostic>) => void) {
     debounce(`diagnostics-${cellDocument.uri.toString()}`, diagnosticDelay, async () => {
-        const client = await clientMapper.getOrAddClient(cellDocument.uri);
+        const client = await clientMapper.getOrAddClient(cellDocument.notebook?.uri || cellDocument.uri);
         const diagnostics = await client.getDiagnostics(language, cellDocument.getText());
         callback(diagnostics);
     });
