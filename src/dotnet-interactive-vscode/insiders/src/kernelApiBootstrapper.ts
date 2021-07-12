9411236861b38c5eefa881f91a6a18a27fe57fe4
Diego Colombo <dicolomb@microsoft.com>
2021-07-12 14:32:20 +0100

    check for types to be promise completion source

--- src/dotnet-interactive-vscode/insiders/src/kernelApiBootstrapper.ts
+++ src/dotnet-interactive-vscode/insiders/src/kernelApiBootstrapper.ts
@@ -2,8 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import { CompositeKernel } from "./common/interactive/compositeKernel";
-import { GenericTransport } from "./common/interactive/genericTransport";
+import * as genericTransport from "./common/interactive/genericTransport";
 import { JavascriptKernel } from "./common/interactive/javascriptKernel";
+import * as contracts from "./common/interfaces/contracts";
 
 export function configure(global?: any) {
     if (!global) {
@@ -17,14 +18,38 @@ export function configure(global?: any) {
 
     compositeKernel.add(jsKernel, ["js"]);
 
-    const transport = new GenericTransport(
+    let completionSources: (genericTransport.PromiseCompletionSource<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope> | Promise<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope>)[] = [];
+
+    // @ts-ignore
+    onDidReceiveKernelMessage(event => {
+        if (event.envelope) {
+            const envelope = <contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope><any>(event.envelope);
+            let completionSource = completionSources[0];
+            if (genericTransport.isPromiseCompletionSource<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope>(completionSource)) {
+                completionSources.shift();
+                completionSource.resolve(envelope);
+            } else {
+                completionSources.push(Promise.resolve(envelope));
+            }
+        }
+    });
+
+    const transport = new genericTransport.GenericTransport(
         (envelope) => {
             // @ts-ignore
             postKernelMessage({ envelope });
             return Promise.resolve();
         },
         () => {
-            throw new Error("Not Implemented");
+            if (completionSources.length == 0) {
+                completionSources.push(new genericTransport.PromiseCompletionSource<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope>());
+            }
+            if (genericTransport.isPromiseCompletionSource<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope>(completionSources[0])) {
+                return completionSources[0].promise;
+            }
+            else {
+                return <Promise<contracts.KernelCommandEnvelope | contracts.KernelEventEnvelope>>completionSources.shift();
+            }
         }
     );
 
