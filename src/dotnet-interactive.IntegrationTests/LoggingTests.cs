9f3889c3aebfa1a8faeefaf4d3516f94345d45d6
KevinRansom <codecutter@hotmail.com>
2021-07-29 19:16:05 -0700

    Logging tests

--- src/dotnet-interactive.IntegrationTests/LoggingTests.cs
+++ src/dotnet-interactive.IntegrationTests/LoggingTests.cs
@@ -86,5 +86,83 @@ namespace Microsoft.DotNet.Interactive.App.IntegrationTests
             var logFileContents = File.ReadAllText(logFile.FullName);
             logFileContents.Should().Contain("CodeSubmissionReceived: 1+1");
         }
+
+        [IntegrationFact]
+        public async Task Pound_r_nuget_should_produce_full_log()
+        {
+            var expectedLogging = new[]
+            {
+                "Installed Packages,",
+                " - google.protobuf,",
+                " - microsoft.ml,",
+                " - microsoft.ml.cpumath,",
+                " - microsoft.ml.dataview,",
+                " - microsoft.ml.onnxruntime,",
+                " - Microsoft.ML.OnnxTransformer,",
+                " - newtonsoft.json,",
+                " - system.codedom,",
+                " - system.numerics.tensors,"
+            };
+
+            using var logPath = DisposableDirectory.Create();
+
+            _output.WriteLine($"Created log file: {logPath.Directory.FullName}");
+
+            using var outputReceived = new ManualResetEvent(false);
+            var outputLock = new object();
+            var receivedOutput = false;
+            var errorLines = new List<string>();
+            var waitTime = TimeSpan.FromSeconds(10);
+
+            // start as external process
+            var kernelServerProcess = ProcessHelper.Start(
+                command: "dotnet",
+                args: $@"interactive stdio --log-path ""{logPath.Directory.FullName}"" --verbose",
+                workingDir: new DirectoryInfo(Directory.GetCurrentDirectory()),
+                output: _line =>
+                {
+                    lock (outputLock)
+                    {
+                        if (!receivedOutput)
+                        {
+                            receivedOutput = true;
+                            outputReceived.Set();
+                        }
+                    }
+                },
+                error: errorLines.Add);
+
+            // wait for log file to be created
+            var logFile = await logPath.Directory.WaitForFile(
+                              timeout: waitTime,
+                              predicate: _file => true); // any matching file is the one we want
+            errorLines.Should().BeEmpty("there should not be any errors");
+            logFile.Should().NotBeNull($"a log file should have been created at {logFile.FullName}");
+
+            // submit code
+            var code = @"#r ""nuget:Microsoft.ML.OnnxTransformer,1.4.0""";
+            var submissionJson = @"{""token"":""abc"",""commandType"":""SubmitCode"",""command"":{""code"":"""+ code + @""",""submissionType"":0,""targetKernelName"":null}}";
+            await kernelServerProcess.StandardInput.WriteLineAsync(submissionJson);
+            await kernelServerProcess.StandardInput.FlushAsync();
+
+            // wait for output to proceed
+            var gotOutput = outputReceived.WaitOne(timeout: TimeSpan.FromSeconds(4));
+            gotOutput.Should().BeTrue("expected to receive on stdout");
+
+            // kill
+            await Task.Delay(TimeSpan.FromSeconds(4)); // allow logs to be flushed
+            kernelServerProcess.Kill();
+            kernelServerProcess.WaitForExit(2000).Should().BeTrue("process should exit quickly");
+            errorLines.Should().BeEmpty();
+
+            // check log file for expected contents
+            (await logFile.WaitForFileCondition(
+                 timeout: waitTime,
+                 predicate: file => file.Length > 0))
+                .Should()
+                .BeTrue($"expected non-empty log file within {waitTime.TotalSeconds}s");
+            var logFileContents = File.ReadAllText(logFile.FullName);
+            logFileContents.Should().ContainAll (expectedLogging);
+        }
     }
 }
