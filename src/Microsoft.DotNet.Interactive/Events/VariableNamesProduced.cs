8a3854e0e03a3a066a555cf9645abc9ef574e0aa
Diego Colombo <dicolomb@microsoft.com>
2021-09-01 08:14:00 +0100

    wip

---
+++ src/Microsoft.DotNet.Interactive/Events/VariableNamesProduced.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Microsoft.DotNet.Interactive.Commands;
+
+namespace Microsoft.DotNet.Interactive.Events
+{
+    public class ValueNamesProduced : KernelEvent
+    {
+        public IReadOnlyCollection<string> ValueNames { get; }
+
+        public ValueNamesProduced(IReadOnlyCollection<string> valueNames, RequestValueNames command) : base(command)
+        {
+            ValueNames = valueNames;
+        }
+    }
+
+    public class ValueProduced : KernelEvent
+    {
+        [JsonIgnore] public object Value { get; }
+        public IReadOnlyCollection<FormattedValue> FormattedValues { get; }
+
+        public ValueProduced(object value,
+            RequestValue command,
+            IReadOnlyCollection<FormattedValue> formattedValues = null) : base(command)
+        {
+            Value = value;
+            FormattedValues = formattedValues;
+        }
+    }
+
+}
