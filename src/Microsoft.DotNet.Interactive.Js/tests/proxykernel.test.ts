72be6471a24ba03ac240eec3f094dadb97ff91fd
Diego Colombo <dicolomb@microsoft.com>
2021-09-28 13:29:32 +0100

    test proxy kernel

--- src/Microsoft.DotNet.Interactive.Js/tests/proxykernel.test.ts
+++ src/Microsoft.DotNet.Interactive.Js/tests/proxykernel.test.ts
@@ -24,10 +24,69 @@ describe("proxyKernel", () => {
         await kernel.send({ commandType: contracts.SubmitCodeType, command: <contracts.SubmitCode>{ code: "1+2" } })
 
         expect(events[0]).to.include({
-            eventType: 'CommandSucceeded',
+            eventType: contracts.CommandSucceededType,
             command: inMemory.sentItems[0]
         });
 
+    });
+
+    it("procudes commandFailed", async () => {
+        let inMemory = createInMemoryTransport(ce => {
+            return [{ eventType: contracts.CommandFailedType, event: <contracts.CommandFailed>{ message: "something is wrong" }, command: ce }];
+        });
+        let kernel = new ProxyKernel("proxy", inMemory.transport);
+        let events: contracts.KernelEventEnvelope[] = [];
+        inMemory.transport.run();
+        kernel.subscribeToKernelEvents((e) => events.push(e));
+        await kernel.send({ commandType: contracts.SubmitCodeType, command: <contracts.SubmitCode>{ code: "1+2" } })
+
+        expect(events[0]).to.include({
+            eventType: contracts.CommandFailedType,
+            command: inMemory.sentItems[0]
+        });
+    });
+
+    it("forwards events", async () => {
+        let inMemory = createInMemoryTransport(ce => {
+            return [
+                { eventType: contracts.ValueProducedType, event: <contracts.ValueProduced>{ name: "a", formattedValue: { mimeType: "text/plain", value: "variable a" } }, command: ce },
+                { eventType: contracts.ValueProducedType, event: <contracts.ValueProduced>{ name: "b", formattedValue: { mimeType: "text/plain", value: "variable b" } }, command: ce },
+                { eventType: contracts.CommandSucceededType, event: <contracts.CommandSucceeded>{}, command: ce }];
+        });
+        let kernel = new ProxyKernel("proxy", inMemory.transport);
+        let events: contracts.KernelEventEnvelope[] = [];
+        inMemory.transport.run();
+        kernel.subscribeToKernelEvents((e) => events.push(e));
+        await kernel.send({ commandType: contracts.SubmitCodeType, command: <contracts.SubmitCode>{ code: "1+2" } })
+
+        expect(events[0]).to.be.deep.eq({
+            eventType: contracts.ValueProducedType,
+            event: {
+                name: "a",
+                formattedValue: {
+                    mimeType: "text/plain",
+                    value: "variable a"
+                }
+            },
+            command: inMemory.sentItems[0]
+        });
+
+        expect(events[1]).to.be.deep.eq({
+            eventType: contracts.ValueProducedType,
+            event: {
+                name: "b",
+                formattedValue: {
+                    mimeType: "text/plain",
+                    value: "variable b"
+                }
+            },
+            command: inMemory.sentItems[0]
+        });
+
+        expect(events[2]).to.include({
+            eventType: contracts.CommandSucceededType,
+            command: inMemory.sentItems[0]
+        });
     });
 });
 
