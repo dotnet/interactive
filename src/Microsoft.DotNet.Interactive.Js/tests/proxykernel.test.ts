b87b3a38d3910f14a3c24d24bd553de4deb6a16d
Diego Colombo <dicolomb@microsoft.com>
2021-09-28 14:48:54 +0100

    test for commands split on the remote

--- src/Microsoft.DotNet.Interactive.Js/tests/proxykernel.test.ts
+++ src/Microsoft.DotNet.Interactive.Js/tests/proxykernel.test.ts
@@ -88,6 +88,50 @@ describe("proxyKernel", () => {
             command: inMemory.sentItems[0]
         });
     });
+
+    it("forwards events ofremotely split commands", async () => {
+        let inMemory = createInMemoryTransport(ce => {
+            return [
+                { eventType: contracts.ValueProducedType, event: <contracts.ValueProduced>{ name: "a", formattedValue: { mimeType: "text/plain", value: "variable a" } }, command: { ...ce, ["command.id"]: "newId" } },
+                { eventType: contracts.ValueProducedType, event: <contracts.ValueProduced>{ name: "b", formattedValue: { mimeType: "text/plain", value: "variable b" } }, command: { ...ce, ["command.id"]: "newId" } },
+                { eventType: contracts.CommandSucceededType, event: <contracts.CommandSucceeded>{}, command: ce }];
+        });
+        let kernel = new ProxyKernel("proxy", inMemory.transport);
+        let events: contracts.KernelEventEnvelope[] = [];
+        inMemory.transport.run();
+        kernel.subscribeToKernelEvents((e) => events.push(e));
+
+        await kernel.send({ commandType: contracts.SubmitCodeType, command: <contracts.SubmitCode>{ code: "1+2" } })
+
+        expect(events[0]).to.be.deep.eq({
+            eventType: contracts.ValueProducedType,
+            event: {
+                name: "a",
+                formattedValue: {
+                    mimeType: "text/plain",
+                    value: "variable a"
+                }
+            },
+            command: { ...inMemory.sentItems[0], ["command.id"]: "newId" }
+        });
+
+        expect(events[1]).to.be.deep.eq({
+            eventType: contracts.ValueProducedType,
+            event: {
+                name: "b",
+                formattedValue: {
+                    mimeType: "text/plain",
+                    value: "variable b"
+                }
+            },
+            command: { ...inMemory.sentItems[0], ["command.id"]: "newId" }
+        });
+
+        expect(events[2]).to.include({
+            eventType: contracts.CommandSucceededType,
+            command: inMemory.sentItems[0]
+        });
+    });
 });
 
 
