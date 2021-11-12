0ed7a415e8e9c3545dadf93a394c469db1ecfd7a
Diego Colombo <dicolomb@microsoft.com>
2021-11-12 21:14:14 +0000

    move http formatter
    remove pacakge
    
    
    remove
    
    
    fix test
    
    
    ef test
    
    
    fixed tests

--- src/Microsoft.DotNet.Interactive.Http/PublishEventRouter.cs
+++ src/Microsoft.DotNet.Interactive.Http/PublishEventRouter.cs
@@ -8,54 +8,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.DotNet.Interactive.Documents;
-using Microsoft.DotNet.Interactive.Extensions;
-using Microsoft.DotNet.Interactive.Formatting;
 using Microsoft.DotNet.Interactive.Server;
 
-namespace Microsoft.DotNet.Interactive.Http
+namespace Microsoft.DotNet.Interactive.Http;
+
+internal class PublishEventRouter : IRouter
 {
-    internal class PublishEventRouter : IRouter
+    private readonly HtmlNotebookFrontendEnvironment _frontendEnvironment;
+
+    public PublishEventRouter(HtmlNotebookFrontendEnvironment frontendEnvironment)
     {
-        private readonly HtmlNotebookFrontendEnvironment _frontendEnvironment;
+        _frontendEnvironment = frontendEnvironment ?? throw new ArgumentNullException(nameof(frontendEnvironment));
+    }
 
-        public PublishEventRouter(HtmlNotebookFrontendEnvironment frontendEnvironment)
-        {
-            _frontendEnvironment = frontendEnvironment ?? throw new ArgumentNullException(nameof(frontendEnvironment));
-        }
+    public VirtualPathData GetVirtualPath(VirtualPathContext context)
+    {
+        return null;
+    }
 
-        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+    public async Task RouteAsync(RouteContext context)
+    {
+        if (context.HttpContext.Request.Method == HttpMethods.Post)
         {
-            return null;
-        }
-
-        public async Task RouteAsync(RouteContext context)
-        {
-            if (context.HttpContext.Request.Method == HttpMethods.Post)
+            var segments =
+                context.HttpContext
+                    .Request
+                    .Path
+                    .Value
+                    .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (segments.Length == 1 && segments[0] == "publishEvent")
             {
-                var segments =
-                    context.HttpContext
-                       .Request
-                       .Path
-                       .Value
-                       .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-                if (segments.Length == 1 && segments[0] == "publishEvent")
+                using var reader = new StreamReader(context.HttpContext.Request.Body);
+                var requestText = await reader.ReadToEndAsync();
+                var requestObject = JsonDocument.Parse(requestText).RootElement;
+                var commandToken = requestObject.GetPropertyFromPath("commandToken")?.GetString();
+                var eventEnvelopeText = requestObject.GetPropertyFromPath("eventEnvelope")?.GetRawText();
+                _frontendEnvironment.PublishEventFromCommand(commandToken, command =>
                 {
-                    using var reader = new StreamReader(context.HttpContext.Request.Body);
-                    var requestText = await reader.ReadToEndAsync();
-                    var requestObject = JsonDocument.Parse(requestText).RootElement;
-                    var commandToken = requestObject.GetPropertyFromPath("commandToken")?.GetString();
-                    var eventEnvelopeText = requestObject.GetPropertyFromPath("eventEnvelope")?.GetRawText();
-                    _frontendEnvironment.PublishEventFromCommand(commandToken, command =>
+                    context.Handler = async httpContext =>
                     {
-                        context.Handler = async httpContext =>
-                        {
-                            httpContext.Response.StatusCode = 200;
-                            await httpContext.Response.CompleteAsync();
-                        };
-                        var eventEnvelope = KernelEventEnvelope.DeserializeWithCommand(eventEnvelopeText, command);
-                        return eventEnvelope.Event;
-                    });
-                }
+                        httpContext.Response.StatusCode = 200;
+                        await httpContext.Response.CompleteAsync();
+                    };
+                    var eventEnvelope = KernelEventEnvelope.DeserializeWithCommand(eventEnvelopeText, command);
+                    return eventEnvelope.Event;
+                });
             }
         }
     }
