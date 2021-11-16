1127431c465fe72ee0d548474cde9ae89f9aab33
Brett V. Forsgren <brettfo@microsoft.com>
2021-11-16 19:45:58 +0000

    move all npm-related stuff to build scripts

--- eng/build.sh
+++ eng/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 source="${BASH_SOURCE[0]}"
 
 # resolve $source until the file is no longer a symlink
@@ -12,28 +14,61 @@ while [[ -h "$source" ]]; do
 done
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
+# parse args
 args=""
+is_ci=false
+no_dotnet=false
 run_tests=false
 
-# scan for `--test` or `-t`
+# scan for `--test` or `--ci` switches
 while [[ $# > 0 ]]; do
   opt="$(echo "$1" | awk '{print tolower($0)}')"
   case "$opt" in
+    --ci)
+      is_ci=true
+      args="$args --ci"
+      ;;
+    --no-dotnet)
+      no_dotnet=true
+      ;;
     --test|-t)
       run_tests=true
       ;;
+    *)
+      args="$args $1"
+      ;;
   esac
-  args="$args $1"
   shift
 done
 
-function TestUsingNPM() {
-    test_path=$1
-    pushd "$test_path"
-    npm i
+# build and test NPM
+npmDirs='src/dotnet-interactive-npm
+         src/dotnet-interactive-vscode/stable
+         src/dotnet-interactive-vscode/insiders
+         src/Microsoft.DotNet.Interactive.Js
+         src/Microsoft.DotNet.Interactive.Mermaid.js
+         src/Microsoft.DotNet.Interactive.nteract.js
+         src/Microsoft.DotNet.Interactive.SandDance.js'
+for npmDir in $npmDirs;
+do
+  echo "Building NPM in directory $npmDir"
+  pushd $npmDir
+  npm ci
+  npm run compile
+  if [[ "$run_tests" == true ]]; then
+    echo "Testing NPM in directory $npmDir"
     npm run ciTest
-    popd
-}
+  fi
+  popd
+done
 
-# invoke regular build/test script
-. "$scriptroot/common/build.sh" "/p:Projects=$scriptroot/../dotnet-interactive.sln" $args
+if [[ "$no_dotnet" != true ]]; then
+  # promote switches to arguments
+  if [[ "$run_tests" == true ]] && [[ "$is_ci" != true ]]; then
+    # CI runs unit tests elsewhere, so only promote the `--test` switch if we're not running CI
+    args="$args --test"
+  fi
+
+  # invoke regular build/test script
+  . "$scriptroot/common/build.sh" "/p:Projects=$scriptroot/../dotnet-interactive.sln" $args
+fi
