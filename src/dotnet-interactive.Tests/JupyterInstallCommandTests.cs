9b29b5db86fe7fa0d2a0dc0fba08d22fa6b2fe71
Diego Colombo <dicolomb@microsoft.com>
2021-11-17 23:19:16 +0000

    verify contract of kernelspecs is not broken

--- src/dotnet-interactive.Tests/JupyterInstallCommandTests.cs
+++ src/dotnet-interactive.Tests/JupyterInstallCommandTests.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CommandLine.IO;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Assent;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Microsoft.DotNet.Interactive.Http;
@@ -13,6 +15,7 @@ namespace Microsoft.DotNet.Interactive.App.Tests
 {
     public class JupyterInstallCommandTests
     {
+
         [Fact]
         public async Task Appends_http_port_range_arguments()
         {
@@ -28,6 +31,29 @@ namespace Microsoft.DotNet.Interactive.App.Tests
                 .HaveCount(3)
                 .And
                 .Match(s => s.All(k => k.Contains("--http-port-range")));
+
+        }
+
+        [Theory]
+        [InlineData(".NET (C#)")]
+        [InlineData(".NET (F#)")]
+        [InlineData(".NET (PowerShell)")]
+        public async Task kernel_spec_is_not_broken(string displayName)
+        {
+            var _configuration = new Configuration()
+                .UsingExtension($"kernelspec_{displayName.Replace(".", "_").Replace(" ", "_")}.txt")
+                .SetInteractive(Debugger.IsAttached);
+
+            var console = new TestConsole();
+            var kernelSpecModule = new JupyterKernelSpecModuleSimulator(true);
+            var kernelSpecInstaller = new JupyterKernelSpecInstaller(console, kernelSpecModule);
+            var jupyterCommandLine = new JupyterInstallCommand(console, kernelSpecInstaller, new HttpPortRange(100, 400));
+
+            await jupyterCommandLine.InvokeAsync();
+
+            var kernelSpec = kernelSpecModule.InstalledKernelSpecs.Single(k => k.Contains(displayName));
+
+            this.Assent(kernelSpec, _configuration);
 
         }
 
