e7dc3b5d3b73fb594d3045f342639ac308746a24
Diego Colombo <dicolomb@microsoft.com>
2021-11-24 17:08:15 +0000

    update update api script

--- src/dotnet-interactive-vscode/update-api.ps1
+++ src/dotnet-interactive-vscode/update-api.ps1
@@ -1,12 +1,28 @@
 $vsCodeStableVersion = (Get-Content "$PSScriptRoot\vscodeStableVersion.txt").Trim()
 
-function DownloadVsCodeApi([string] $branchName, [string] $destinationDirectory) {
+function DownloadVsCodeApi([string] $branchName, [string] $destinationDirectory) {    
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vs/vscode.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vs/vscode.proposed.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.d.ts"
+}
+
+function DownloadVsCodeProposedNotebookApi([string] $branchName, [string] $destinationDirectory){
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookConcatTextDocument.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookConcatTextDocument.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookContentProvider.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookContentProvider.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookCellExecutionState.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookCellExecutionState.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookControllerKind.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookControllerKind.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookDebugOptions.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookDebugOptions.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookDeprecated.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookDeprecated.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditor.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditor.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditorDecorationType.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditorDecorationType.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditorEdit.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditorEdit.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookLiveShare.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookLiveShare.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMessaging.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMessaging.d.ts"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMime.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMime.d.ts"
 }
 
 # stable
 DownloadVsCodeApi -branchName "release/$vsCodeStableVersion" -destinationDirectory "stable\src"
 
 # insiders
-DownloadVsCodeApi -branchName "main" -destinationDirectory "insiders\src"
+DownloadVsCodeProposedNotebookApi -branchName "main" -destinationDirectory "insiders\src"
