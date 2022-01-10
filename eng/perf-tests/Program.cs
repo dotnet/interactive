0c99fc7a714c2a938635a5efb713c1ec9d476538
Brett V. Forsgren <brettfo@microsoft.com>
2022-01-10 16:42:08 -0700

    add perf test project

---
+++ eng/perf-tests/Program.cs
@@ -0,0 +1,12 @@
+using BenchmarkDotNet.Running;
+
+namespace PerfTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
