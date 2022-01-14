17a09b47e59dddd657c191b93568f56986d368f3
Diego Colombo <dicolomb@microsoft.com>
2022-01-14 11:48:04 +0000

    refactor namedpipe tests

---
+++ src/Microsoft.DotNet.Interactive.Tests/KernelConnectionTestsBase.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Connection;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Tests.Utility;
+
+using Pocket;
+using Xunit.Abstractions;
+
+namespace Microsoft.DotNet.Interactive.Tests;
+
+public abstract class KernelConnectionTestsBase<T>: IDisposable
+{
+    private readonly CompositeDisposable _disposables = new();
+
+    protected KernelConnectionTestsBase(ITestOutputHelper output)
+    {
+        _disposables.Add(output.SubscribeToPocketLogger());
+    }
+
+    protected void RegisterForDisposal(IDisposable disposable)
+    {
+        _disposables.Add(disposable);
+    }
+
+    public void Dispose()
+    {
+        _disposables.Dispose();
+    }
+
+    [WindowsFact]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Test only enabled on windows platforms")]
+    public async Task it_can_reuse_connection_for_multiple_proxy_kernel()
+    {
+        var configuration = CreateConnectionConfiguration();
+        using var _ = await CreateRemoteKernelTopologyAsync(configuration);
+        var connector = await CreateConnectorAsync(configuration);
+
+        // use same connection to create 2 proxy kernel
+
+        using var proxyKernel1 = await connector.ConnectKernelAsync(new KernelInfo("kernel1"));
+
+        using var proxyKernel2 = await connector.ConnectKernelAsync(new KernelInfo("kernel2"));
+
+        var kernelCommand1 = new SubmitCode("\"echo1\"");
+
+        var kernelCommand2 = new SubmitCode("\"echo2\"");
+
+        var res1 = await proxyKernel1.SendAsync(kernelCommand1);
+
+        var res2 = await proxyKernel2.SendAsync(kernelCommand2);
+
+        var kernelEvents1 = res1.KernelEvents.ToSubscribedList();
+
+        var kernelEvents2 = res2.KernelEvents.ToSubscribedList();
+
+        kernelEvents1.Should().ContainSingle<CommandSucceeded>().Which.Command.As<SubmitCode>().Code.Should()
+            .Be(kernelCommand1.Code);
+
+        kernelEvents1.Should().ContainSingle<ReturnValueProduced>().Which.
+            FormattedValues.Should().
+            ContainSingle(f => f.Value == "echo1");
+
+        kernelEvents2.Should().ContainSingle<CommandSucceeded>().Which.Command.As<SubmitCode>().Code.Should()
+            .Be(kernelCommand2.Code);
+
+        kernelEvents2.Should().ContainSingle<ReturnValueProduced>().Which.FormattedValues.Should().ContainSingle(f => f.Value == "echo2");
+    }
+
+    protected abstract Task<IKernelConnector> CreateConnectorAsync(T configuration);
+
+    [WindowsFact]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Test only enabled on windows platforms")]
+    public async Task can_connect_to_remote_using_connect_command()
+    {
+        var configuration = CreateConnectionConfiguration();
+
+        using var _ = await CreateRemoteKernelTopologyAsync(configuration);
+
+        using var localCompositeKernel = new CompositeKernel
+        {
+            new FakeKernel("fsharp")
+        };
+
+        ConfigureConnectCommand(localCompositeKernel);
+
+        localCompositeKernel.DefaultKernelName = "fsharp";
+
+        var connectToRemoteKernel = CreateConnectionCommand(configuration);
+        var codeSubmissionForRemoteKernel = new SubmitCode(@"
+#!newKernelName
+var x = 1 + 1;
+x.Display(""text/plain"");");
+
+        var connectResults = await localCompositeKernel.SendAsync(connectToRemoteKernel);
+
+        var submissionResults = await localCompositeKernel.SendAsync(codeSubmissionForRemoteKernel);
+
+        connectResults.KernelEvents.ToSubscribedList().Should().NotContainErrors();
+
+        submissionResults.KernelEvents.ToSubscribedList()
+            .Should()
+            .ContainSingle<DisplayedValueProduced>()
+            .Which
+            .FormattedValues
+            .Single()
+            .Value
+            .Should()
+            .Be("2");
+    }
+
+    protected abstract T CreateConnectionConfiguration();
+
+    protected abstract SubmitCode CreateConnectionCommand(T configuration);
+
+    protected abstract void ConfigureConnectCommand(CompositeKernel compositeKernel);
+
+
+    protected abstract Task<KernelHost> CreateRemoteKernelTopologyAsync(T configuration);
+
+    protected abstract Task<KernelHost> ConnectHostAsync(CompositeKernel remoteKernel, T configuration);
+
+}
--- src/Microsoft.DotNet.Interactive.Tests/NamedPipeConnectionTests.cs
+++ src/Microsoft.DotNet.Interactive.Tests/NamedPipeConnectionTests.cs
@@ -4,158 +4,76 @@
 using System;
 using System.IO.Pipes;
 using System.Threading.Tasks;
-using FluentAssertions;
+
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Connection;
-using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.CSharp;
 using Microsoft.DotNet.Interactive.FSharp;
-using Microsoft.DotNet.Interactive.Tests.Utility;
-using Pocket;
+
 using Xunit.Abstractions;
 
-namespace Microsoft.DotNet.Interactive.Tests
+namespace Microsoft.DotNet.Interactive.Tests;
+
+public class NamedPipeConnectionTests : KernelConnectionTestsBase<string>
 {
-    public class NamedPipeConnectionTests : IDisposable
+    public NamedPipeConnectionTests(ITestOutputHelper output) : base(output)
     {
-        private readonly CompositeDisposable _disposables = new();
+    }
 
-        public NamedPipeConnectionTests(ITestOutputHelper output)
-        {
-            _disposables.Add(output.SubscribeToPocketLogger());
-        }
+    protected override Task<IKernelConnector> CreateConnectorAsync(string pipeName)
+    {
+        return Task.FromResult<IKernelConnector>(new NamedPipeKernelConnector(pipeName));
+    }
 
-        public void Dispose()
-        {
-            _disposables.Dispose();
-        }
+    protected override string CreateConnectionConfiguration()
+    {
+        return Guid.NewGuid().ToString();
+    }
 
-        [WindowsFact]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Test only enabled on windows platforms")]
-        public async Task it_can_reuse_connection_for_multiple_proxy_kernel()
-        {
-            var pipeName = Guid.NewGuid().ToString();
+    protected override SubmitCode CreateConnectionCommand(string pipeName)
+    {
+        return new SubmitCode($"#!connect named-pipe --kernel-name newKernelName --pipe-name {pipeName}");
+    }
 
-            // start server
-            
-            using var remoteCompositeKernel = new CompositeKernel
+    protected override void ConfigureConnectCommand(CompositeKernel compositeKernel)
+    {
+        compositeKernel.UseKernelClientConnection(new ConnectNamedPipeCommand());
+    }
+
+    protected override Task<KernelHost> CreateRemoteKernelTopologyAsync(string pipeName)
+    {
+        var remoteCompositeKernel = new CompositeKernel
             {
-                new FakeKernel("csharp")
-                {
-                    Handle = (command, context) =>
-                    {
-                        if (command is SubmitCode sc)
-                        {
-                            context.Display(sc.Code);
-                        }
-                        return Task.CompletedTask;
-                    }
-                },
-                new FakeKernel("powershell")
-            };
-
-            remoteCompositeKernel.DefaultKernelName = "csharp";
-
-            using var _ = StartServer(remoteCompositeKernel, pipeName);
-
-            // setup connection
-
-            var connector = new NamedPipeKernelConnector(pipeName);
-
-            // use same connection to create 2 proxy kernel
-
-            using var localKernel1 =  await connector.ConnectKernelAsync(new KernelInfo("kernel1"));
-           
-            using var localKernel2 = await connector.ConnectKernelAsync(new KernelInfo("kernel2"));
-
-            var kernelCommand1 = new SubmitCode("echo1");
-
-            var kernelCommand2 = new SubmitCode("echo2");
-
-            var res1 = await localKernel1.SendAsync(kernelCommand1);
-
-            var res2 = await localKernel2.SendAsync(kernelCommand2);
-
-            var kernelEvents1 = res1.KernelEvents.ToSubscribedList();
-
-            var kernelEvents2 = res2.KernelEvents.ToSubscribedList();
-
-            kernelEvents1.Should().ContainSingle<CommandSucceeded>().Which.Command.As<SubmitCode>().Code.Should()
-                .Be(kernelCommand1.Code);
-
-            kernelEvents1.Should().ContainSingle<DisplayedValueProduced>().Which.FormattedValues.Should().ContainSingle(f => f.Value == kernelCommand1.Code);
-
-            kernelEvents2.Should().ContainSingle<CommandSucceeded>().Which.Command.As<SubmitCode>().Code.Should()
-                .Be(kernelCommand2.Code);
-
-            kernelEvents2.Should().ContainSingle<DisplayedValueProduced>().Which.FormattedValues.Should().ContainSingle(f => f.Value == kernelCommand2.Code);
-        }
-
-        [WindowsFact]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Test only enabled on windows platforms")]
-        public async Task can_address_remote_composite_kernel_using_named_pipe()
-        {
-            using var localCompositeKernel = new CompositeKernel
-            {
+                new CSharpKernel(),
                 new FSharpKernel()
-            }.UseKernelClientConnection(new ConnectNamedPipeCommand());
-
-            localCompositeKernel.DefaultKernelName = "fsharp";
-
-            var pipeName = Guid.NewGuid().ToString();
-            var remoteDefaultKernelInvoked = false;
-
-            using var remoteCompositeKernel = new CompositeKernel
-            {
-                new FakeKernel("csharp")
-                {
-                    Handle = (command, context) =>
-                    {
-                        remoteDefaultKernelInvoked = true;
-                        return Task.CompletedTask;
-                    }
-                },
-                new FakeKernel("powershell")
             };
 
-            remoteCompositeKernel.DefaultKernelName = "csharp";
+        remoteCompositeKernel.DefaultKernelName = "csharp";
+        RegisterForDisposal(remoteCompositeKernel);
+        return ConnectHostAsync(remoteCompositeKernel, pipeName);
+    }
 
-            using var _ = StartServer(remoteCompositeKernel, pipeName);
 
-            using var events = localCompositeKernel.KernelEvents.ToSubscribedList();
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility",
+        Justification = "Test only enabled on windows platform")]
+    protected override Task<KernelHost> ConnectHostAsync(CompositeKernel remoteKernel, string pipeName)
+    {
+        var serverStream = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+        var kernelCommandAndEventPipeStreamReceiver = new KernelCommandAndEventPipeStreamReceiver(serverStream);
+        var kernelCommandAndEventPipeStreamSender = new KernelCommandAndEventPipeStreamSender(serverStream);
+        var host = new KernelHost(remoteKernel,
+            kernelCommandAndEventPipeStreamSender,
+            new MultiplexingKernelCommandAndEventReceiver(kernelCommandAndEventPipeStreamReceiver));
 
-            var connectToRemoteKernel = new SubmitCode($"#!connect named-pipe --kernel-name newKernelName --pipe-name {pipeName}");
-            var codeSubmissionForRemoteKernel = new SubmitCode(@"
-#!newKernelName
-var x = 1 + 1;
-x");
 
-            await localCompositeKernel.SendAsync(connectToRemoteKernel);
-            await localCompositeKernel.SendAsync(codeSubmissionForRemoteKernel);
-            
-            events.Should().NotContainErrors();
 
-            remoteDefaultKernelInvoked.Should()
-                                      .BeTrue();
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Test only enabled on windows platform")]
-        KernelHost StartServer(CompositeKernel remoteKernel, string pipeName)
+        Task.Run(() =>
         {
-            var serverStream = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
-            var kernelCommandAndEventPipeStreamReceiver = new KernelCommandAndEventPipeStreamReceiver(serverStream);
-            var kernelCommandAndEventPipeStreamSender = new KernelCommandAndEventPipeStreamSender(serverStream);
-            var host = new KernelHost(remoteKernel,
-                kernelCommandAndEventPipeStreamSender,
-                new MultiplexingKernelCommandAndEventReceiver(kernelCommandAndEventPipeStreamReceiver));
-            remoteKernel.RegisterForDisposal(serverStream);
+            // required as waiting connection on named pipe server will block
+            serverStream.WaitForConnection();
+            var _ = host.ConnectAsync();
+        });
 
-            Task.Run(() =>
-            {
-                serverStream.WaitForConnection();
-                var _ = host.ConnectAsync();
-            });
-
-            return host;
-        }
+        return Task.FromResult(host);
     }
 }
