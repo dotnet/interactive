43cc393cec974b521ebfc1eeec0380c557324407
Diego Colombo <dicolomb@microsoft.com>
2022-01-17 13:20:42 +0000

    use temp dir utility

--- src/Microsoft.DotNet.Interactive.Tests/Utility/TemporaryDirectory.cs
+++ src/Microsoft.DotNet.Interactive.Tests/Utility/TemporaryDirectory.cs
@@ -4,31 +4,58 @@
 using System;
 using System.IO;
 
-namespace Microsoft.DotNet.Interactive.Tests.Utility
-{
-    public class TemporaryDirectory : IDisposable
-    {
-        public DirectoryInfo Directory { get; }
+namespace Microsoft.DotNet.Interactive.Tests.Utility;
 
-        public TemporaryDirectory(params (string relativePath, string content)[] contents)
+public class TemporaryDirectory : IDisposable
+{
+    public DirectoryInfo Directory { get; }
+
+    public TemporaryDirectory(params (string relativePath, string content)[] contents)
+    {
+        var tempPath = Path.GetTempPath();
+        var dirName = Guid.NewGuid().ToString();
+        var fullPath = Path.Combine(tempPath, dirName);
+        var parentDirectory = new DirectoryInfo(fullPath);
+        Directory = DirectoryUtility.CreateDirectory(parentDirectory: parentDirectory);
+        DirectoryUtility.Populate(Directory, contents);
+    }
+
+    public static TemporaryDirectory CreateFromDeepCopy(DirectoryInfo source)
+    {
+        var tmp = new TemporaryDirectory();
+        
+        CopyAll(source, tmp.Directory);
+
+        static void CopyAll(DirectoryInfo source, DirectoryInfo target)
         {
-            var tempPath = Path.GetTempPath();
-            var dirName = Guid.NewGuid().ToString();
-            var fullPath = Path.Combine(tempPath, dirName);
-            var parentDirectory = new DirectoryInfo(fullPath);
-            Directory = DirectoryUtility.CreateDirectory(parentDirectory: parentDirectory);
-            DirectoryUtility.Populate(Directory, contents);
+            target.Create();
+
+            // Copy each file into the new directory.
+            foreach (var fi in source.GetFiles())
+            {
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each sub-directory using recursion.
+            foreach (var diSourceSubDir in source.GetDirectories())
+            {
+                var nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
         }
 
-        public void Dispose()
+        return tmp;
+    }
+
+    public void Dispose()
+    {
+        try
         {
-            try
-            {
-                Directory.Delete(true);
-            }
-            catch
-            {
-            }
+            Directory.Delete(true);
+        }
+        catch
+        {
         }
     }
 }
