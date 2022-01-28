6dc9ea918428a56c4153d186fb7b72434ff1f999
Diego Colombo <dicolomb@microsoft.com>
2022-01-28 03:13:33 +0000

    bypass sql lite test

--- src/Microsoft.DotNet.Interactive.Kql.Tests/KqlConnectionTests.cs
+++ src/Microsoft.DotNet.Interactive.Kql.Tests/KqlConnectionTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +9,6 @@ using FluentAssertions;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.CSharp;
 using Microsoft.DotNet.Interactive.Events;
-using Microsoft.DotNet.Interactive.ExtensionLab;
 using Microsoft.DotNet.Interactive.Formatting;
 using Microsoft.DotNet.Interactive.Formatting.Csv;
 using Microsoft.DotNet.Interactive.Formatting.TabularData;
