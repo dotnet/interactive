15d07d93383c7f6477778df9eb944f73aa58d0e1
Diego Colombo <dicolomb@microsoft.com>
2022-04-07 14:02:59 +0100

    per Johannes comments

--- src/dotnet-interactive-vscode/update-api.ps1
+++ src/dotnet-interactive-vscode/update-api.ps1
@@ -10,13 +10,12 @@ function DownloadVsCodeApi([string] $branchName, [string] $destinationDirectory)
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditor.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditor.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditorDecorationType.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditorDecorationType.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditorEdit.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditorEdit.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookLiveShare.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookLiveShare.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMessaging.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMessaging.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMime.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMime.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.textDocumentNotebook.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.textDocumentNotebook.d.ts"
 }
 
-function DownloadLegacyVsCodeApi([string] $branchName, [string] $destinationDirectory) {    
+function DownloadLegacyVsCodeApi([string] $branchName, [string] $destinationDirectory) {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vs/vscode.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vs/vscode.proposed.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.d.ts"
 }
