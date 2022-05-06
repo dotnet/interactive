e03ff11cf6843ab99a3830d219cc92673e7f6809
Jon Sequeira <jonsequeira@gmail.com>
2022-05-06 14:06:08 -0700

    wip

---
+++ src/Microsoft.DotNet.Interactive/Connection/ReadCommandOrEvent.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading;
+
+namespace Microsoft.DotNet.Interactive.Connection;
+
+public delegate CommandOrEvent ReadCommandOrEvent(CancellationToken cancellationToken = default);
