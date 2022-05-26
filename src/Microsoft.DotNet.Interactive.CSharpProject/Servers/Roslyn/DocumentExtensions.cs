5f3f30e2a1aa3a8a286b7fe1a0de449cbcb46aa3
Brett V. Forsgren <brettfo@microsoft.com>
2022-05-26 15:35:02 -0600

    fix diagnostic gathering and make project files case insensitive

--- src/Microsoft.DotNet.Interactive.CSharpProject/Servers/Roslyn/DocumentExtensions.cs
+++ src/Microsoft.DotNet.Interactive.CSharpProject/Servers/Roslyn/DocumentExtensions.cs
@@ -1,9 +1,8 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using Microsoft.CodeAnalysis;
-using Microsoft.DotNet.Interactive.CSharpProject.MLS.Project;
-using Microsoft.DotNet.Interactive.CSharpProject.Tools;
 
 namespace Microsoft.DotNet.Interactive.CSharpProject.Servers.Roslyn
 {
@@ -16,6 +15,8 @@ namespace Microsoft.DotNet.Interactive.CSharpProject.Servers.Roslyn
             d.IsMatch(source.Name);
 
         public static bool IsMatch(this Document d, string sourceName) =>
-            d.Name == sourceName || d.FilePath == sourceName || (!string.IsNullOrWhiteSpace(sourceName) && (new RelativeFilePath(sourceName).Value == new RelativeFilePath(d.Name).Value));
+            string.Compare(d.Name, sourceName, StringComparison.OrdinalIgnoreCase) == 0 ||
+            string.Compare(d.FilePath, sourceName, StringComparison.OrdinalIgnoreCase) == 0 ||
+            (!string.IsNullOrWhiteSpace(sourceName) && (string.Compare(new RelativeFilePath(sourceName).Value, new RelativeFilePath(d.Name).Value, StringComparison.OrdinalIgnoreCase) == 0));
     }
 }
