2e97d2ac8dde9a4d67cffef39a68c796e6eed1ab
Jon Sequeira <jonsequeira@gmail.com>
2022-07-01 12:30:06 -0700

    remove ref from Interactive to Interactive.Documents

--- src/Microsoft.DotNet.Interactive.Documents/DataDictionaryConverter.cs
+++ src/Microsoft.DotNet.Interactive.Documents/DataDictionaryConverter.cs
@@ -5,82 +5,81 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json;
 
-namespace Microsoft.DotNet.Interactive.Documents
+namespace Microsoft.DotNet.Interactive.Documents;
+
+internal class DataDictionaryConverter : JsonConverter<IDictionary<string, object?>>
 {
-    public class DataDictionaryConverter : JsonConverter<IDictionary<string, object?>>
+    public override IDictionary<string, object?> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        public override IDictionary<string, object?> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        EnsureStartObject(reader, typeToConvert);
+
+        var value = new Dictionary<string, object?>();
+
+        while (reader.Read())
         {
-            EnsureStartObject(reader, typeToConvert);
-
-            var value = new Dictionary<string, object?>();
-
-            while (reader.Read())
+            if (reader.TokenType == JsonTokenType.EndObject)
             {
-                if (reader.TokenType == JsonTokenType.EndObject)
-                {
-                    return value;
-                }
-
-                var keyString = reader.GetString();
-
-                reader.Read();
-
-                value.Add(keyString ?? throw new InvalidOperationException(), GetValue(ref reader, options));
+                return value;
             }
 
-            throw new JsonException($"Cannot deserialize {typeToConvert.Name}");
+            var keyString = reader.GetString();
+
+            reader.Read();
+
+            value.Add(keyString ?? throw new InvalidOperationException(), GetValue(ref reader, options));
         }
 
-        private static object? GetValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
-        {
-            object? itemValue;
+        throw new JsonException($"Cannot deserialize {typeToConvert.Name}");
+    }
 
-            switch (reader.TokenType)
+    private static object? GetValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+    {
+        object? itemValue;
+
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.String:
+                itemValue = reader.GetString();
+                break;
+            case JsonTokenType.Number:
+                itemValue = reader.GetDouble();
+                break;
+            case JsonTokenType.True:
+                itemValue = true;
+                break;
+            case JsonTokenType.False:
+                itemValue = false;
+                break;
+            case JsonTokenType.Null:
+                itemValue = null;
+                break;
+            case JsonTokenType.StartArray:
+                itemValue = ParseArray(ref reader, options);
+                break;
+            case JsonTokenType.StartObject:
+                itemValue = JsonSerializer.Deserialize(ref reader, typeof(IDictionary<string, object>), options);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return itemValue;
+    }
+
+    private static object ParseArray(ref Utf8JsonReader reader, JsonSerializerOptions options)
+    {
+        var values = new List<object?>();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndArray)
             {
-                case JsonTokenType.String:
-                    itemValue = reader.GetString();
-                    break;
-                case JsonTokenType.Number:
-                    itemValue = reader.GetDouble();
-                    break;
-                case JsonTokenType.True:
-                    itemValue = true;
-                    break;
-                case JsonTokenType.False:
-                    itemValue = false;
-                    break;
-                case JsonTokenType.Null:
-                    itemValue = null;
-                    break;
-                case JsonTokenType.StartArray:
-                    itemValue = ParseArray(ref reader, options);
-                    break;
-                case JsonTokenType.StartObject:
-                    itemValue = JsonSerializer.Deserialize(ref reader, typeof(IDictionary<string, object>), options);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                return values.ToArray();
             }
 
-            return itemValue;
+            values.Add(GetValue(ref reader, options));
         }
 
-        private static object ParseArray(ref Utf8JsonReader reader, JsonSerializerOptions options)
-        {
-            var values = new List<object?>();
-
-            while (reader.Read())
-            {
-                if (reader.TokenType == JsonTokenType.EndArray)
-                {
-                    return values.ToArray();
-                }
-
-                values.Add(GetValue(ref reader, options));
-            }
-
-            throw new JsonException("Missing end of array.");
-        }
+        throw new JsonException("Missing end of array.");
     }
 }
--- src/Microsoft.DotNet.Interactive.Documents/Jupyter/NotebookCellOutputConverter.cs
+++ src/Microsoft.DotNet.Interactive.Documents/Jupyter/NotebookCellOutputConverter.cs
@@ -6,72 +6,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 
-namespace Microsoft.DotNet.Interactive.Documents.Jupyter
+namespace Microsoft.DotNet.Interactive.Documents.Jupyter;
+
+internal class NotebookCellOutputConverter : JsonConverter<InteractiveDocumentOutputElement>
 {
-    public class NotebookCellOutputConverter : JsonConverter<InteractiveDocumentOutputElement>
+    public override InteractiveDocumentOutputElement Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        public override InteractiveDocumentOutputElement Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            EnsureStartObject(reader, typeToConvert);
+        EnsureStartObject(reader, typeToConvert);
             
-            IDictionary<string, object>? data = null;
-            string? text = null;
-            string? errorName = null;
-            string? errorValue = null;
-            IEnumerable<string>? stackTrace = null;
+        IDictionary<string, object>? data = null;
+        string? text = null;
+        string? errorName = null;
+        string? errorValue = null;
+        IEnumerable<string>? stackTrace = null;
 
-            while (reader.Read())
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.PropertyName)
             {
-                if (reader.TokenType == JsonTokenType.PropertyName)
+                switch (reader.GetString())
                 {
-                    switch (reader.GetString())
-                    {
-                        case "data":
-                            data = JsonSerializer.Deserialize<IDictionary<string, object>>(ref reader, options);
-                            break;
-                        case "text":
-                            text = JsonSerializer.Deserialize<string>(ref reader, options);
-                            break;
-                        case "errorName":
-                            if (reader.Read() && reader.TokenType == JsonTokenType.String)
-                            {
-                                errorName = reader.GetString();
-                            }
-                            break;
-                        case "errorValue":
-                            if (reader.Read() && reader.TokenType == JsonTokenType.String)
-                            {
-                                errorValue = reader.GetString();
-                            }
-                            break;
-                        case "stackTrace":
-                            if (reader.Read() && reader.TokenType == JsonTokenType.StartArray)
-                            {
-                                stackTrace = JsonSerializer.Deserialize<string[]>(ref reader, options);
-                            }
-                            break;
-                    }
-                }
-                else if (reader.TokenType == JsonTokenType.EndObject)
-                {
-                    if (text is not null)
-                    {
-                        return new TextElement(text);
-                    }
-
-                    if (data is not null)
-                    {
-                        return new DisplayElement(data);
-                    }
-
-                    if (errorName is not null && errorValue is not null && stackTrace is not null)
-                    {
-                        return new ErrorElement(errorName, errorValue, stackTrace.ToArray());
-                    }
+                    case "data":
+                        data = JsonSerializer.Deserialize<IDictionary<string, object>>(ref reader, options);
+                        break;
+                    case "text":
+                        text = JsonSerializer.Deserialize<string>(ref reader, options);
+                        break;
+                    case "errorName":
+                        if (reader.Read() && reader.TokenType == JsonTokenType.String)
+                        {
+                            errorName = reader.GetString();
+                        }
+                        break;
+                    case "errorValue":
+                        if (reader.Read() && reader.TokenType == JsonTokenType.String)
+                        {
+                            errorValue = reader.GetString();
+                        }
+                        break;
+                    case "stackTrace":
+                        if (reader.Read() && reader.TokenType == JsonTokenType.StartArray)
+                        {
+                            stackTrace = JsonSerializer.Deserialize<string[]>(ref reader, options);
+                        }
+                        break;
                 }
             }
+            else if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                if (text is not null)
+                {
+                    return new TextElement(text);
+                }
 
-            throw new JsonException($"Cannot deserialize {typeToConvert.Name}");
+                if (data is not null)
+                {
+                    return new DisplayElement(data);
+                }
+
+                if (errorName is not null && errorValue is not null && stackTrace is not null)
+                {
+                    return new ErrorElement(errorName, errorValue, stackTrace.ToArray());
+                }
+            }
         }
+
+        throw new JsonException($"Cannot deserialize {typeToConvert.Name}");
     }
 }
