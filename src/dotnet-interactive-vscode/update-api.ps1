90024b2a4f7071ab62399614f98e54abee9cdecc
Brett V. Forsgren <brettfo@microsoft.com>
2022-07-20 15:29:14 -0600

    update VS Code notebook API usage (#2158)
    * use finalized `notebookEditor` API
    
    * remove unused notebook API proposals

--- src/dotnet-interactive-vscode/update-api.ps1
+++ src/dotnet-interactive-vscode/update-api.ps1
@@ -2,12 +2,7 @@ $vsCodeStableVersion = (Get-Content "$PSScriptRoot\vscodeStableVersion.txt").Tri
 
 function DownloadVsCodeApi([string] $branchName, [string] $destinationDirectory) {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookCellExecutionState.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookCellExecutionState.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookControllerKind.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookControllerKind.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookDebugOptions.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookDebugOptions.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookEditor.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookEditor.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMessaging.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMessaging.d.ts"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMime.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMime.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookWorkspaceEdit.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookWorkspaceEdit.d.ts"
 }
 
