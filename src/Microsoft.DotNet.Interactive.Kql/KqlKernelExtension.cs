c8b5114b449f1e954a25151ea6ab8c0c7abbc3fd
Cory Rivera <corivera@microsoft.com>
2022-07-25 13:10:12 -0700

    Add Kusto tool logic

--- src/Microsoft.DotNet.Interactive.Kql/KqlKernelExtension.cs
+++ src/Microsoft.DotNet.Interactive.Kql/KqlKernelExtension.cs
@@ -3,56 +3,46 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
 using Microsoft.DotNet.Interactive.SqlServer;
+using Microsoft.DotNet.Interactive.Utility;
 
 namespace Microsoft.DotNet.Interactive.Kql
 {
     public class KqlKernelExtension : IKernelExtension
     {
-        public Task OnLoadAsync(Kernel kernel)
+        public async Task OnLoadAsync(Kernel kernel)
         {
             if (kernel is CompositeKernel compositeKernel)
             {
-                var root = compositeKernel.RootKernel.FindResolvedNativeSqlToolsServicePackageReference();
-                if (root != null)
+                // Check if the required Sql Tools Service tool is installed, and then install it if necessary
+                var dotnet = new Dotnet();
+                var installedGlobalTools = await dotnet.ToolList(null, true);
+                const string kqlToolName = "MicrosoftKustoServiceLayer";
+                bool kqlToolInstalled = installedGlobalTools.Any(tool => string.Equals(tool, kqlToolName, StringComparison.InvariantCultureIgnoreCase));
+                if (!kqlToolInstalled)
                 {
-                    var pathToService = root.PathToToolsService("MicrosoftKustoServiceLayer");
-
-                    if (!string.IsNullOrEmpty(pathToService))
-                    {
-                        if (File.Exists(pathToService))
-                        {
-                            compositeKernel
-                                .AddKernelConnector(new ConnectKqlCommand(pathToService));
-
-                            KernelInvocationContext.Current?.Display(
-                                new HtmlString(@"<details><summary>Query Microsoft Kusto Server databases.</summary>
-        <p>This extension adds support for connecting to Microsoft Kusto Server databases using the <code>#!connect kql</code> magic command. For more information, run a cell using the <code>#!kql</code> magic command.</p>
-        </details>"),
-                                "text/html");
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException($"The KQL extension was loaded successfully and resolved the path to the Kusto Tools Service but the file {pathToService} does not exist. The #!connect kql command will not be available.");
-                        }
-
-                    }
-                    else
-                    {
-                        throw new InvalidOperationException("The KQL extension was loaded successfully but was unable to determine the path to the KQL Tools Service. The #!connect kql command will not be available.");
-                    }
+                    var nugetSource = "https://mssqltools.pkgs.visualstudio.com/_packaging/mssqltools/nuget/v3/index.json";
+                    var commandLineResult = await dotnet.ToolInstall("Microsoft.Kusto.ServiceLayer.Tool", null, nugetSource, null, true);
+                    commandLineResult.ThrowOnFailure();
+                    KernelInvocationContext.Current?.Display("Kusto package successfully installed.");
                 }
                 else
                 {
-                    throw new InvalidOperationException($"The KQL extension was loaded successfully but was unable to find the KQL Tools Service package. The #!connect kql command will not be available. (RID: {RuntimeInformation.RuntimeIdentifier})");
+                    KernelInvocationContext.Current?.Display("Kusto package already installed.");
                 }
+                compositeKernel
+                    .AddKernelConnector(new ConnectKqlCommand(kqlToolName));
 
+                KernelInvocationContext.Current?.Display(
+                    new HtmlString(@"<details><summary>Query Microsoft Kusto Server databases.</summary>
+        <p>This extension adds support for connecting to Microsoft Kusto Server databases using the <code>#!connect kql</code> magic command. For more information, run a cell using the <code>#!kql</code> magic command.</p>
+        </details>"),
+                    "text/html");
             }
-
-            return Task.CompletedTask;
         }
     }
 }
