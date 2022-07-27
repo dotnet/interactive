26a64fe5b443ee4b11f3d51d54375f3228739183
Diego Colombo <dicolomb@microsoft.com>
2022-07-27 23:41:24 +0100

    make setters internal

--- src/Microsoft.DotNet.Interactive/KernelInfo.cs
+++ src/Microsoft.DotNet.Interactive/KernelInfo.cs
@@ -46,9 +46,9 @@ namespace Microsoft.DotNet.Interactive
             init => NameAndAliases.UnionWith(value);
         }
 
-        public string? LanguageName { get; set; }
+        public string? LanguageName { get; internal set; }
 
-        public string? LanguageVersion { get; set; }
+        public string? LanguageVersion { get; internal set; }
 
         public string LocalName { get; }
 
