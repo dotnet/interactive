930f58e1e2f1b704dc4b7ae764181b9463e894e5
Shibani Basava <shibanib@microsoft.com>
2022-07-29 18:35:10 -0700

    add completion for juptyer kernels

---
+++ src/Microsoft.DotNet.Interactive.Jupyter/Connection/RequestCompletionsHandler.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Formatting;
+using Microsoft.DotNet.Interactive.Jupyter.Messaging;
+using Microsoft.DotNet.Interactive.Jupyter.Protocol;
+using Microsoft.DotNet.Interactive.Utility;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Interactive.Jupyter.Connection
+{
+    internal class RequestCompletionsHandler : CommandToJupyterMessageHandlerBase<RequestCompletions>
+    {
+        public RequestCompletionsHandler(IMessageSender sender, IMessageReceiver reciever) : base(sender, reciever)
+        {
+        }
+
+        public override async Task HandleCommandAsync(RequestCompletions command, ICommandExecutionContext context, CancellationToken token)
+        {
+            var request = Messaging.Message.Create(new CompleteRequest(
+                                                command.Code,
+                                                SourceUtilities.GetCursorOffsetFromPosition(command.Code, command.LinePosition)));
+
+            var reply = Receiver.Messages.ChildOf(request)
+                                    .SelectContent()
+                                    .Do(replyMessage => HandleReplyMessage(replyMessage, command, context))
+                                    .TakeUntilMessageType(JupyterMessageContentTypes.CompleteReply, JupyterMessageContentTypes.Error);
+            // run until we get a definitive pass or fail
+
+            await Sender.SendAsync(request);
+            await reply.ToTask(token);
+        }
+
+        private void HandleReplyMessage(Protocol.Message message, RequestCompletions command, ICommandExecutionContext context)
+        {
+            switch (message)
+            {
+                case (CompleteReply results):
+
+                    bool metadataAvailable = results.MetaData.TryGetValue(CompletionResultMetadata.Entry, out IReadOnlyList<CompletionResultMetadata> resultsMetadata);
+                    if (!metadataAvailable)
+                    {
+                        resultsMetadata = new List<CompletionResultMetadata>();
+                    }
+
+                    var completionItems = from match in results.Matches
+                                join metadata in resultsMetadata on match equals metadata.Text into ci
+                                from itemMetadata in ci.DefaultIfEmpty()
+                                select new CompletionItem(
+                                    displayText: itemMetadata?.DisplayText ?? match,
+                                    kind: itemMetadata?.Type ?? string.Empty,
+                                    insertText: match,
+                                    filterText: match,
+                                    sortText: match);
+
+                    var completion = new CompletionsProduced(
+                        completionItems, command,
+                        SourceUtilities.GetLinePositionSpanFromStartAndEndIndices(command.Code, results.CursorStart, results.CursorEnd));
+
+                    context.Publish(completion);
+                    context.Publish(new CommandSucceeded(command));
+                    break;
+                case (Error error):
+                    // TODO: how to translate traceback to exception;
+                    context.Publish(new CommandFailed(null, command, error.EValue));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
