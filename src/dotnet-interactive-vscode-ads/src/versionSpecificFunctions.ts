d53697a47d47f79ec49143dece7f9e13962e13fe
Cory Rivera <corivera@microsoft.com>
2022-09-28 13:45:53 -0700

    Use connection dialog for connectionstring inputs in ADS.

--- src/dotnet-interactive-vscode-ads/src/versionSpecificFunctions.ts
+++ src/dotnet-interactive-vscode-ads/src/versionSpecificFunctions.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import * as vscode from 'vscode';
+import * as azdata from 'azdata';
 
 export function getNotebookDocumentFromEditor(notebookEditor: vscode.NotebookEditor): vscode.NotebookDocument {
     return notebookEditor.document;
@@ -26,4 +27,20 @@ export async function replaceNotebookMetadata(notebookUri: vscode.Uri, documentM
     edit.replaceNotebookMetadata(notebookUri, documentMetadata);
     const succeeded = await vscode.workspace.applyEdit(edit);
     return succeeded;
+}
+
+export async function handleRequestInput(prompt: string, password: boolean, inputTypeHint: string): Promise<string | null | undefined> {
+    let result: string | null | undefined;
+    if (inputTypeHint === 'connectionstring') {
+        let connection = await azdata.connection.openConnectionDialog();
+        if (connection) {
+            result = await azdata.connection.getConnectionString(connection.connectionId, true);
+        }
+    } else {
+        result = (inputTypeHint === 'file')
+            ? await vscode.window.showOpenDialog({ canSelectFiles: true, canSelectFolders: false, title: prompt, canSelectMany: false })
+                .then(v => typeof v?.[0].fsPath === 'undefined' ? null : v[0].fsPath)
+            : await vscode.window.showInputBox({ prompt, password });
+    }
+    return result;
 }
