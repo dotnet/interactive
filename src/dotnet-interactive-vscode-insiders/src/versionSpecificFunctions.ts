d549b8309ed60600cb79a69bda640cddde625de9
Cory Rivera <corivera@microsoft.com>
2022-09-29 14:20:12 -0700

    Remove unneeded imports

--- src/dotnet-interactive-vscode-insiders/src/versionSpecificFunctions.ts
+++ src/dotnet-interactive-vscode-insiders/src/versionSpecificFunctions.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import * as vscode from 'vscode';
-import { CompositeKernel } from './vscode-common/dotnet-interactive/compositeKernel';
 
 export function getNotebookDocumentFromEditor(notebookEditor: vscode.NotebookEditor): vscode.NotebookDocument {
     return notebookEditor.notebook;
