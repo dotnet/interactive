ed2e393e21586c954ca05d3748c4b72b525073eb
Brett V. Forsgren <brettfo@microsoft.com>
2022-10-04 17:07:24 -0600

    prepare for vscode 1.72 release

--- src/dotnet-interactive-vscode/update-api.ps1
+++ src/dotnet-interactive-vscode/update-api.ps1
@@ -3,10 +3,6 @@ $vsCodeStableVersion = (Get-Content "$PSScriptRoot\vscodeStableVersion.txt").Tri
 function DownloadVsCodeApi([string] $branchName, [string] $destinationDirectory, [bool] $isInsiders = $false) {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.d.ts"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookMessaging.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookMessaging.d.ts"
-
-    if (-not $isInsiders) {
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/$branchName/src/vscode-dts/vscode.proposed.notebookWorkspaceEdit.d.ts" -OutFile "$PSScriptRoot\$destinationDirectory\vscode.proposed.notebookWorkspaceEdit.d.ts"
-    }
 }
 
 function DownloadLegacyVsCodeApi([string] $branchName, [string] $destinationDirectory) {
