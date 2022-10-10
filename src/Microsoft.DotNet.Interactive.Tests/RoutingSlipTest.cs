852d47a99fcd64e0685a060c48749f22c259c53a
Diego Colombo <dicolomb@microsoft.com>
2022-10-10 18:56:13 +0100

    create tests

---
+++ src/Microsoft.DotNet.Interactive.Tests/RoutingSlipTest.cs
@@ -0,0 +1,28 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.Tests;
+
+public class RoutingSlipTest
+{
+    [Fact]
+    public void it_can_be_marked_when_arriving_in_a_kernel()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void it_can_be_marked_when_command_is_handled_a_kernel()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void once_handled_in_a_kernel_cannot_be_marked_again()
+    {
+        throw new NotImplementedException();
+    }
+}
