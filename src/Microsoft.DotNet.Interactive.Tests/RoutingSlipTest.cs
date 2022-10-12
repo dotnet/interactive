c6fbc6a877d49314fe8dbacd6da18c86ecbaf763
Diego Colombo <dicolomb@microsoft.com>
2022-10-12 16:53:33 +0100

    wip

--- src/Microsoft.DotNet.Interactive.Tests/RoutingSlipTest.cs
+++ src/Microsoft.DotNet.Interactive.Tests/RoutingSlipTest.cs
@@ -14,22 +14,32 @@ public class RoutingSlipTest
     {
         var routingSlip = new RoutingSlip();
         routingSlip.MarkAsReceived(new Uri("kernel://pid-1234"));
-        
-        throw new NotImplementedException();
+        routingSlip.Contains(new Uri("kernel://pid-1234")).Should().BeTrue();
+
     }
 
     [Fact]
     public void can_be_marked_when_completed()
     {
         var routingSlip = new RoutingSlip();
-        routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
-        throw new NotImplementedException();
+        routingSlip.MarkAsReceived(new Uri("kernel://pid-1234"));
+        var markAction = () => routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
+        markAction.Should().NotThrow();
+    }
+
+    [Fact]
+    public void when_does_not_contain_kernel_uri_can_not_be_marked_as_completed()
+    {
+        var routingSlip = new RoutingSlip();
+        var markAction = () => routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
+        markAction.Should().Throw<InvalidOperationException > ();
     }
 
     [Fact]
     public void when_marked_as_completed_cannot_be_marked_again_as_received()
     {
         var routingSlip = new RoutingSlip();
+        routingSlip.MarkAsReceived(new Uri("kernel://pid-1234"));
         routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
 
         var markAgain = () => routingSlip.MarkAsReceived(new Uri("kernel://pid-1234"));
@@ -41,6 +51,7 @@ public class RoutingSlipTest
     public void when_marked_as_completed_cannot_be_marked_again_as_completed()
     {
         var routingSlip = new RoutingSlip();
+        routingSlip.MarkAsReceived(new Uri("kernel://pid-1234"));
         routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
 
         var markAgain = () => routingSlip.MarkAsCompleted(new Uri("kernel://pid-1234"));
--- src/Microsoft.DotNet.Interactive/RoutingSlip.cs
+++ src/Microsoft.DotNet.Interactive/RoutingSlip.cs
@@ -2,65 +2,63 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
+using Microsoft.DotNet.Interactive.Formatting;
+
 namespace Microsoft.DotNet.Interactive;
 
-public class RoutingSlip 
+public class RoutingSlip
 {
-    private readonly HashSet<Uri> _uniqueUris ;
-    private readonly List<Uri> _uris;
-    private readonly object _lock = new();
+    private readonly ConcurrentDictionary<Uri, Entry> _entriesByKernelUris = new();
+    private readonly List<Entry> _orderedEntries = new();
+   
 
     public RoutingSlip(RoutingSlip source = null)
     {
         if (source is { })
         {
-            _uniqueUris = new HashSet<Uri>(source._uniqueUris);
-            _uris = new List<Uri>(source._uris);
+            foreach (var entry in source._orderedEntries)
+            {
+                var newEntry = new Entry(entry.KernelUri, entry.IsCompleted);
+                _entriesByKernelUris.TryAdd(entry.KernelUri, entry);
+                _orderedEntries.Add(newEntry);
+            }
         }
-        else
-        {
-            _uniqueUris = new HashSet<Uri>();
-            _uris = new List<Uri>();
-        }
+
     }
 
     public void MarkAsReceived(Uri kernelOrKernelHostUri)
     {
-        lock (_lock)
+        var newEntry = new Entry(kernelOrKernelHostUri, false);
+        if (!_entriesByKernelUris.TryAdd(kernelOrKernelHostUri, newEntry))
+
         {
-            if (_uniqueUris.Add(kernelOrKernelHostUri))
-            {
-                _uris.Add(kernelOrKernelHostUri);
-                
-            }
-            else
-            {
-                throw new InvalidOperationException($"The routing slip already contains {kernelOrKernelHostUri}");
-            }
+            throw new InvalidOperationException($"The routing slip already contains {kernelOrKernelHostUri}");
         }
-        
+        else
+        {
+            _orderedEntries.Add(newEntry);
+        }
     }
-    
+
     public bool StartsWith(RoutingSlip other)
     {
-        return StartsWith(other._uris.ToArray());
-       
+        return StartsWith(other._orderedEntries.Select(e => e.KernelUri).ToArray());
     }
 
     public bool StartsWith(params Uri[] kernelUris)
     {
-        if (kernelUris.Length > _uris.Count)
+        if (kernelUris.Length > _orderedEntries.Count)
         {
             return false;
         }
-        var contains = _uris.Zip(kernelUris, (o, i) => o.Equals(i)).All(x => x);
+        var contains = _orderedEntries.Zip(kernelUris, (o, i) => o.KernelUri.Equals(i)).All(x => x);
         return contains;
     }
-    
+
     public void Append(RoutingSlip other)
     {
         throw new NotImplementedException();
@@ -68,11 +66,51 @@ public class RoutingSlip
 
     public Uri[] ToUriArray()
     {
-        return _uris.ToArray();
+        return _orderedEntries.Select(e => e.KernelUri).ToArray();
     }
 
     public void MarkAsCompleted(Uri uri)
     {
-        throw new NotImplementedException();
+        if (_entriesByKernelUris.TryGetValue(uri, out var entry))
+        {
+            entry.MarkAsCompleted();
+        }
+        else
+        {
+            throw new InvalidOperationException($"The routing slip does not contain {uri}");
+        }
+    }
+
+    public void AddAndMarkAsCompleted(Uri uri)
+    {
+        MarkAsReceived(uri);
+        MarkAsCompleted(uri);
+    }
+
+    class Entry
+    {
+        public Entry(Uri kernelUri, bool isCompleted)
+        {
+            KernelUri = kernelUri;
+            IsCompleted = isCompleted;
+        }
+
+        public bool IsCompleted { get; private set; }
+
+        public Uri KernelUri { get; }
+
+        public void MarkAsCompleted()
+        {
+            if (IsCompleted)
+            {
+                throw new InvalidOperationException($"The routing slip is already marked as completed for {KernelUri}");
+            }
+            IsCompleted = true;
+        }
+    }
+
+    public bool Contains(Uri uri)
+    {
+        return _entriesByKernelUris.ContainsKey(uri);
     }
 }
