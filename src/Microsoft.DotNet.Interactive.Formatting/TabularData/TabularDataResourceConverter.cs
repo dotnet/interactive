7b7818a1f6332a35b95489c1df81fe7cf7d50223
Diego Colombo <dicolomb@microsoft.com>
2022-10-13 16:53:04 +0100

    converter

--- src/Microsoft.DotNet.Interactive.Formatting/TabularData/TabularDataResourceConverter.cs
+++ src/Microsoft.DotNet.Interactive.Formatting/TabularData/TabularDataResourceConverter.cs
@@ -4,37 +4,34 @@
 using System;
 using System.Text.Json;
 
-namespace Microsoft.DotNet.Interactive.Formatting.TabularData
-{
-    public class TabularDataResourceConverter : JsonConverter<TabularDataResource>
-    {
-        public override TabularDataResource Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            throw new NotImplementedException();
-        }
+namespace Microsoft.DotNet.Interactive.Formatting.TabularData;
 
-        public override void Write(Utf8JsonWriter writer, TabularDataResource value, JsonSerializerOptions options)
+public class TabularDataResourceConverter : JsonConverter<TabularDataResource>
+{
+    public override TabularDataResource Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, TabularDataResource value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteString("profile",value.Profile);
+        writer.WritePropertyName("schema");
+        JsonSerializer.Serialize(writer, value.Schema, options);
+        writer.WritePropertyName("data");
+        writer.WriteStartArray();
+        foreach (var row in value.Data)
         {
             writer.WriteStartObject();
-            writer.WriteString("profile",value.Profile);
-            writer.WritePropertyName("schema");
-            JsonSerializer.Serialize(writer, value.Schema, options);
-            writer.WritePropertyName("data");
-            writer.WriteStartArray();
-            foreach (var row in value.Data)
+            foreach (var field in row)
             {
-                writer.WriteStartObject();
-                foreach (var field in row)
-                {
-                    writer.WritePropertyName(field.Key);
-                    JsonSerializer.Serialize(writer, field.Value, options);
-                }
-               
-                writer.WriteEndObject();
+                writer.WritePropertyName(field.Key);
+                JsonSerializer.Serialize(writer, field.Value, options);
             }
-            JsonSerializer.Serialize(writer, value.Data, options);
-            writer.WriteEndArray();
             writer.WriteEndObject();
         }
+        writer.WriteEndArray();
+        writer.WriteEndObject();
     }
 }
