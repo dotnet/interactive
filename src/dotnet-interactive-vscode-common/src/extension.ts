19fd0cfe6c8270bbba5bfe5f8bcf097da2c8f0ca
Brett V. Forsgren <brettfo@microsoft.com>
2022-10-31 10:42:38 -0600

    retain `dotnet-interactive` prefix on dotnet-specific configs (#2399)

--- src/dotnet-interactive-vscode-common/src/extension.ts
+++ src/dotnet-interactive-vscode-common/src/extension.ts
@@ -63,15 +63,16 @@ export const DotNetPathManager = new CachedDotNetPathManager();
 
 export async function activate(context: vscode.ExtensionContext) {
 
-    const config = vscode.workspace.getConfiguration('polyglot-notebook');
-    const minDotNetSdkVersion = config.get<string>('minimumDotNetSdkVersion') || '6.0';
+    const dotnetConfig = vscode.workspace.getConfiguration('dotnet-interactive');
+    const polyglotConfig = vscode.workspace.getConfiguration('polyglot-notebook');
+    const minDotNetSdkVersion = dotnetConfig.get<string>('minimumDotNetSdkVersion') || '6.0';
     const diagnosticsChannel = new OutputChannelAdapter(vscode.window.createOutputChannel('Polyglot Notebook : diagnostics'));
     const loggerChannel = new OutputChannelAdapter(vscode.window.createOutputChannel('Polyglot Notebook : logger'));
     DotNetPathManager.setOutputChannelAdapter(diagnosticsChannel);
 
     Logger.configure('extension host', logEntry => {
-        const config = vscode.workspace.getConfiguration('polyglot-notebook');
-        const loggerLevelString = config.get<string>('logLevel') || LogLevel[LogLevel.Error];
+        const polyglotConfig = vscode.workspace.getConfiguration('polyglot-notebook');
+        const loggerLevelString = polyglotConfig.get<string>('logLevel') || LogLevel[LogLevel.Error];
         const loggerLevelKey = loggerLevelString as keyof typeof LogLevel;
         const logLevel = LogLevel[loggerLevelKey];
         if (logEntry.logLevel >= logLevel) {
@@ -100,7 +101,8 @@ export async function activate(context: vscode.ExtensionContext) {
     }
 
     async function kernelChannelCreator(notebookUri: vscodeLike.Uri): Promise<KernelCommandAndEventChannel> {
-        const config = vscode.workspace.getConfiguration('polyglot-notebook');
+        const dotnetConfig = vscode.workspace.getConfiguration('dotnet-interactive');
+        const polyglotConfig = vscode.workspace.getConfiguration('polyglot-notebook');
         const launchOptions = await getInteractiveLaunchOptions();
         if (!launchOptions) {
             throw new Error(`Unable to get interactive launch options.  Please see the '${diagnosticsChannel.getName()}' output window for details.`);
@@ -108,10 +110,10 @@ export async function activate(context: vscode.ExtensionContext) {
 
         // prepare kernel transport launch arguments and working directory using a fresh config item so we don't get cached values
 
-        const kernelTransportArgs = config.get<Array<string>>('kernelTransportArgs')!;
+        const kernelTransportArgs = dotnetConfig.get<Array<string>>('kernelTransportArgs')!;
         const argsTemplate = {
             args: kernelTransportArgs,
-            workingDirectory: config.get<string>('kernelTransportWorkingDirectory')!
+            workingDirectory: dotnetConfig.get<string>('kernelTransportWorkingDirectory')!
         };
 
         // try to use $HOME/Downloads as a fallback for remote notebooks, but use the home directory if all else fails
@@ -121,7 +123,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
         const workspaceFolderUris = vscode.workspace.workspaceFolders?.map(folder => folder.uri) || [];
         const workingDirectory = getWorkingDirectoryForNotebook(notebookUri, workspaceFolderUris, fallbackWorkingDirectory);
-        const environmentVariables = config.get<{ [key: string]: string }>('kernelEnvironmentVariables');
+        const environmentVariables = polyglotConfig.get<{ [key: string]: string }>('kernelEnvironmentVariables');
         const processStart = processArguments(argsTemplate, workingDirectory, DotNetPathManager.getDotNetPath(), launchOptions!.workingDirectory, environmentVariables);
         let notification = {
             displayError: async (message: string) => { await vscode.window.showErrorMessage(message, { modal: false }); },
@@ -206,13 +208,13 @@ export async function activate(context: vscode.ExtensionContext) {
 
     const hostVersionSuffix = isInsidersBuild() ? 'Insiders' : 'Stable';
     diagnosticsChannel.appendLine(`Extension started for VS Code ${hostVersionSuffix}.`);
-    const languageServiceDelay = config.get<number>('languageServiceDelay') || 500; // fall back to something reasonable
+    const languageServiceDelay = polyglotConfig.get<number>('languageServiceDelay') || 500; // fall back to something reasonable
 
     const preloads = getPreloads(context.extensionPath);
 
     ////////////////////////////////////////////////////////////////////////////////
     const launchOptions = await getInteractiveLaunchOptions();
-    const serializerCommand = <string[]>config.get('notebookParserArgs') || []; // TODO: fallback values?
+    const serializerCommand = <string[]>dotnetConfig.get('notebookParserArgs') || []; // TODO: fallback values?
     const serializerCommandProcessStart = processArguments({ args: serializerCommand, workingDirectory: launchOptions!.workingDirectory }, '.', DotNetPathManager.getDotNetPath(), context.globalStorageUri.fsPath);
     const serializerLineAdapter = new ChildProcessLineAdapter(serializerCommandProcessStart.command, serializerCommandProcessStart.args, serializerCommandProcessStart.workingDirectory, true, diagnosticsChannel);
     const messageClient = new MessageClient(serializerLineAdapter);
