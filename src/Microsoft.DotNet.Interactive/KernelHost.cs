31fcb4d8c7762bf9c037a17443598013c66bdc7d
Brett V. Forsgren <brettfo@microsoft.com>
2022-11-14 16:58:59 -0700

    log errors

--- src/Microsoft.DotNet.Interactive/KernelHost.cs
+++ src/Microsoft.DotNet.Interactive/KernelHost.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Connection;
 using Microsoft.DotNet.Interactive.Events;
+using Pocket;
 
 namespace Microsoft.DotNet.Interactive
 {
@@ -111,10 +112,16 @@ namespace Microsoft.DotNet.Interactive
                     // this needs to be dispatched this way so that it does not block the current thread, which we see in certain bidirectional command scenarios (RequestInput sent by the SubmissionParser during magic command token interpolation) in stdio mode only (i.e. System.Console.In implementation details), and it has proven non-reproducible in tests.
                     _eventLoop?.Schedule(async () =>
                     {
-                        await _kernel.SendAsync(commandOrEvent.Command, _cancellationTokenSource.Token);
+                        try
+                        {
+                            await _kernel.SendAsync(commandOrEvent.Command, _cancellationTokenSource.Token);
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.Log.Error(e);
+                        }
                     });
                 }
-                
             });
 
             await _defaultSender.SendAsync(
