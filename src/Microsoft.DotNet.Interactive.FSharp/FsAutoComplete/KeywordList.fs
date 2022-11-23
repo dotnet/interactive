f9af4d257291fe1638b15c622fd80967c14cc2e7
Brett V. Forsgren <brettfo@microsoft.com>
2022-11-23 15:37:48 -0700

    update to F# 7

--- src/Microsoft.DotNet.Interactive.FSharp/FsAutoComplete/KeywordList.fs
+++ src/Microsoft.DotNet.Interactive.FSharp/FsAutoComplete/KeywordList.fs
@@ -1,40 +1,39 @@
 namespace FsAutoComplete
 
-open FSharp.Compiler.EditorServices
 open FSharp.Compiler.Text
-open FSharp.Compiler.Xml
 open FSharp.Compiler.Tokenization
+open FSharp.Compiler.EditorServices
 open FSharp.Compiler.Symbols
-open FSharp.Compiler.Syntax
 
-#nowarn "57"
+module KeywordList =
 
-module internal KeywordList =
+  let keywordDescriptions = FSharpKeywords.KeywordsWithDescription |> dict
 
-    let keywordDescriptions =
-        FSharpKeywords.KeywordsWithDescription
-        |> dict
+  let keywordTooltips =
+    keywordDescriptions
+    |> Seq.map (fun kv ->
+      let lines = kv.Value.Replace("\r\n", "\n").Split('\n')
 
-    let keywordTooltips =
-      keywordDescriptions
-      |> Seq.map (fun kv ->
-        let tip = ToolTipText[ ToolTipElement.Single( [| TaggedText(TextTag.Keyword, kv.Key) |], FSharpXmlDoc.FromXmlText (XmlDoc([|kv.Value|], Range.rangeStartup))) ]
-        kv.Key, tip)
-      |> dict
+      let allLines = Array.concat [| [| "<summary>" |]; lines; [| "</summary>" |] |]
 
-    let hashDirectives =
-        [ "r", "References an assembly"
-          "load", "Reads a source file, compiles it, and runs it."
-          "I", "Specifies an assembly search path in quotation marks."
-          "light", "Enables or disables lightweight syntax, for compatibility with other versions of ML"
-          "if", "Supports conditional compilation"
-          "else", "Supports conditional compilation"
-          "endif", "Supports conditional compilation"
-          "nowarn", "Disables a compiler warning or warnings"
-          "line", "Indicates the original source code line"]
-        |> dict
+      let tip =
+        ToolTipText
+          [ ToolTipElement.Single(
+              [| TaggedText.tagText kv.Key |],
+              FSharpXmlDoc.FromXmlText(FSharp.Compiler.Xml.XmlDoc(allLines, Range.Zero))
+            ) ]
 
+      kv.Key, tip)
+    |> dict
 
-    let allKeywords : string list =
-        FSharpKeywords.KeywordsWithDescription
-        |> List.map fst
+  let hashDirectives =
+    [ "r", "References an assembly"
+      "load", "Reads a source file, compiles it, and runs it."
+      "I", "Specifies an assembly search path in quotation marks."
+      "light", "Enables or disables lightweight syntax, for compatibility with other versions of ML"
+      "if", "Supports conditional compilation"
+      "else", "Supports conditional compilation"
+      "endif", "Supports conditional compilation"
+      "nowarn", "Disables a compiler warning or warnings"
+      "line", "Indicates the original source code line" ]
+    |> dict
