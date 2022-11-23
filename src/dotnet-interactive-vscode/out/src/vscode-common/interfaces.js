d247c861a74ad96f06ac6daaa3d495b4f3e28123
Diego Colombo <dicolomb@microsoft.com>
2022-11-23 22:54:34 +0000

    clear

--- src/dotnet-interactive-vscode/out/src/vscode-common/interfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NonWindowsEol = exports.WindowsEol = void 0;
-// host architecture
-exports.WindowsEol = "\r\n";
-exports.NonWindowsEol = "\n";
-//# sourceMappingURL=interfaces.js.map
