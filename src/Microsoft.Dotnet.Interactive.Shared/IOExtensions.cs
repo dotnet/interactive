002484c27096f9968c2f1b914c14ddd4e3fd58bc
Shyam Namboodiripad <gnamboo@microsoft.com>
2023-01-27 17:09:47 -0800

    Fix a NullReferenceException.

--- src/Microsoft.Dotnet.Interactive.Shared/IOExtensions.cs
+++ src/Microsoft.Dotnet.Interactive.Shared/IOExtensions.cs
@@ -11,25 +11,33 @@ namespace Microsoft.DotNet.Interactive.Utility;
 
 internal class IOExtensions
 {
-    private const int DefaultBufferSize = 4096;
+    /// NOTE: This matches the default encoding used in <see cref="File.ReadAllTextAsync(string, CancellationToken)"/>.
+    /// We don't expect this default to change as it would be a significant breaking change for .NET.
     private static readonly Encoding DefaultEncoding = Encoding.UTF8;
 
     internal static Task<string> ReadAllTextAsync(
         string filePath,
         Encoding? encoding = null,
         CancellationToken cancellationToken = default)
-#if !NETSTANDARD2_0
-            => File.ReadAllTextAsync(filePath, encoding, cancellationToken);
-#else
-            => ReadAllTextInternalAsync(filePath, encoding, cancellationToken);
-
-    private static async Task<string> ReadAllTextInternalAsync(
-        string filePath,
-        Encoding? encoding,
-        CancellationToken cancellationToken)
     {
         encoding ??= DefaultEncoding;
 
+#if NETSTANDARD2_0
+        return ReadAllTextInternalAsync(filePath, encoding, cancellationToken);
+#else
+        return File.ReadAllTextAsync(filePath, encoding, cancellationToken);
+#endif
+    }
+
+#if NETSTANDARD2_0
+    /// NOTE: This matches the default buffer size used in <see cref="File.ReadAllTextAsync"/>.
+    private const int DefaultBufferSize = 4096;
+
+    private static async Task<string> ReadAllTextInternalAsync(
+        string filePath,
+        Encoding encoding,
+        CancellationToken cancellationToken)
+    {
         using var stream =
             new FileStream(
                 filePath,
