b1778ccfd5e3f3afcf2c3a42cec76540e421eebc
Jon Sequeira <jonsequeira@gmail.com>
2023-01-30 06:20:27 -0800

    move extension loading to tool project, improve extension.dib loading (#2656)
    * move extension loading to tool project, improve extension.dib loading
    
    * put back LogEventsToPocketLogger as internal
    
    * update HTTP API test to use UseNuGetExtensions

--- src/dotnet-interactive.Tests/FileProviderTests.cs
+++ src/dotnet-interactive.Tests/FileProviderTests.cs
@@ -1,13 +1,9 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Threading.Tasks;
-
 using FluentAssertions;
 using Microsoft.DotNet.Interactive.Http;
 using Microsoft.DotNet.Interactive.Tests;
-using Microsoft.DotNet.Interactive.Tests.Utility;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -31,60 +27,5 @@ public class FileProviderTests : LanguageKernelTestBase
 
         file.Should()
             .NotBeNull();
-    }
-
-    [Theory]
-    [InlineData(Language.CSharp)]
-    [InlineData(Language.FSharp)]
-    public async Task It_does_not_track_extensions_that_are_not_file_providers(Language language)
-    {
-
-        var kernel = CreateKernel(language);
-        var provider = new FileProvider(kernel, typeof(Program).Assembly);
-
-        var extensionPackage = await KernelExtensionTestHelper.GetSimpleExtensionAsync();
-
-        await kernel.SubmitCodeAsync($@"
-#i ""nuget:{extensionPackage.PackageLocation}""
-#r ""nuget:{extensionPackage.Name},{extensionPackage.Version}""            ");
-
-        Action action = () => provider.GetFileInfo("extensions/TestKernelExtension/resources/file.txt");
-
-        action.Should().Throw<StaticContentSourceNotFoundException>();
-    }
-
-    [Theory]
-    [InlineData(Language.CSharp)]
-    [InlineData(Language.FSharp)]
-    public async Task It_tracks_extensions_that_are_not_file_providers(Language language)
-    {
-
-        var kernel = CreateKernel(language);
-        var provider = new FileProvider(kernel, typeof(Program).Assembly);
-
-        var extensionPackage = await KernelExtensionTestHelper.GetFileProviderExtensionAsync();
-
-        await kernel.SubmitCodeAsync($@"
-#i ""nuget:{extensionPackage.PackageLocation}""
-#r ""nuget:{extensionPackage.Name},{extensionPackage.Version}""            ");
-
-        var file = provider.GetFileInfo("extensions/TestKernelExtension/resources/file.txt");
-
-        file.Should()
-            .NotBeNull();
-    }
-
-    [Theory]
-    [InlineData(Language.CSharp)]
-    [InlineData(Language.FSharp)]
-    public void it_cannot_resolve_unregistered_extensions(Language language)
-    {
-        var kernel = CreateKernel(language);
-        var provider = new FileProvider(kernel, typeof(Program).Assembly);
-
-        Action action
-            = () => provider.GetFileInfo("extensions/not_found/resources/file.txt");
-
-        action.Should().Throw<StaticContentSourceNotFoundException>();
     }
 }
