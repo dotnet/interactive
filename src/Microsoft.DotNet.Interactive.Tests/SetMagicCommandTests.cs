fbf3c228256730a78123d97369422db3e0780515
Diego Colombo <colombod@me.com>
2023-01-30 14:49:44 +0000

    refactor set magic command as part of the value sharing gesture

--- src/Microsoft.DotNet.Interactive.Tests/SetMagicCommandTests.cs
+++ src/Microsoft.DotNet.Interactive.Tests/SetMagicCommandTests.cs
@@ -19,7 +19,7 @@ public class SetMagicCommandTests
     [Fact]
     public async Task can_set_value_using_return_value()
     {
-        using var kernel = CreateKernel(Language.CSharp).UseSet();
+        using var kernel = CreateKernel(Language.CSharp);
 
         await kernel.SendAsync(new SubmitCode(@"
 #!set --name x --from-result
@@ -35,7 +35,7 @@ public class SetMagicCommandTests
     [Fact]
     public async Task can_set_value_prompting_user()
     {
-        var kernel = CreateKernel(Language.CSharp).UseSet();
+        var kernel = CreateKernel(Language.CSharp);
 
         using var composite = new CompositeKernel();
 
@@ -63,7 +63,7 @@ public class SetMagicCommandTests
     [Fact]
     public async Task can_set_value_from_another_kernel()
     {
-        var csharpKernel = CreateKernel(Language.CSharp).UseSet();
+        var csharpKernel = CreateKernel(Language.CSharp);
         var fsharpKernel = CreateKernel(Language.FSharp);
 
         using var composite = new CompositeKernel
@@ -89,7 +89,7 @@ public class SetMagicCommandTests
     [Fact]
     public async Task set_value_using_return_value_fails_when_successful_code_does_not_produce_return_value()
     {
-        using var kernel = CreateKernel(Language.CSharp).UseSet();
+        using var kernel = CreateKernel(Language.CSharp);
 
         var results  = await kernel.SendAsync(new SubmitCode(@"
 #!set --name x --from-result
@@ -105,7 +105,7 @@ var num = 1+3;"));
     [Fact]
     public async Task does_not_set_value_if_the_submission_fails()
     {
-        using var kernel = CreateKernel(Language.CSharp).UseSet();
+        using var kernel = CreateKernel(Language.CSharp);
 
         var results = await kernel.SendAsync(new SubmitCode(@"
 #!set --name x --from-result
@@ -120,7 +120,7 @@ throw new Exception(""custom error."");"));
     [Fact]
     public async Task set_does_not_allow_from_value_and_from_results_at_the_same_time()
     {
-        using var kernel = CreateKernel(Language.CSharp).UseSet();
+        using var kernel = CreateKernel(Language.CSharp);
 
         var results = await kernel.SendAsync(new SubmitCode(@"
 #!set --name x --from-result --from-value fsharp:y
@@ -134,7 +134,7 @@ throw new Exception(""custom error."");"));
     [Fact]
     public async Task set_requires_from_value_or_from_results_at_the_same_time()
     {
-        using var kernel = CreateKernel(Language.CSharp).UseSet();
+        using var kernel = CreateKernel(Language.CSharp);
 
         var results = await kernel.SendAsync(new SubmitCode(@"
 #!set --name x
