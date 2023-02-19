f62b9086794997e0f5ba4314c6fa33c30ea1a5ad
Shibani Basava <shibanib@microsoft.com>
2023-02-19 13:55:07 -0800

    add tests for request all and bat file to run the tests

---
+++ src/Microsoft.DotNet.Interactive.Jupyter.Tests/LanguageHandlerTests/run_tests.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM this script needs to be run in a conda environment where both ipython and r are installed
+
+REM run python tests using ipython 
+ipython ./tests_python_coe_comm_handler.py
+
