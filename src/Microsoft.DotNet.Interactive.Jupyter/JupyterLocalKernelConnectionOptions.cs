9cb34e6b7e6a05eba9f0aaf2063ebea0fcd776ef
shibanib <shibanib@microsoft.com>
2023-04-06 14:30:16 -0700

    split classes into their own files

---
+++ src/Microsoft.DotNet.Interactive.Jupyter/JupyterLocalKernelConnectionOptions.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Interactive.Jupyter.Connection;
+using Microsoft.DotNet.Interactive.Jupyter.ZMQ;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+
+namespace Microsoft.DotNet.Interactive.Jupyter;
+
+public sealed class JupyterLocalKernelConnectionOptions : IJupyterKernelConnectionOptions
+{
+    private static JupyterConnection _currentJupyterConnection;
+
+    /// <summary>
+    /// Represents connection to the kernels in the current environment
+    /// </summary>
+    private static JupyterConnection CurrentConnection
+    {
+        get
+        {
+            _currentJupyterConnection ??= new(new JupyterKernelSpecModule());
+            return _currentJupyterConnection;
+        }
+    }
+
+    public IJupyterConnection GetConnection(ParseResult connectionOptionsParseResult)
+    {
+        return CurrentConnection;
+    }
+
+    public IReadOnlyCollection<Option> GetOptions()
+    {
+        return Array.Empty<Option>();
+    }
+}
