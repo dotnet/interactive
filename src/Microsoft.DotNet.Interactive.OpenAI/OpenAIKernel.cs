1bdf12dbe1e5a16f04ac813fcef608d6e252874c
Jon Sequeira <jonsequeira@gmail.com>
2023-04-13 22:46:01 -0700

    wip

---
+++ src/Microsoft.DotNet.Interactive.OpenAI/OpenAIKernel.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine;
+using Microsoft.DotNet.Interactive.Commands;
+
+namespace Microsoft.DotNet.Interactive.OpenAI;
+
+public class OpenAIKernel : 
+    Kernel,
+    IKernelCommandHandler<SubmitCode>
+{
+    public OpenAIKernel(string name = "openai") : base(name)
+    {
+        KernelInfo.LanguageName = "text";
+
+        var configureCommand = new Command("#!configure");
+        var useAzureOpenAIOption = new Option<bool>("--use-azure-openai", "Use Azure OpenAI");
+        configureCommand.Add(useAzureOpenAIOption);
+        configureCommand.SetHandler(async ctx =>
+        {
+            var parseResult = ctx.ParseResult;
+            var useAzureOpenAI = parseResult.GetValueForOption(useAzureOpenAIOption);
+            await Authenticate(useAzureOpenAI);
+        });
+        AddDirective(configureCommand);
+    }
+
+    private static async Task Authenticate(bool useAzureOpenAI)
+    {
+        await Settings.AskAzureEndpoint(useAzureOpenAI);
+        await Settings.AskModel(useAzureOpenAI);
+        await Settings.AskApiKey(useAzureOpenAI);
+    }
+
+    public Task HandleAsync(SubmitCode command, KernelInvocationContext context)
+    {
+        
+    }
+}
---
+++ src/Microsoft.DotNet.Interactive.OpenAI/OpenAIKernelConnector.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using Microsoft.DotNet.Interactive.Connection;
+using Microsoft.SemanticKernel;
+
+namespace Microsoft.DotNet.Interactive.OpenAI;
+
+public class OpenAIKernelConnector
+{
+    public static async Task AddKernelConnector()
+    {
+        if (KernelInvocationContext.Current is { } context &&
+            context.HandlingKernel.RootKernel is CompositeKernel root)
+        {
+            var config = new KernelConfig();
+
+            root.AddKernelConnector(new ConnectOpenAICommand());
+        }
+    }
+}
+
+public class ConnectOpenAICommand : ConnectKernelCommand
+{
+    public ConnectOpenAICommand() : base("openai", "Connects a kernel that can be used to run OpenAI prompts")
+    {
+    }
+
+    public Option<>
+
+    public override async Task<Kernel> ConnectKernelAsync(
+        KernelInvocationContext context,
+        InvocationContext commandLineContext)
+    {
+        var name = commandLineContext.ParseResult.GetValueForOption(KernelNameOption);
+
+
+
+
+        return new OpenAIKernel();
+    }
+}
