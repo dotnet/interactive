d58446bf127ed640574d46e4df6d14c2632f28df
Diego Colombo <dicolomb@microsoft.com>
2023-04-20 21:55:12 +0100

    progress...

--- src/Microsoft.DotNet.Interactive.OpenAI.Tests/EmbeddingKernelIntegrationTests.cs
+++ src/Microsoft.DotNet.Interactive.OpenAI.Tests/EmbeddingKernelIntegrationTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Events;
 using Microsoft.DotNet.Interactive.OpenAI.Configuration;
@@ -28,7 +29,7 @@ public class EmbeddingKernelIntegrationTests
     }
 
     [AIIngregrationTestFact]
-    public async Task Embeddings_are_retrieved_implicitly_when_something_something()
+    public async Task Embeddings_are_used_when_the_context_is_declared()
     {
         using var root = new CompositeKernel();
 
@@ -46,27 +47,45 @@ public class EmbeddingKernelIntegrationTests
 
         await root.SendAsync(new SubmitCode($"""
             #!{embeddingsKernel.Name} --name fact2
-            .NET Interactive is developed by a team of engineers at Microsoft and open source contributors.
+            .NET Interactive is developed by a team of engineers at Microsoft.
+            """));
+
+        await root.SendAsync(new SubmitCode($"""
+            #!{embeddingsKernel.Name} --name fact3
+            .NET Interactive is an open source project with a lot of contributors.
+            """));
+
+        await root.SendAsync(new SubmitCode($"""
+            #!{embeddingsKernel.Name} --name fact4
+            .NET Interactive is used by engineers.
+            """));
+
+
+        await root.SendAsync(new SubmitCode($"""
+            #!{embeddingsKernel.Name} --name fact5
+            .NET Interactive is developed by open source community.
+            """));
+
+        await root.SendAsync(new SubmitCode($"""
+            #!{embeddingsKernel.Name} --name fact6
+            .NET Interactive is used by software engineers.
+            """));
+
+        await root.SendAsync(new SubmitCode($"""
+            #!{embeddingsKernel.Name} --name fact7
+            .NET Interactive can create rich outputs and visualizations.
             """));
 
         var textCompletionKernel = root.ChildKernels.OfType<TextCompletionKernel>().Single();
 
         var result = await textCompletionKernel.SendAsync(new SubmitCode("""
+            #!conversation-context .NET Interactive open source
             Who is developing a polyglot kernel?
             """));
 
         var skContext = result.Events.Should().ContainSingle<ReturnValueProduced>()
                               .Which.Value.As<SKContext>();
 
-        var variables = skContext.Variables;
-
-        variables.ContainsKey("fact1").Should().BeTrue();
-        variables.ContainsKey("fact2").Should().BeTrue();
-
-
-
-
-        // TODO (testname) write test
-        throw new NotImplementedException();
+        skContext.Result.Should().Contain("Microsoft").And.Contain("community");
     }
 }
--- src/Microsoft.DotNet.Interactive.OpenAI/TextCompletionKernel.cs
+++ src/Microsoft.DotNet.Interactive.OpenAI/TextCompletionKernel.cs
@@ -17,13 +17,18 @@ public class TextCompletionKernel :
     IKernelCommandHandler<SubmitCode>,
     IKernelCommandHandler<SendValue>
 {
-    private readonly TextEmbeddingGenerationKernel? _embeddingsKernel;
-    private readonly Command _promptCommand = new("#!use-skills");
-    private readonly Dictionary<string, string> _values = new();
-    private readonly Argument<string[]> _functionPipelineArgument = new("pipeline");
+    private readonly Command _conversationContextCommand = new("#!conversation-context");
+    private readonly Argument<string[]> _conversationContextArgument = new("arguments");
+
+    private readonly Command _useSkillsCommand = new("#!use-skills");
+    private readonly Argument<string[]> _skillsArgument = new("pipeline");
     private readonly Option<bool> _usePlannerOption = new("--use-planner");
+
+    private readonly TextEmbeddingGenerationKernel? _embeddingsKernel;
+    private readonly Dictionary<string, string> _values = new();
     private string[]? _functionNamesForPipeline;
     private bool _usePlanner;
+    private string[]? _contextForMemory;
 
     public TextCompletionKernel(
         IKernel semanticKernel,
@@ -36,20 +41,30 @@ public class TextCompletionKernel :
         KernelInfo.LanguageName = "text";
         KernelInfo.DisplayName = $"{Name} - {modelName}";
 
-        _functionPipelineArgument.AddCompletions(context => SemanticKernel.GetFunctionNames());
+        _skillsArgument.AddCompletions(context => SemanticKernel.GetFunctionNames());
 
-        _promptCommand.Add(_functionPipelineArgument);
-        _promptCommand.Add(_usePlannerOption);
+        _useSkillsCommand.Add(_skillsArgument);
+        _useSkillsCommand.Add(_usePlannerOption);
 
-        _promptCommand.SetHandler(context =>
+        _useSkillsCommand.SetHandler(context =>
         {
-            _functionNamesForPipeline = context.ParseResult.GetValueForArgument(_functionPipelineArgument);
+            _functionNamesForPipeline = context.ParseResult.GetValueForArgument(_skillsArgument);
             _usePlanner = context.ParseResult.GetValueForOption(_usePlannerOption);
         });
 
-        AddDirective(_promptCommand);
+        _conversationContextCommand.Add(_conversationContextArgument);
+
+        _conversationContextCommand.SetHandler(context =>
+        {
+            _contextForMemory = context.ParseResult.GetValueForArgument(_conversationContextArgument);
+        });
+
+        AddDirective(_useSkillsCommand);
+
+        AddDirective(_conversationContextCommand);
 
         this.UseValueSharing();
+
     }
 
     public IKernel SemanticKernel { get; }
@@ -72,27 +87,53 @@ public class TextCompletionKernel :
             skContext.Variables.Set("INPUT", submitCode.Code);
 
             var pipeline = new List<ISKFunction>();
-
-            if (_embeddingsKernel is not null)
+            var hasFacts = false;
+            if (_embeddingsKernel is not null && _contextForMemory?.Length > 1)
             {
                 var sb = new StringBuilder();
-
-                foreach (var id in _embeddingsKernel.EmbeddingIds)
+                var uniqueFacts = new HashSet<string>();
+                var factCount = 0;
+                foreach (var memoryContext in _contextForMemory)
                 {
-                    skContext.Variables.Set(id, id);
-                    sb.AppendLine($$$"""
-                    {{retrieve ${{{id}}}}} 
+                    var memories = 
+                        SemanticKernel.Memory.SearchAsync(TextEmbeddingGenerationKernel.DefaultMemoryCollectionName,
+                            memoryContext, 5);
 
-                    """);
+                    await foreach (var memory in memories)
+                    {
+                        var fact = memory.Metadata.Text;
+                        if (uniqueFacts.Add(fact))
+                        {
+                            if (!hasFacts)
+                            {
+                                hasFacts = true;
+                                sb.AppendLine("""
+                                    Given the following facts:
+
+                                    """);
+                            }
+                            sb.AppendLine($"""
+                            -{++factCount} {fact}
+
+                            """);
+                        }
+                    }
                 }
 
-                sb.AppendLine("{{$INPUT}}");
+                if (hasFacts)
+                {
+                    sb.AppendLine("""
+                                    then:
 
+                                    """);
+                }
+                sb.AppendLine("{{$INPUT}}");
+                var code = sb.ToString();
                 var semanticFunction = SemanticKernel.CreateSemanticFunction(
-                    sb.ToString(),
+                    code,
                     skillName: FixUpCharacters(Name),
                     functionName: "loadmemory");
-
+                
                 pipeline.Add(semanticFunction);
 
                 string FixUpCharacters(string name)
@@ -101,7 +142,7 @@ public class TextCompletionKernel :
                 }
             }
 
-            if (_functionNamesForPipeline is null || _functionNamesForPipeline.Length == 0)
+            else if (_functionNamesForPipeline is null || _functionNamesForPipeline.Length == 0)
             {
                 var semanticFunction = SemanticKernel.CreateSemanticFunction("""
                     {{$INPUT}}
