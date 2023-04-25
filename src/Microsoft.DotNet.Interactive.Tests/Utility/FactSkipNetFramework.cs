1be6dafa6dcbf07d7bfcd9bab68aac47a148735e
Shyam Namboodiripad <gnamboo@microsoft.com>
2023-04-24 21:08:22 -0700

    Include KernelSchedulerTests.cs.
    Also, skip flaky cancellation test. On my machine this test passes if executed by itself but fails when executed as part of a test suite along with other tests.

---
+++ src/Microsoft.DotNet.Interactive.Tests/Utility/FactSkipNetFramework.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.Tests.Utility;
+
+public sealed class FactSkipNetFramework : FactAttribute
+{
+    public FactSkipNetFramework(string reason = null)
+    {
+        if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase))
+        {
+            Skip = string.IsNullOrWhiteSpace(reason) ? "Ignored on .NET Framework" : reason;
+        }
+    }
+}
