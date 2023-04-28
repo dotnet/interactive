6e8fe7375872257916cba53b76dae00e3817d8bd
Diego Colombo <dicolomb@microsoft.com>
2023-04-28 18:42:12 +0100

    enable jupyter

--- src/Microsoft.DotNet.Interactive.Jupyter.Tests/JupyterKernelTests.cs
+++ src/Microsoft.DotNet.Interactive.Jupyter.Tests/JupyterKernelTests.cs
@@ -155,7 +155,7 @@ public class JupyterKernelTests : JupyterKernelTestBase
         options.Connection.KernelConnection.IsDisposed.Should().BeTrue();
     }
 
-    [Fact(Skip = "fix test")]
+    [Fact]
     public async Task can_cancel_submit_code_and_interrupt_kernel()
     {
         var options = new TestJupyterConnectionOptions(GenerateReplies(new[] {
