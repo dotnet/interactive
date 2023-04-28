29c11d365f2780271e1cb2ad62ccd15375878b17
Shyam Namboodiripad <gnamboo@microsoft.com>
2023-04-28 11:39:40 -0700

    Add test for http formatter.

--- src/Microsoft.DotNet.Interactive.HttpRequest/HttpRequestKernelExtension.cs
+++ src/Microsoft.DotNet.Interactive.HttpRequest/HttpRequestKernelExtension.cs
@@ -1,17 +1,18 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Net.Http;
 using Microsoft.DotNet.Interactive.Formatting;
 
 namespace Microsoft.DotNet.Interactive.HttpRequest;
 
 public class HttpRequestKernelExtension
 {
-    public static void Load(Kernel kernel)
+    public static void Load(Kernel kernel, HttpClient? httpClient = null)
     {
         if (kernel.RootKernel is CompositeKernel compositeKernel)
         {
-            var httpRequestKernel = new HttpRequestKernel();
+            var httpRequestKernel = new HttpRequestKernel(client: httpClient);
             compositeKernel.Add(httpRequestKernel);
             httpRequestKernel.UseValueSharing();
 
@@ -29,8 +30,7 @@ public class HttpRequestKernelExtension
 
     private static void RegisterFormatters()
     {
-        // TODO: Add Json.
-        Formatter.SetPreferredMimeTypesFor(typeof(HttpResponse), HtmlFormatter.MimeType, PlainTextFormatter.MimeType);
+        Formatter.SetPreferredMimeTypesFor(typeof(HttpResponse), HtmlFormatter.MimeType, PlainTextFormatter.MimeType, JsonFormatter.MimeType);
 
         Formatter.Register<HttpResponse>(
             (value, context) =>
