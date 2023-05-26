5eaa34840dd740136d02c852678b63571d848f52
Diego Colombo <dicolomb@microsoft.com>
2023-05-26 22:03:20 +0100

    fix compiler error

--- src/Microsoft.DotNet.Interactive/Commands/KernelCommand.cs
+++ src/Microsoft.DotNet.Interactive/Commands/KernelCommand.cs
@@ -5,11 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine.Parsing;
 using System.Threading.Tasks;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.Interactive.Commands;
 
