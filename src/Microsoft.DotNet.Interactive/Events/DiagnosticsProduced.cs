3e52274fba67ea23b834111c0f562840b2bf4961
Shyam Namboodiripad <gnamboo@microsoft.com>
2023-06-01 21:31:15 -0700

    Make it an error to construct DiagnosticsProduced with 0 diagnostics.

--- src/Microsoft.DotNet.Interactive/Events/DiagnosticsProduced.cs
+++ src/Microsoft.DotNet.Interactive/Events/DiagnosticsProduced.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.DotNet.Interactive.Commands;
 
 namespace Microsoft.DotNet.Interactive.Events;
@@ -16,7 +17,16 @@ public class DiagnosticsProduced : KernelEvent
         KernelCommand command,
         IReadOnlyCollection<FormattedValue> formattedDiagnostics = null) : base(command)
     {
-        _diagnostics = (diagnostics ?? Array.Empty<Diagnostic>()).ToImmutableList();
+        if (diagnostics is null)
+        {
+            throw new ArgumentNullException(nameof(diagnostics));
+        }
+        else if (!diagnostics.Any())
+        {
+            throw new ArgumentException("At least one diagnostic required.", nameof(diagnostics));
+        }
+
+        _diagnostics = diagnostics.ToImmutableList();
         FormattedDiagnostics = formattedDiagnostics ?? Array.Empty<FormattedValue>();
     }
 
