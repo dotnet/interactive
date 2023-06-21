e0ba1795c6fae3ea4391f5b19a6c5ea09dc35078
Shyam Namboodiripad <gnamboo@microsoft.com>
2023-06-21 02:28:40 -0700

    Add back null-forgiving operator.
    This allows code in an external repo that links in this source file to compile without errors.

--- src/dotnet-interactive/Pocket/Format.CustomizeLogString.cs
+++ src/dotnet-interactive/Pocket/Format.CustomizeLogString.cs
@@ -332,7 +332,7 @@ internal static partial class Format
             return string.Empty;
         }
 
-        var lines = value.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        var lines = value!.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
         var firstLine = lines.FirstOrDefault();
 
         if (string.IsNullOrEmpty(firstLine))
