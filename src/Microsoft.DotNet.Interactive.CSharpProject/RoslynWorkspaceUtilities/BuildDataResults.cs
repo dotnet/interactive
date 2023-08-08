0110e6caacc167deb1385be940bb1fd51bf1dad1
Osvaldo Calles <oscalles@microsoft.com>
2023-08-08 13:40:16 -0700

    Add utilities to create Roslyn Workspace without binlogs

---
+++ src/Microsoft.DotNet.Interactive.CSharpProject/RoslynWorkspaceUtilities/BuildDataResults.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.DotNet.Interactive.CSharpProject.RoslynWorkspaceUtilities.RoslynWorkspaceUtilities;
+
+namespace Microsoft.DotNet.Interactive.CSharpProject.RoslynWorkspaceUtilities;
+
+public class BuildDataResults
+{
+    public BuildProjectData BuildProjectData { get; set; }
+
+    public CodeAnalysis.Workspace Workspace { get; set; }
+
+    public bool Succeeded { get; set; }
+
+    public string CacheFilePath { get; set; }
+
+    public CSharpParseOptions CSharpParseOptions { get; set; }
+
+    // TODO: Set this value
+    public string ProjectFilePath { get; set; }
+
+    internal Dictionary<string, BuildDataProjectItem[]> Items { get; set; } = new Dictionary<string, BuildDataProjectItem[]>();
+}
---
+++ src/Microsoft.DotNet.Interactive.CSharpProject/RoslynWorkspaceUtilities/RoslynWorkspaceUtilities.cs
@@ -0,0 +1,332 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static Microsoft.DotNet.Interactive.CSharpProject.RoslynWorkspaceUtilities.BuildCacheFileUtilities;
+
+namespace Microsoft.DotNet.Interactive.CSharpProject.RoslynWorkspaceUtilities;
+
+internal static class RoslynWorkspaceUtilities
+{
+    internal static BuildDataResults GetResultsFromCacheFile(string cacheFilePath)
+    {
+        CodeAnalysis.Workspace ws = null;
+        BuildProjectData buildProjectData = null;
+        CSharpParseOptions cSharpParseOptions = null;
+
+        try
+        {
+            if (!File.Exists(cacheFilePath))
+            {
+                return null;
+            }
+
+            string fileContent = File.ReadAllText(cacheFilePath);
+
+            PopulateBuildProjectData(fileContent, out buildProjectData);
+
+            ws = GetWorkspace(buildProjectData, true);
+
+            cSharpParseOptions = CreateCSharpParseOptions(buildProjectData);
+
+            return new BuildDataResults()
+            {
+                BuildProjectData = buildProjectData,
+                ProjectFilePath = cacheFilePath,
+                CacheFilePath = cacheFilePath + cacheFilenameSuffix,
+                Workspace = ws,
+                Succeeded = true,
+                CSharpParseOptions = cSharpParseOptions
+            };
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    internal static BuildDataResults GetResultsFromProjectFile(string csprojFilePath)
+    {
+        if (!File.Exists(csprojFilePath))
+        {
+            return null;
+        }
+
+        var cacheFilePath = csprojFilePath + cacheFilenameSuffix;
+
+        return GetResultsFromCacheFile(cacheFilePath);
+    }
+
+    internal static void PopulateBuildProjectData(string fileContent, out BuildProjectData buildProjectData)
+    {
+        buildProjectData = new BuildProjectData();
+
+        string[] lines = fileContent.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        List<string> referencesList = new List<string>();
+        List<string> projectReferencesList = new List<string>();
+        List<string> analyzerReferencesList = new List<string>();
+        List<string> preprocessorSymbolsList = new List<string>();
+        List<string> sourceFilesList = new List<string>();
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('=');
+            if (parts.Length == 2)
+            {
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+
+                switch (key)
+                {
+                    case "ProjectGuid":
+                        if (Guid.TryParse(value, out Guid projectGuid))
+                        {
+                            buildProjectData.ProjectGuid = projectGuid;
+                        }
+                        else
+                        {
+                            // Net Core projects do not provide a project guid, so lets create one.
+                            // Project guid is not relevant for build.
+                            buildProjectData.ProjectGuid = Guid.NewGuid();
+                        }
+                        break;
+                    case "ProjectReferences":
+                        projectReferencesList.Add(value);
+                        break;
+                    case "ProjectFilePath":
+                        buildProjectData.ProjectFilePath = value;
+                        break;
+                    case "LanguageName":
+                        buildProjectData.LanguageName = value;
+                        break;
+                    case "PropertyTargetPath":
+                        buildProjectData.TargetPath = value;
+                        break;
+                    case "SourceFiles":
+                        sourceFilesList.Add(value);
+                        break;
+                    case "References":
+                        referencesList.Add(value);
+                        break;
+                    case "AnalyzerReferences":
+                        analyzerReferencesList.Add(value);
+                        break;
+                    case "PreprocessorSymbols":
+                        preprocessorSymbolsList.Add(value);
+                        break;
+                    case "PropertyLangVersion":
+                        buildProjectData.LangVersion = value;
+                        break;
+                    case "PropertyOutputType":
+                        buildProjectData.OutputType = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        buildProjectData.References = referencesList.ToArray();
+        buildProjectData.ProjectReferences = projectReferencesList.ToArray();
+        buildProjectData.AnalyzerReferences = analyzerReferencesList.ToArray();
+        buildProjectData.PreprocessorSymbols = preprocessorSymbolsList.ToArray();
+        buildProjectData.SourceFiles = sourceFilesList.ToArray();
+    }
+
+    public static AdhocWorkspace GetWorkspace(BuildProjectData buildProjectData, bool addProjectReferences = false)
+    {
+        if (buildProjectData is null)
+        {
+            throw new ArgumentNullException(nameof(buildProjectData));
+        }
+        AdhocWorkspace workspace = CreateWorkspace();
+        AddToWorkspace(buildProjectData, workspace, addProjectReferences);
+        return workspace;
+    }
+
+    public static CodeAnalysis.Project AddToWorkspace(BuildProjectData analyzerResult, CodeAnalysis.Workspace workspace, bool addProjectReferences = false)
+    {
+        if (analyzerResult is null)
+        {
+            throw new ArgumentNullException(nameof(analyzerResult));
+        }
+        if (workspace is null)
+        {
+            throw new ArgumentNullException(nameof(workspace));
+        }
+
+        ProjectId projectId = ProjectId.CreateFromSerialized(analyzerResult.ProjectGuid);
+
+        ConcurrentDictionary<Guid, string[]> WorkspaceProjectReferences = new ConcurrentDictionary<Guid, string[]>();
+        WorkspaceProjectReferences[projectId.Id] = analyzerResult.ProjectReferences.ToArray();
+
+        ProjectInfo projectInfo = GetProjectInfo(analyzerResult, workspace, projectId);
+        if (projectInfo is null)
+        {
+            // Error
+            return null;
+        }
+        Solution solution = workspace.CurrentSolution.AddProject(projectInfo);
+
+        if (!workspace.TryApplyChanges(solution))
+        {
+            throw new InvalidOperationException("Could not apply workspace solution changes");
+        }
+
+        return workspace.CurrentSolution.GetProject(projectId);
+    }
+
+    internal static AdhocWorkspace CreateWorkspace()
+    {
+        AdhocWorkspace workspace = new AdhocWorkspace();
+        workspace.WorkspaceChanged += WorkspaceChangedHandler;
+        workspace.WorkspaceFailed += WorkspaceFailedHandler;
+        return workspace;
+    }
+
+    private static void WorkspaceFailedHandler(object sender, WorkspaceDiagnosticEventArgs e)
+    {
+        // Log error
+    }
+
+    private static void WorkspaceChangedHandler(object sender, WorkspaceChangeEventArgs e)
+    {
+        // Log
+    }
+
+    private static ProjectInfo GetProjectInfo(BuildProjectData buildProjectData, CodeAnalysis.Workspace workspace, ProjectId projectId)
+    {
+        string projectName = Path.GetFileNameWithoutExtension(buildProjectData.ProjectFilePath);
+        if (!TryGetSupportedLanguageName(buildProjectData.ProjectFilePath, out string languageName))
+        {
+            return null;
+        }
+        return ProjectInfo.Create(
+            projectId,
+            VersionStamp.Create(),
+            projectName,
+            projectName,
+            languageName,
+            filePath: buildProjectData.ProjectFilePath,
+            outputFilePath: buildProjectData.TargetPath,
+            documents: GetDocuments(buildProjectData, projectId),
+            projectReferences: GetExistingProjectReferences(buildProjectData, workspace),
+            metadataReferences: GetMetadataReferences(buildProjectData),
+            analyzerReferences: GetAnalyzerReferences(buildProjectData, workspace),
+            parseOptions: CreateParseOptions(buildProjectData, languageName),
+            compilationOptions: CreateCompilationOptions(buildProjectData, languageName));
+    }
+
+    private static bool TryGetSupportedLanguageName(string projectPath, out string languageName)
+    {
+        switch (Path.GetExtension(projectPath))
+        {
+            case ".csproj":
+                languageName = LanguageNames.CSharp;
+                return true;
+            default:
+                languageName = null;
+                return false;
+        }
+    }
+
+    private static IReadOnlyList<DocumentInfo> GetDocuments(BuildProjectData buildProjectData, ProjectId projectId) =>
+        buildProjectData
+            .SourceFiles?
+                .Where(File.Exists)
+                .Select(x => DocumentInfo.Create(
+                    DocumentId.CreateNewId(projectId),
+                    Path.GetFileName(x),
+                    loader: TextLoader.From(
+                        TextAndVersion.Create(
+                        SourceText.From(File.ReadAllText(x), Encoding.Unicode), VersionStamp.Create())),
+                    filePath: x)).ToList();
+
+    private static IReadOnlyList<ProjectReference> GetExistingProjectReferences(BuildProjectData buildProjectData, CodeAnalysis.Workspace workspace) =>
+        buildProjectData.ProjectReferences
+            .Select(x => workspace.CurrentSolution.Projects.FirstOrDefault(y => y.FilePath.Equals(x, StringComparison.OrdinalIgnoreCase)))
+            .Where(x => x != null)
+            .Select(x => new ProjectReference(x.Id)).ToList();
+
+    private static IReadOnlyList<MetadataReference> GetMetadataReferences(BuildProjectData buildProjectData) =>
+        buildProjectData
+            .References?.Where(File.Exists)
+            .Select(x => MetadataReference.CreateFromFile(x)).ToList();
+
+    private static IReadOnlyList<AnalyzerReference> GetAnalyzerReferences(BuildProjectData buildProjectData, CodeAnalysis.Workspace workspace)
+    {
+        IAnalyzerAssemblyLoader loader = workspace.Services.GetRequiredService<IAnalyzerService>().GetLoader();
+
+        return buildProjectData
+                .AnalyzerReferences?
+                    .Where(File.Exists)
+                    .Select(x => new AnalyzerFileReference(x, loader)).ToList();
+    }
+
+    private static ParseOptions CreateParseOptions(BuildProjectData buildProjectData, string languageName)
+    {
+        if (languageName == LanguageNames.CSharp)
+        {
+            return CreateCSharpParseOptions(buildProjectData);
+        }
+
+        return null;
+    }
+
+    private static CSharpParseOptions CreateCSharpParseOptions(BuildProjectData buildProjectData)
+    {
+        CSharpParseOptions parseOptions = new CSharpParseOptions();
+
+        parseOptions = parseOptions.WithPreprocessorSymbols(buildProjectData.PreprocessorSymbols);
+
+        string langVersion = buildProjectData.LangVersion;
+        if (!string.IsNullOrWhiteSpace(langVersion)
+            && LanguageVersionFacts.TryParse(langVersion, out LanguageVersion languageVersion))
+        {
+            parseOptions = parseOptions.WithLanguageVersion(languageVersion);
+        }
+
+        return parseOptions;
+    }
+
+    private static CompilationOptions CreateCompilationOptions(BuildProjectData buildProjectData, string languageName)
+    {
+        string outputType = buildProjectData.OutputType;
+        OutputKind? kind = null;
+        switch (outputType)
+        {
+            case "Library":
+                kind = OutputKind.DynamicallyLinkedLibrary;
+                break;
+            case "Exe":
+                kind = OutputKind.ConsoleApplication;
+                break;
+            case "Module":
+                kind = OutputKind.NetModule;
+                break;
+            case "Winexe":
+                kind = OutputKind.WindowsApplication;
+                break;
+        }
+
+        if (kind.HasValue)
+        {
+            if (languageName == LanguageNames.CSharp)
+            {
+                CompilationOptions CreateCSharpCompilationOptions() => new CSharpCompilationOptions(kind.Value);
+
+                return CreateCSharpCompilationOptions();
+            }
+        }
+
+        return null;
+    }
+}
