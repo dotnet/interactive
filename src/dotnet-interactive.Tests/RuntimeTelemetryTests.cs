9e0b1e321f18bf94ee886d8e38a5e13bf20032c8
Diego Colombo <dicolomb@microsoft.com>
2023-08-14 20:22:11 +0100

    change noda time version

--- src/dotnet-interactive.Tests/RuntimeTelemetryTests.cs
+++ src/dotnet-interactive.Tests/RuntimeTelemetryTests.cs
@@ -150,7 +150,7 @@ telemetry
     [Fact]
     public async Task Package_and_version_number_are_sent_on_successful_package_load()
     {
-        var results = await _kernel.SendAsync(new SubmitCode("#r \"nuget:NodaTime,3.1.0\"", "csharp"));
+        var results = await _kernel.SendAsync(new SubmitCode("#r \"nuget:NodaTime,3.0.9\"", "csharp"));
 
         results.Events.Should().NotContainErrors();
 
@@ -162,6 +162,6 @@ telemetry
                         .Should()
                         .Contain(
                             new KeyValuePair<string, string>("PackageName", "nodatime".ToSha256HashWithNormalizedCasing()),
-                            new KeyValuePair<string, string>("PackageVersion", "3.1.0".ToSha256Hash()));
+                            new KeyValuePair<string, string>("PackageVersion", "3.0.9".ToSha256Hash()));
     }
 }
