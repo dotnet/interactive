25d4a7cfe6eda10728bafef06b894ab384228a5c
Diego Colombo <dicolomb@microsoft.com>
2023-10-04 21:59:52 +0100

    clean up api design for similarity comparer

--- src/Microsoft.DotNet.Interactive.AIUtilities.Tests/SimilaritySearchTests.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/SimilaritySearchTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections;
 using FluentAssertions;
 using Xunit;
 
@@ -20,13 +19,35 @@ public class SimilaritySearchTests
             "chuck"
         };
 
-        Func<string, float[]> stringToFloatArray = a => a.Select(c => (float)c).ToArray();
-        var search = collection.Search(
+        var search = collection.OrderBySimilarity(
             "diego",
-            new CosineSimilarityComparer<string>(stringToFloatArray),
-            stringToFloatArray
-        );
+            new CosineSimilarityComparer<string>(a => a.Select(c => (float)c).ToArray())
+        ).Take(1);
 
-        search.Should().BeEquivalentTo(new [] { new ScoredItem<string>("diago", 0.9998783f) });
-    } 
+        search.Should().BeEquivalentTo(new [] { KeyValuePair.Create("diago", 0.9998783f) });
+    }
+
+    [Fact]
+    public void can_search_collection_for_similar_element_f()
+    {
+        var vectorCollection = new List<float[]>
+        {
+            new []{1f,0f},
+            new []{1f,1.5f},
+            new []{1f,1.7f},
+            new []{1f,1.9f}
+        };
+        var search = vectorCollection.OrderBySimilarity(
+            new[] { 1f, 1f },
+            new CosineSimilarityComparer<float[]>(t => t)
+        ).Take(2);
+
+        var expected = new[]
+        {
+            KeyValuePair.Create(  new  [] { 1f,1.5f },  0.9805807f ),
+            KeyValuePair.Create( new[] { 1f,1.7f },  0.9679969f )
+        };
+
+        search.Should().BeEquivalentTo(expected);
+    }
 }
--- src/Microsoft.DotNet.Interactive.AIUtilities/ISimilarityComparer.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities/ISimilarityComparer.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// ReSharper disable once CheckNamespace
 
-namespace System.Collections;
 
-public interface ISimilarityComparer<in T, in TQuery>
+namespace Microsoft.DotNet.Interactive.AIUtilities;
+
+public interface ISimilarityComparer<in T>
 {
     public float Score(T a, T b);
-    public float Score(T a, TQuery query);
 }
