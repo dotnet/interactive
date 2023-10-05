4e2c4bdb3710b65cca47dbebba4fc6d665345b1b
Diego Colombo <dicolomb@microsoft.com>
2023-10-05 09:43:17 +0100

    support averaging token count in chunks

--- src/Microsoft.DotNet.Interactive.AIUtilities.Tests/TextChunkingTests.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/TextChunkingTests.cs
@@ -64,6 +64,27 @@ public class TextChunkingTests
     }
 
     [Fact]
+    public async Task can_create_chunks_by_token_count_using_average_size()
+    {
+        var chunks = await LongText.ChunkByTokenCountAsync(maxTokenCount: 20, model: TokenizerModel.ada2, true);
+        chunks.Should().BeEquivalentTo(new[]
+        {
+          "Call me Ishmael. Some years ago—never mind how long precisely—having little or no", 
+          " money in my purse, and nothing particular to interest me on shore, I thought I would sail about", 
+          " a little and see the watery part of the world. It is a way I have of driving", 
+          " off the spleen, and regulating the circulation. Whenever I find myself growing grim about the mouth;", 
+          " whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily", 
+          " pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever", 
+          " my hypos get such an upper hand of me, that it requires a strong moral principle to prevent", 
+          " me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I", 
+          " account it high time to get to sea as soon as I can. This is my substitute for pistol", 
+          " and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the", 
+          " ship. There is nothing surprising in this. If they but knew it, almost all men in their", 
+          " degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
+        });
+    }
+
+    [Fact]
     public async Task can_create_overlapping_chunks_by_token_count()
     {
         var chunks = await LongText.ChunkByTokenCountWithOverlapAsync(maxTokenCount: 80, overlapTokenCount: 10, model: TokenizerModel.ada2);
@@ -73,6 +94,19 @@ public class TextChunkingTests
             " Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and method", 
             " me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their",
             " they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
+        });
+    }
+
+    [Fact]
+    public async Task can_create_overlapping_chunks_by_token_count_using_average_size()
+    {
+        var chunks = await LongText.ChunkByTokenCountWithOverlapAsync(maxTokenCount: 80, overlapTokenCount: 10, model: TokenizerModel.ada2, true);
+        chunks.Should().BeEquivalentTo(new[]
+        {
+            "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen, and regulating the",
+            " of driving off the spleen, and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an",
+            "; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With",
+            " This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
         });
     }
 
