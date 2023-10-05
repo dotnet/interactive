3601576c33afc5fb7c8c44c8448a145136e212c5
Jon Sequeira <jonsequeira@gmail.com>
2023-10-05 13:17:35 -0700

    add .dib for extension

---
+++ src/Microsoft.DotNet.Interactive.AIUtilities/InteractiveExtension.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.ComponentModel;
+
+namespace Microsoft.DotNet.Interactive.AIUtilities;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class InteractiveExtension
+{
+    public static async Task LoadAsync()
+    {
+
+      
+
+
+    }
+}
