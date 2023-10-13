2a627286fde5b551cf48a95240b6832ecfcba5d5
Diego Colombo <dicolomb@microsoft.com>
2023-10-13 17:23:16 +0100

    test execution

--- src/Microsoft.DotNet.Interactive.AIUtilities.Tests/FunctionTests.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/FunctionTests.cs
@@ -19,9 +19,9 @@ public class FunctionTests
     [Fact]
     public void can_create_function_from_delegate_with_no_return_type()
     {
-        var declaration = GPTFunctioDefinition.Do((int a, string b, string[] c) => {}, "DoCompute");
+        var declaration = GPTFunctionDefinition.Create((int a, string b, string[] c) => {}, "DoCompute");
 
-        declaration.Should().Be("""
+        declaration.Signature.Should().Be("""
                                 {
                                   "name": "DoCompute",
                                   "parameters": {
@@ -55,9 +55,9 @@ public class FunctionTests
     [Fact]
     public void can_create_function_from_delegate()
     {
-        var declaration = GPTFunctioDefinition.Do((int a, string b, string[]c) => $"{a} {b} {c}", "DoCompute");
+        var declaration = GPTFunctionDefinition.Create((int a, string b, string[]c) => $"{a} {b} {c}", "DoCompute");
 
-        declaration.Should().Be("""
+        declaration.Signature.Should().Be("""
                                 {
                                   "name": "DoCompute",
                                   "parameters": {
@@ -94,9 +94,9 @@ public class FunctionTests
     [Fact]
     public void can_create_function_from_delegate_with_enums_as_parameters()
     {
-        var declaration = GPTFunctioDefinition.Do((int a, double b, EnumType c) => $"{a} {b} {c}", "DoCompute");
+        var declaration = GPTFunctionDefinition.Create((int a, double b, EnumType c) => $"{a} {b} {c}", "DoCompute");
 
-        declaration.Should().Be("""
+        declaration.Signature.Should().Be("""
                                 {
                                   "name": "DoCompute",
                                   "parameters": {
@@ -136,9 +136,9 @@ public class FunctionTests
     [Fact]
     public void can_create_function_from_delegate_with_array_of_enums_as_parameters()
     {
-        var declaration = GPTFunctioDefinition.Do((byte a, bool b, EnumType[] c) => $"{a} {b} {c}", "DoCompute");
+        var declaration = GPTFunctionDefinition.Create((byte a, bool b, EnumType[] c) => $"{a} {b} {c}", "DoCompute");
 
-        declaration.Should().Be("""
+        declaration.Signature.Should().Be("""
                                 {
                                   "name": "DoCompute",
                                   "parameters": {
@@ -176,5 +176,53 @@ public class FunctionTests
                                 """);
 
 
+    }
+
+    [Fact]
+    public void can_invoke_function()
+    {
+        var function = GPTFunctionDefinition.Create((string a, double b) => $"{a} {b}", "concatString");
+
+        var jsonArgs = """
+                        {
+                            "name": "concatString",
+                            "arguments": "{ \"a\": \"Diego\", \"b\":123.0}"
+                        }
+                       """;
+        
+        function.Execute<string>(jsonArgs, out var result);
+        result.Should().Be("Diego 123");
+    }
+
+    [Fact]
+    public void can_invoke_function_2()
+    {
+        var function = GPTFunctionDefinition.Create((string uriString) =>  new Uri(uriString), "concatString");
+
+        var jsonArgs = """
+                        {
+                            "name": "concatString",
+                            "arguments": "{ \"uriString\": \"http://www.microsoft.com\"}"
+                        }
+                       """;
+
+        function.Execute<Uri>(jsonArgs, out var result);
+        result.Should().Be("http://www.microsoft.com");
+    }
+
+    [Fact]
+    public void can_invoke_function_3()
+    {
+        var function = GPTFunctionDefinition.Create((int a, int b = 1) => a+b, "inc");
+
+        var jsonArgs = """
+                        {
+                            "name": "inc",
+                            "arguments": "{ \"a\": 23}"
+                        }
+                       """;
+
+        function.Execute<int>(jsonArgs, out var result);
+        result.Should().Be(24);
     }
 }
