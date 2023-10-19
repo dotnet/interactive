fcb6b67b5596985046ca32f21bc14d1ae9a8dc05
cartercanedy <cartercanedy42@gmail.com>
2023-10-19 07:20:16 -0700

    corrects logic for cursor buffer position & adds tests to catch unhandled exception

--- src/Microsoft.DotNet.Interactive/Kernel.cs
+++ src/Microsoft.DotNet.Interactive/Kernel.cs
@@ -228,13 +228,16 @@ public abstract partial class Kernel :
         var absolutePosition = tree.GetAbsolutePosition(command.LinePosition);
 
         // don't let abs position drop below 0
-        if (absolutePosition >= tree.Length && absolutePosition > 0)
+        if (absolutePosition > 0)
         {
-            absolutePosition--;
-        }
-        else if (char.IsWhiteSpace(rootNode.Text[absolutePosition]))
-        {
-            absolutePosition--;
+            if (absolutePosition >= tree.Length)
+            {
+                absolutePosition--;
+            }
+            else if (char.IsWhiteSpace(rootNode.Text[absolutePosition]))
+            {
+                absolutePosition--;
+            }
         }
 
         if (rootNode.FindNode(absolutePosition) is LanguageNode node)
@@ -261,7 +264,7 @@ public abstract partial class Kernel :
             adjustedCommand = null;
             // need to return false to notify caller of failure
             // otherwise caller assumes out param is valid ref
-            return false;
+            //return false;
         }
 
         return true;
