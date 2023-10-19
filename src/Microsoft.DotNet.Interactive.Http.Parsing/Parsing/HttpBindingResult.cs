5c8800bde6b4bb63ed3ae057eabb67b73fcc5195
Shyam N <shyamnamboodiripad@users.noreply.github.com>
2023-10-18 23:47:30 -0700

    Relax error to warning (#3276)

--- src/Microsoft.DotNet.Interactive.Http.Parsing/Parsing/HttpBindingResult.cs
+++ src/Microsoft.DotNet.Interactive.Http.Parsing/Parsing/HttpBindingResult.cs
@@ -4,6 +4,7 @@
 #nullable enable
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.DotNet.Interactive.Http.Parsing;
 
@@ -20,11 +21,27 @@ internal class HttpBindingResult<T>
 
     public T? Value { get; set; }
 
-    public static HttpBindingResult<T> Success(T value) => new()
+    public static HttpBindingResult<T> Success(T value, params Diagnostic[] diagnostics)
     {
-        IsSuccessful = true,
-        Value = value
-    };
+        if (diagnostics is not null &&
+            diagnostics.Any(d => d.Severity is CodeAnalysis.DiagnosticSeverity.Error))
+        {
+            throw new ArgumentException("Errors must not be present when binding is successful.", nameof(diagnostics));
+        }
+
+        var result = new HttpBindingResult<T>
+        {
+            IsSuccessful = true,
+            Value = value
+        };
+
+        if (diagnostics is not null)
+        {
+            result.Diagnostics.AddRange(diagnostics);
+        }
+
+        return result;
+    }
 
     public static HttpBindingResult<T> Failure(params Diagnostic[] diagnostics)
     {
@@ -33,9 +50,14 @@ internal class HttpBindingResult<T>
             throw new ArgumentNullException(nameof(diagnostics));
         }
 
-        if (diagnostics.Length == 0)
+        if (diagnostics.Length is 0)
         {
             throw new ArgumentException("Value cannot be an empty collection.", nameof(diagnostics));
+        }
+
+        if (!diagnostics.Any(e => e.Severity is CodeAnalysis.DiagnosticSeverity.Error))
+        {
+            throw new ArgumentException("At least one error must be present when binding is unsuccessful.", nameof(diagnostics));
         }
 
         var result = new HttpBindingResult<T>
