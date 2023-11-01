9e02cd72a5aee5f80e46d9ecfd64f2f8cdae96e7
Diego Colombo <dicolomb@microsoft.com>
2023-11-01 13:05:15 +0000

    support for random order

---
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/SamplingTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.AIUtilities.Tests;
+
+public class SamplingTest
+{
+    [Fact]
+    public void can_shuffle_collection()
+    {
+        var src = Enumerable.Range(0, 10);
+        var shuffled = src.RandomOrder().ToArray();
+        shuffled.Should().NotBeInAscendingOrder();
+    }
+
+    [Fact]
+    public void does_not_duplicate_items()
+    {
+        var src = Enumerable.Range(0, 10).ToArray();
+        var shuffled = src.RandomOrder().Distinct().ToArray();
+        shuffled.Should().HaveCount(src.Length);
+    }
+}
