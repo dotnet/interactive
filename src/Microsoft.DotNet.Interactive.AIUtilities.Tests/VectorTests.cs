1967e52d9d14946f218bb2841ac51c4bebc9feb6
Diego Colombo <dicolomb@microsoft.com>
2023-11-03 17:49:15 +0000

    add support for calculating centroid of a list of vectors

---
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/VectorTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.AIUtilities.Tests;
+
+public class VectorTests
+{
+    [Fact]
+    public void can_calculate_centroid_of_vectors()
+    {
+        var data = new[]
+        {
+            new []{ 1f, 1f, 1f},
+            new []{ 2f, 2f, 2f},
+            new []{ 1f, 2f, 3f}
+        };
+
+        var centroid = data.Centroid();
+
+        centroid.Should().BeEquivalentTo(new[]{ 1.3333334F, 1.6666666F, 2F });
+    }
+}
---
+++ src/Microsoft.DotNet.Interactive.AIUtilities/Vectors.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+namespace Microsoft.DotNet.Interactive.AIUtilities;
+
+public static class Vectors
+{
+    public static float[] Centroid(this IEnumerable<float[]> vectors)
+    {
+        var size = vectors.First().Length;
+
+        var accumulated = vectors.Aggregate((Enumerable.Repeat<float>(0f, size), 0), (acc, d) => (acc.Item1.Zip(d, (a, b) => a + b).ToArray(), acc.Item2 + 1));
+
+        return accumulated.Item1.Select(e => e/ accumulated.Item2).ToArray();
+    }
+}
