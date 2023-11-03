8d7a280e47400bbe07f6aac48893e1813ea5b5d0
Diego Colombo <dicolomb@microsoft.com>
2023-11-03 18:16:55 +0000

    use weighted

--- src/Microsoft.DotNet.Interactive.AIUtilities.Tests/VectorTests.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities.Tests/VectorTests.cs
@@ -9,7 +9,7 @@ namespace Microsoft.DotNet.Interactive.AIUtilities.Tests;
 public class VectorTests
 {
     [Fact]
-    public void can_calculate_centroid_of_vectors()
+    public void can_calculate_average_of_vectors()
     {
         var data = new[]
         {
@@ -21,5 +21,20 @@ public class VectorTests
         var centroid = data.Centroid();
 
         centroid.Should().BeEquivalentTo(new[]{ 1.3333334F, 1.6666666F, 2F });
+    }
+
+    [Fact]
+    public void can_calculate_weighted_centroid_of_vectors()
+    {
+        var data = new[]
+        {
+            new []{ 1f, 1f, 1f},
+            new []{ 2f, 2f, 2f},
+            new []{ 1f, 2f, 3f}
+        };
+
+        var centroid = data.Centroid(d => d.Last());
+
+        centroid.Should().BeEquivalentTo(new[] { 4.3333335F, 4.6666665F, 5F });
     }
 }
