191b4a9f754a365f49bb2eab4d1dabc80adf56c3
Diego Colombo <dicolomb@microsoft.com>
2023-11-15 09:15:55 +0000

    change target frameworks for Ai utilities

--- src/Microsoft.DotNet.Interactive.AIUtilities/CosineSimilarityComparer.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities/CosineSimilarityComparer.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 
+using System;
 using Pocket;
 
 namespace Microsoft.DotNet.Interactive.AIUtilities;
