5c255464dc9af2a12409d0609525c981b58dc63a
Diego Colombo <dicolomb@microsoft.com>
2024-01-08 22:39:13 +0000

    improve api for image encoding

--- src/Microsoft.DotNet.Interactive.AIUtilities/Image.cs
+++ src/Microsoft.DotNet.Interactive.AIUtilities/Image.cs
@@ -1,18 +1,20 @@
-﻿
-
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using Pocket;
 using SkiaSharp;
 
-
 namespace Microsoft.DotNet.Interactive.AIUtilities;
 
 using Logger = Pocket.Logger<SKImage>;
 
 public static class Image {
+    /// <summary>
+    /// Generates a base64 encoded string for the image.
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns>The base64 encoding of the image as png</returns>
     public static string ToBase64(this SKImage image)
     {
         Logger.Log.Event();
@@ -20,10 +22,16 @@ public static class Image {
         return Convert.ToBase64String(encoded.ToArray());
     }
 
-    public static string ToDataUrl(this SKImage image)
+    /// <summary>
+    /// Generates a data URI for the image.
+    /// This can be used in the src attribute of an img tag to display the image in a browser.
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns>A string in the format data:image/jpeg;base64,{base64EncodedImage}</returns>
+    public static string ToImageSrc(this SKImage image)
     {
         Logger.Log.Event();
         var base64Image = image.ToBase64();
-        return $"data:image/jpeg; base64,{base64Image}";
+        return $"data:image/png;base64,{base64Image}";
     }
 }
