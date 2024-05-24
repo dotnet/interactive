2864315bb8e1a809ee920feefff523b26f44ef9d
bleaphar <33817274+bleaphar@users.noreply.github.com>
2024-05-24 10:49:48 -0700

    staging for clean

---
+++ src/Microsoft.DotNet.Interactive.Http.Parsing/Parsing/HttpCommentNamedRequestValueNode.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.DotNet.Interactive.Http.Parsing;
+internal class HttpCommentNamedRequestValueNode : HttpSyntaxNode
+{
+    public HttpCommentNamedRequestValueNode(SourceText sourceText, HttpSyntaxTree syntaxTree) : base(sourceText, syntaxTree)
+    {
+    }
+}
---
+++ src/Microsoft.DotNet.Interactive.Http.Parsing/Parsing/HttpNamedRequestNode.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.DotNet.Interactive.Http.Parsing
+{
+    internal class HttpNamedRequestNode : HttpRequestNode
+    {
+        internal HttpNamedRequestNode(SourceText sourceText, HttpSyntaxTree syntaxTree) : base(sourceText, syntaxTree)
+        {
+        }
+
+        public string Name { get; private set; }
+
+        private HttpRequestNode RequestNode;
+
+        private HttpResponse Response;
+    }
+}
