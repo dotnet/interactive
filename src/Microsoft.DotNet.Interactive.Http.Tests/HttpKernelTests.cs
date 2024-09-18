3f253edfcd5a8863621f68649d301cbcfdfa2a1e
bleaphar <33817274+bleaphar@users.noreply.github.com>
2024-09-17 17:13:30 -0700

    Change to string literal and nit changes

--- src/Microsoft.DotNet.Interactive.Http.Tests/HttpKernelTests.cs
+++ src/Microsoft.DotNet.Interactive.Http.Tests/HttpKernelTests.cs
@@ -2314,38 +2314,42 @@ public class HttpKernelTests
     [Fact]
     public async Task headers_for_named_requests_can_be_accessed_correctly()
     {
-
+        var headerValue = string.Empty;
         var responseHandler = new InterceptingHttpMessageHandler((message, _) =>
         {
             var response = new HttpResponseMessage(HttpStatusCode.OK);
+            if(message.Headers.TryGetValues("Detectedserver", out var values))
+            {
+                headerValue = values.First();
+            }
+                
             response.RequestMessage = message;
-            var contentString = @"
+            var contentString = """
              {
-    ""headers"": {
-        ""Accept"": ""text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"",
-        ""Accept-Encoding"": ""gzip, deflate, br, zstd"",
-        ""Accept-Language"": ""en-US,en;q=0.9"",
-        ""Host"": ""httpbin.org"",
-        ""Priority"": ""u=0, i"",
-        ""Sec-Ch-Ua"": ""\""Chromium\"";v=\""128\"", \""Not;A=Brand\"";v=\""24\"", \""Microsoft Edge\"";v=\""128\"""",
-        ""Sec-Ch-Ua-Mobile"": ""?0"",
-        ""Sec-Ch-Ua-Platform"": ""\""Windows\"""",
-        ""Sec-Fetch-Dest"": ""document"",
-        ""Sec-Fetch-Mode"": ""navigate"",
-        ""Sec-Fetch-Site"": ""none"",
-        ""Sec-Fetch-User"": ""?1"",
-        ""Upgrade-Insecure-Requests"": ""1"",
-        ""User-Agent"": ""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0"",
-        ""X-Amzn-Trace-Id"": ""Root=1-66e9f16e-7afea6f643e754f854c57d85""
-    }
-}
-            ";
+                "headers": {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "Accept-Encoding": "gzip, deflate, br, zstd",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Host": "httpbin.org",
+                    "Priority": "u=0, i",
+                    "Sec-Ch-Ua": "\""Chromium\"";v=\""128\"", \""Not;A=Brand\"";v=\""24\"", \""Microsoft Edge\"";v=\""128\"",
+                    "Sec-Ch-Ua-Mobile": "?0",
+                    "Sec-Ch-Ua-Platform": "\""Windows\"",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "none",
+                    "Sec-Fetch-User": "?1",
+                    "Upgrade-Insecure-Requests": "1",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+                    "X-Amzn-Trace-Id": "Root=1-66e9f16e-7afea6f643e754f854c57d85"
+                }
+            }
+            """;
             response.Content = new StringContent(contentString, Encoding.UTF8, "application/json");
             response.Headers.Add("server", "gunicorn/19.9.0");
             return Task.FromResult(response);
         });
         var client = new HttpClient(responseHandler);
-        // var client = new HttpClient();
         using var kernel = new HttpKernel("http", client);
 
         var code = """
@@ -2375,7 +2379,7 @@ public class HttpKernelTests
 
         var response = (HttpResponse)returnValue.Value;
 
-        response.Headers.GetValueOrDefault("Server").First().Should().Be("gunicorn/19.9.0");
+        headerValue.Should().Be("gunicorn/19.9.0");
     }
 
     [Theory]
@@ -2417,7 +2421,6 @@ public class HttpKernelTests
             return Task.FromResult(response);
         });
         var client = new HttpClient(responseHandler);
-        // var client = new HttpClient();
         using var kernel = new HttpKernel("http", client);
 
         var code = """
