3efeba196648272fd5cc1fad26a9ceb46da4eb9b
Jon Sequeira <jonsequeira@gmail.com>
2024-10-03 10:52:42 -0700

    update repack.ps1 to run in script location

--- repack.ps1
+++ repack.ps1
@@ -1,24 +1,33 @@
 
-# clean up the previously-cached NuGet packages
-Remove-Item -Recurse ~\.nuget\packages\microsoft.dotnet.interactive* -Force
+Push-Location $PSScriptRoot
 
-# build and pack dotnet-interactive 
-dotnet clean -c debug
-dotnet build -c debug
-dotnet pack -c debug /p:PackageVersion=2.0.0
+try
+{
+    # clean up the previously-cached NuGet packages
+    Remove-Item -Recurse ~\.nuget\packages\microsoft.dotnet.interactive* -Force
 
-# copy the dotnet-interactive packages to the temp directory
-$destinationPath = "C:\temp\packages"
-if (-not (Test-Path -Path $destinationPath -PathType Container)) {
-    New-Item -Path $destinationPath -ItemType Directory -Force
+    # build and pack dotnet-interactive 
+    dotnet clean -c debug
+    dotnet build -c debug
+    dotnet pack -c debug /p:PackageVersion=2.0.0
+
+    # copy the dotnet-interactive packages to the temp directory
+    $destinationPath = "C:\temp\packages"
+    if (-not (Test-Path -Path $destinationPath -PathType Container)) {
+        New-Item -Path $destinationPath -ItemType Directory -Force
+    }
+    Get-ChildItem -Recurse -Filter *.nupkg | Move-Item -Destination $destinationPath -Force
+
+    # delete the #r nuget caches
+    if (Test-Path -Path ~\.packagemanagement\nuget\Cache -PathType Container) {
+        Remove-Item -Recurse -Force ~\.packagemanagement\nuget\Cache
+    }
+
+    if (Test-Path -Path ~\.packagemanagement\nuget\Projects -PathType Container) {
+        Remove-Item -Recurse -Force ~\.packagemanagement\nuget\Projects
+    }
 }
-Get-ChildItem -Recurse -Filter *.nupkg | Move-Item -Destination $destinationPath -Force
-
-# delete the #r nuget caches
-if (Test-Path -Path ~\.packagemanagement\nuget\Cache -PathType Container) {
-    Remove-Item -Recurse -Force ~\.packagemanagement\nuget\Cache
-}
-
-if (Test-Path -Path ~\.packagemanagement\nuget\Projects -PathType Container) {
-    Remove-Item -Recurse -Force ~\.packagemanagement\nuget\Projects
+finally
+{
+    $PopLocation
 }
