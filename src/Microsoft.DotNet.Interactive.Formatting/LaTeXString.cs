64f9d75be066b1ac3f3eec1dfff0d17594282805
Jon Sequeira <jonsequeira@gmail.com>
2024-10-16 14:31:11 -0700

    move some formatter registrations to TypeFormatter implementations

--- src/Microsoft.DotNet.Interactive.Formatting/LaTeXString.cs
+++ src/Microsoft.DotNet.Interactive.Formatting/LaTeXString.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (c) Microsoft. All rights reserved. 
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 
-namespace Microsoft.DotNet.Interactive.Jupyter.Formatting;
+namespace Microsoft.DotNet.Interactive.Formatting;
 
+[TypeFormatterSource(typeof(LaTeXStringFormatterSource))]
 public class LaTeXString
 {
     private readonly string _latexCode;
@@ -14,7 +15,7 @@ public class LaTeXString
         _latexCode = latexCode ?? throw new ArgumentNullException(nameof(latexCode));
     }
 
-    public static implicit operator LaTeXString(string source) => new LaTeXString(source);
+    public static implicit operator LaTeXString(string source) => new(source);
 
     public override string ToString()
     {
