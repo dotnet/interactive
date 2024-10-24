1d5d45f4a00609da96faddecaee400b4fa338e22
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2024-10-24 11:11:28 +0300

    feat: add initial test

---
+++ src/Microsoft.DotNet.Interactive.PostgreSql.Tests/PSqlConnectionTests.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.DotNet.Interactive.App;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.CSharp;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Formatting;
+using Microsoft.DotNet.Interactive.Formatting.Csv;
+using Microsoft.DotNet.Interactive.Formatting.TabularData;
+using Microsoft.DotNet.Interactive.Tests.Utility;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.PostgreSql.Tests;
+
+public class PSqlConnectionTests : IDisposable
+{
+    private CompositeKernel CreateKernel()
+    {
+        Formatter.SetPreferredMimeTypesFor(typeof(TabularDataResource), HtmlFormatter.MimeType, CsvFormatter.MimeType);
+        var csharpKernel = new CSharpKernel().UseNugetDirective().UseValueSharing();
+
+        // TODO: remove SQLKernel it is used to test current patch
+        var kernel = new CompositeKernel
+        {
+            new SqlDiscoverabilityKernel(),
+            csharpKernel,
+            new KeyValueStoreKernel()
+        };
+
+        kernel.DefaultKernelName = csharpKernel.Name;
+
+        PSqlKernelExtension.Load(kernel);
+
+        return kernel;
+    }
+
+
+    [PSqlFact]
+    public async Task It_can_connect_and_query_data()
+    {
+        var connectionString = PSqlFactAttribute.GetConnectionStringForTests();
+        using var kernel = CreateKernel();
+        var connect = $"#!connect psql --kernel-name adventureworks \"{connectionString}\"";
+        var result = await kernel.SubmitCodeAsync(connect);
+
+        result.Events
+              .Should()
+              .NotContainErrors();
+
+        result = await kernel.SubmitCodeAsync("""
+            #!sql-adventureworks
+            SELECT * FROM Person.Person LIMIT 100;
+            """);
+
+        result.Events.Should()
+              .NotContainErrors()
+              .And
+              .ContainSingle<DisplayedValueProduced>(e =>
+                e.FormattedValues.Any(f => f.MimeType == PlainTextFormatter.MimeType));
+
+        result.Events.Should()
+              .ContainSingle<DisplayedValueProduced>(e =>
+                e.FormattedValues.Any(f => f.MimeType == HtmlFormatter.MimeType));
+    }
+
+    public void Dispose()
+    {
+        DataExplorer.ResetToDefault();
+        Formatter.ResetToDefault();
+    }
+}
---
+++ src/Microsoft.DotNet.Interactive.PostgreSql.Tests/PSqlFactAttribute.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace Microsoft.DotNet.Interactive.PostgreSql.Tests;
+
+public sealed class PSqlFactAttribute : FactAttribute
+{
+    private const string TEST_PSQL_CONNECTION_STRING = nameof(TEST_PSQL_CONNECTION_STRING);
+    private static readonly string _skipReason;
+
+    static PSqlFactAttribute()
+    {
+        _skipReason = TestConnectionAndReturnSkipReason();
+    }
+
+    public PSqlFactAttribute()
+    {
+        if (_skipReason is not null)
+        {
+            Skip = _skipReason;
+        }
+    }
+
+    internal static string TestConnectionAndReturnSkipReason()
+    {
+        string connectionString = GetConnectionStringForTests();
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            return $"Environment variable {TEST_PSQL_CONNECTION_STRING} is not set. To run tests that require "
+                   + "SQL Server, this environment variable must be set to a valid connection string value.";
+        }
+
+        try
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+        catch (Exception e)
+        {
+            return $"A connection could not be established to SQL Server. Verify the connection string value used " +
+                   $"for environment variable {TEST_PSQL_CONNECTION_STRING} targets a running SQL Server instance. " +
+                   $"Connection failed failed with error: {e}";
+        }
+
+        return null;
+    }
+
+    public static string GetConnectionStringForTests()
+    {
+        // example:
+        // Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=postgres
+        return Environment.GetEnvironmentVariable(TEST_PSQL_CONNECTION_STRING);
+    }
+}
