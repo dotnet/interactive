431f62514a4713f84263a6f70da97e21e3bddedc
Diego Colombo <dicolomb@microsoft.com>
2024-10-30 20:03:08 +0000

    add duckdb kernel

---
+++ src/Microsoft.DotNet.Interactive.DuckDB/ConnectDuckDBKernel.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Interactive.Commands;
+
+namespace Microsoft.DotNet.Interactive.DuckDB;
+
+public class ConnectDuckDBKernel : ConnectKernelCommand
+{
+    public ConnectDuckDBKernel(string connectedKernelName) : base(connectedKernelName)
+    {
+    }
+
+    public string ConnectionString { get; set; }
+}
