51e5012760f9a3ef392396d785cbea1496624e14
Osvaldo Calles <oscalles@microsoft.com>
2024-11-07 15:44:58 -0800

    Refactor to avoid duplicate code

--- eng/publish/PublishInsidersPolyglotNotebooks.ps1
+++ eng/publish/PublishInsidersPolyglotNotebooks.ps1
@@ -1,3 +1,5 @@
+Import-Module "$PSScriptRoot\PublishPolyglotNotebooksHelper.psm1"
+
 [CmdletBinding(PositionalBinding = $false)]
 param (
     [string]$artifactsPath,
@@ -5,39 +7,18 @@ param (
     [string]$isSimulated
 )
 
-Set-StrictMode -version 2.0
+Set-StrictMode -Version 2.0
 $ErrorActionPreference = "Stop"
 
-$simulate = if ($isSimulated -eq "true") { $true } else { $false }
+$simulate = $isSimulated -eq "true"
 Write-Host "Simulate switch is set to: $simulate"
 
 try {
-    Write-Host "vscode target: insiders"
-
-    Write-Host "Find extension vsix..."
-    $extension = Get-ChildItem "$artifactsPath\vscode\insiders\dotnet-interactive-vscode-*.vsix" | Select-Object -First 1
-    Write-Host "Found extension: $extension"
-
-    Write-Host "Verify the extension..."
-    if ($simulate) {
-        Write-Host "Simulated command: . '$PSScriptRoot\VerifyVSCodeExtension.ps1' -extensionPath $extension"
-    } else {
-        . "$PSScriptRoot\VerifyVSCodeExtension.ps1" -extensionPath $extension
-        if ($LASTEXITCODE -ne 0) {
-            exit $LASTEXITCODE
-        }
-    }
-
-    Write-Host "Publishing extension $extension to VS Code Marketplace using Managed Identity..."
-    if ($simulate) {
-        Write-Host "Simulated command: vsce publish --pre-release --packagePath $extension --noVerify --azure-credential"
-    } else {
-        vsce publish --pre-release --packagePath $extension --noVerify --azure-credential 
-    }
+    PublishInsidersExtension -artifactsPath $artifactsPath -simulate $simulate
 }
 catch {
     Write-Host $_
-    Write-Host $_.Exception
+    Write-Host $_.Exception.Message
     Write-Host $_.ScriptStackTrace
     exit 1
 }
