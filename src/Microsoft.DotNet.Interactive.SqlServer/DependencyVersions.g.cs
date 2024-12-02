6ec1643dc97a293c8e37216f27a2dee0f5c82753
Jon Sequeira <jonsequeira@gmail.com>
2024-12-02 09:42:52 -0800

    generate consts for package versions used in emitted EF code

---
+++ src/Microsoft.DotNet.Interactive.SqlServer/DependencyVersions.g.cs
@@ -0,0 +1,12 @@
+namespace Microsoft.DotNet.Interactive.SqlServer;
+ 
+/// <summary>Provides information about the package versions depended on by the current project. This class was code-generated.</summary>
+internal static class DependencyVersions
+{
+    public const string HumanizerCoreVersion = "2.14.1";
+    public const string HumanizerVersion = "2.14.1";
+    public const string MicrosoftDataSqlClientVersion = "5.2.2";
+    public const string MicrosoftEntityFrameworkCoreDesignVersion = "8.0.10";
+    public const string MicrosoftEntityFrameworkCoreSqlServerVersion = "8.0.10";
+    public const string MicrosoftIdentityClientVersion = "4.65.0";
+}
