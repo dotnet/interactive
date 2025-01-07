e7846739c63cfdd658a643629136a52a7e8ead2b
Jon Sequeira <jonsequeira@gmail.com>
2025-01-07 11:13:07 -0800

    try skipping these tests this way

--- test-retry-runner.ps1
+++ test-retry-runner.ps1
@@ -9,7 +9,8 @@ $ErrorActionPreference = "Stop"
 
 if ($IsWindows) {
     $projectsToSkip = @(
-        "Microsoft.DotNet.Interactive.CSharpProject.Tests"
+        "Microsoft.DotNet.Interactive.CSharpProject.Tests",
+        "Microsoft.DotNet.Interactive.Jupyter.Tests"
         )
 }
 else
@@ -18,7 +19,8 @@ else
         "Microsoft.DotNet.Interactive.NetFramework.Tests",
         "Microsoft.DotNet.Interactive.CSharpProject.Tests",
         "Microsoft.DotNet.Interactive.NamedPipeConnector.Tests",
-        "Microsoft.DotNet.Interactive.VisualStudio.Tests"        
+        "Microsoft.DotNet.Interactive.VisualStudio.Tests",
+        "Microsoft.DotNet.Interactive.Jupyter.Tests"
         )
 }
 
