f014d0ab4b8dec06091b907fe32d063772b74951
Youssef1313 <youssefvictor00@gmail.com>
2025-03-20 20:58:32 +0100

    Cleanup

--- test-retry-runner.ps1
+++ test-retry-runner.ps1
@@ -28,15 +28,10 @@ function ExecuteTestDirectory([string]$testDirectory, [string]$extraArgs = "") {
 
 try {
     $repoRoot = Resolve-Path $PSScriptRoot
-    $flakyTestAssemblyDirectories = @(
-        "Microsoft.DotNet.Interactive.Tests",
-        "Microsoft.DotNet.Interactive.App.Tests",
-        "Microsoft.DotNet.Interactive.Browser.Tests"
-        )
-    
-    $normalTestAssemblyDirectories = Get-ChildItem -Path "$repoRoot/src" -Directory -Filter *.Tests -Recurse | Where-Object { !$flakyTestAssemblyDirectories.contains($_.Name)}
 
-    foreach ($testAssemblyDirectory in $normalTestAssemblyDirectories) {
+    $testAssemblyDirectories = Get-ChildItem -Path "$repoRoot/src" -Directory -Filter *.Tests -Recurse
+
+    foreach ($testAssemblyDirectory in $testAssemblyDirectories) {
         $projectName = $testAssemblyDirectory.Name
         if($projectsToSkip.contains($projectName)){
             Write-Host "Skipping test project $projectName"
@@ -51,26 +46,6 @@ try {
         }
         if ($LASTEXITCODE -ne 0) {
             exit $LASTEXITCODE
-        }
-    }
-
-    foreach ($flakyTestAssemblyDirectory in $flakyTestAssemblyDirectories){
-        $testNamePattern = "    ([^(]+)" # skip 4 spaces then get everything that's not a left paren because test names start with 4 spaces and [Theory] tests have a parenthesized argument list
-        $testNames = dotnet test "$repoRoot/src/$flakyTestAssemblyDirectory/" --no-restore --no-build --configuration $buildConfig --list-tests | Select-String -Pattern $testNamePattern | ForEach-Object { $_.Matches[0].Groups[1].Value }
-        $testClasses = $testNames | ForEach-Object { $_.Substring(0, $_.LastIndexOf([char]".")) } # trim off the test name, just get the class
-        $distinctTestClasses = $testClasses | Get-Unique
-
-        foreach ($testClass in $distinctTestClasses) {
-            for ($i = 1; $i -le $retryCount; $i++) {
-                Write-Host "Testing class $testClass, attempt $i"
-                ExecuteTestDirectory -testDirectory "$repoRoot/src/$flakyTestAssemblyDirectory" -extraArgs "--filter `"FullyQualifiedName~$testClass&Category!=Skip`""
-                if ($LASTEXITCODE -eq 0) {
-                    break
-                }
-            }
-            if ($LASTEXITCODE -ne 0) {
-                exit $LASTEXITCODE
-            }
         }
     }
 }
