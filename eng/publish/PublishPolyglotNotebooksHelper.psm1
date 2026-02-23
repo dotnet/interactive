d6d07907d9104d669fe3f2521e08f267b0a5b259
Abhitej John <aajohn@microsoft.com>
2026-02-23 09:50:11 -0800

    Use AGENT_TEMPDIRECTORY for Push-Location workaround

--- eng/publish/PublishPolyglotNotebooksHelper.psm1
+++ eng/publish/PublishPolyglotNotebooksHelper.psm1
@@ -121,11 +121,7 @@ function PublishStableExtensionAndNuGetPackages {
         "Microsoft.DotNet.Interactive"
     )
 
-    # Run dotnet CLI from outside the repo so global.json in the sources directory
-    # does not force an unavailable SDK during publish-only operations.
-    # Use $HOME rather than $env:TEMP to avoid inheriting any config files
-    # that other processes might have placed in the temp directory.
-    Push-Location $HOME
+    Push-Location $env:AGENT_TEMPDIRECTORY
     try {
         Get-ChildItem "$artifactsPath\packages\Shipping\*.nupkg" -Exclude '*.symbols.nupkg' | ForEach-Object {
             $nugetPackagePath = $_.FullName
